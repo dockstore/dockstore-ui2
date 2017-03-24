6559bd032b666e96a84c209b3866735f06376869
k-cao <hanyuancaowaterloo@gmail.com>
2017-03-24 13:44:12 -0400

    Navigate back to main documentation page upon unexpected slug

--- src/app/docs/page/page.component.ts
+++ src/app/docs/page/page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Subscription } from 'rxjs/Rx';
 
 import { Doc } from '../doc.model';
@@ -15,7 +15,7 @@ export class PageComponent implements OnInit {
 
   selectedDoc: Doc;
 
-  constructor(private activatedRoute: ActivatedRoute, private docsService: DocsService) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private docsService: DocsService) { }
 
   getDoc(slug: string): Doc {
       return this.docsService.getDoc(slug);
@@ -23,10 +23,14 @@ export class PageComponent implements OnInit {
 
   ngOnInit() {
     this.subscription = this.activatedRoute.params.subscribe(
-          (params: any) => {
-            this.slug = params['slug'];
-            this.selectedDoc = this.docsService.getDoc(this.slug);
-          }
+      (params: any) => {
+        this.slug = params['slug'];
+        this.selectedDoc = this.docsService.getDoc(this.slug);
+
+        if (!this.selectedDoc) {
+          this.router.navigate(['/docs']);
+        }
+      }
     );
   }
 
