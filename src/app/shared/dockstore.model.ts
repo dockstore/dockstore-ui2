c20f45af5df912a20252a369d39aaa772a2465b2
k-cao <hanyuancaowaterloo@gmail.com>
2017-04-21 10:27:17 -0400

    Updated Dockstore model and npm package

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -1,4 +1,19 @@
 export class Dockstore {
 
-  public static readonly API_URI = 'https://dockstore.org:8443';
+  static readonly API_URI = 'http://localhost:8080';
+
+  static readonly GITHUB_CLIENT_ID = '';
+
+  static readonly QUAYIO_AUTH_URL = 'https://quay.io/oauth/authorize';
+  static readonly QUAYIO_REDIRECT_URI = 'http://localhost:4200/auth/quay.io';
+  static readonly QUAYIO_SCOPE = 'repo:read,user:read';
+  static readonly QUAYIO_CLIENT_ID = '';
+
+  static readonly BITBUCKET_AUTH_URL = 'https://bitbucket.org/site/oauth2/authorize';
+  static readonly BITBUCKET_CLIENT_ID = '';
+
+  static readonly GITLAB_AUTH_URL = 'https://gitlab.com/oauth/authorize';
+  static readonly GITLAB_CLIENT_ID = '';
+  static readonly GITLAB_REDIRECT_URI = 'http://localhost:4200/auth/gitlab';
+
 }
