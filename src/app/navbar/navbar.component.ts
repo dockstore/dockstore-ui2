e4e47ea7d9a19db6e3cc90f7937646b74b2f52f8
k-cao <hanyuancaowaterloo@gmail.com>
2017-04-24 16:13:34 -0400

    Tokens page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
-import { Subscription } from 'rxjs/Subscription';
-
-import { TrackLoginService } from '../shared/track-login.service';
-import { LogoutService } from '../shared/logout.service';
+import { Logout } from '../loginComponents/logout';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnDestroy {
-
-  subscription: Subscription;
-  isLoggedIn: boolean;
-
-  constructor(private trackLoginService: TrackLoginService,
-              private logoutService: LogoutService,
-              private router: Router) {
-    this.subscription = this.trackLoginService.isLoggedIn$.subscribe(
-      state => {
-        this.isLoggedIn = state;
-      }
-    );
-  }
-
-  logout() {
-    this.logoutService.logout();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-}
+export class NavbarComponent extends Logout { }
