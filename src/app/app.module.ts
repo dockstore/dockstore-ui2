ae3904809efea79850c5b80c880e1290a8f8a3ee
garyluu <garylau.work@gmail.com>
2017-05-31 09:30:06 -0400

    Feature/containers css (#14)
    Fix SCSS for Available Tools page and Home page' Browse Tools and Workflows component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+/* Angular Modules */
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-/* Angular Modules */
 import { BrowserModule } from '@angular/platform-browser';
 
 /* Bootstrap */
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public browseToolsTab = 'browseToolsTab';
+  public browseWorkflowsTab = 'browseWorkflowsTab';
   constructor() {
   }
 
   ngOnInit() {
     (<any>$('.youtube')).colorbox({iframe: true, innerWidth: 640, innerHeight: 390});
   }
+
+
+
 
 }
