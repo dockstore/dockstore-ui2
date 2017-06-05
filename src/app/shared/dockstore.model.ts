6e13595194b95a9c529e916f3e170a6eb86c616f
Denis Yuen <denis.yuen@oicr.on.ca>
2017-06-05 11:42:18 -0400

    Feature/quay login fix (#17)
    * Fix typo for quay.io blocking linking

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -4,11 +4,11 @@ export class Dockstore {
 
   static readonly GITHUB_CLIENT_ID = 'fill this in';
   static readonly GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
-  static readonly GITHUB_REDIRECT_URI = 'http://fill this in:8080/auth/tokens/github.com';
+  static readonly GITHUB_REDIRECT_URI = 'http://fill this in:4200/auth/tokens/github';
   static readonly GITHUB_SCOPE = 'user,user:email';
 
   static readonly QUAYIO_AUTH_URL = 'https://quay.io/oauth/authorize';
-  static readonly QUAYIO_REDIRECT_URI = 'http://fill this in:4200/auth/quay.io';
+  static readonly QUAYIO_REDIRECT_URI = 'http://fill this in:4200/auth/quay';
   static readonly QUAYIO_SCOPE = 'repo:read,user:read';
   static readonly QUAYIO_CLIENT_ID = 'fill this in';
 
