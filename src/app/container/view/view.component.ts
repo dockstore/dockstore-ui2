122d60f7fba5ce768b344a92b3d764066cd88cb6
garyluu <garylau.work@gmail.com>
2017-06-07 11:39:59 -0400

    Initial form validation

--- src/app/container/view/view.component.ts
+++ src/app/container/view/view.component.ts
@@ -2,7 +2,8 @@ import { ContainerTagsService } from './../../shared/containerTags.service';
 import { ParamfilesService } from './../paramfiles/paramfiles.service';
 import { ContainerService } from './../container.service';
 import { CommunicatorService } from './../../shared/communicator.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
+import { NgForm, Validators } from '@angular/forms';
 
 import { DateService } from '../../shared/date.service';
 import { ListContainersService } from './../../containers/list/list.service';
@@ -16,9 +17,9 @@ import { DescriptorType } from '../../shared/enum/descriptorType.enum';
   styleUrls: ['./view.component.css']
 })
 // This is actually the tag edtior
-export class ViewContainerComponent extends View implements OnInit {
+export class ViewContainerComponent extends View implements OnInit, AfterViewChecked {
 
-  // Enums
+  // Enumss
   public TagEditorMode = TagEditorMode;
   public DescriptorType = DescriptorType;
 
@@ -34,7 +35,64 @@ export class ViewContainerComponent extends View implements OnInit {
   private unsavedWDLTestParameterFilePaths: Array<string>;
   private unsavedTestCWLFile: string;
   private unsavedTestWDLFile: string;
+  formErrors = {
+    'cwlPath': '',
+    'wdlPath': '',
+    'dockerfilePath': '',
+    'gitPath': '',
+    'imagePath': '',
+    'label': '',
+    'testParameterFilePath': '',
+    'toolName': ''
+  };
 
+  validationMessages = {
+    'cwlPath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Descriptor Path is too short. (Min. 3 characters.)',
+      'maxlength': 'Descriptor Path is too long. (Max 256 characters.)',
+      'pattern': 'Invalid Descriptor Path format. Descriptor Path must begin with \'/\' and end with \'*.cwl\', \'*.yml\', or\'*.yaml\'.'
+    },
+    'wdlPath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Descriptor Path is too short. (Min. 3 characters.)',
+      'maxlength': 'Descriptor Path is too long. (Max 256 characters.)',
+      'pattern': 'Invalid Descriptor Path format. Descriptor Path must begin with \'/\' and end with \'*.wdl\'.'
+    },
+    'dockerfilePath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Dockerfile Path is too short. (Min. 3 characters.)',
+      'maxlength': 'Dockerfile Path is too long. (Max 256 characters.)',
+      'pattern': 'Invalid Dockerfile Path format. Dockerfile Path must begin with \'/\' and end with \'/Dockerfile\'.'
+    },
+    'gitPath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Source Code Repository Path is too short. (Min. 3 characters.)',
+      'maxlength': 'Source Code Repository Path is too long. (Max 128 characters.)',
+      'pattern': 'The namespace and name of the Git repository, separated by a \'/\'. Currently, only GitHub, Bitbucket and GitLab are supported third-party platforms.'
+    },
+    'imagePath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Image Path is too short. (Min. 3 characters.)',
+      'maxlength': 'Image Path is too long. (Max 128 characters.)',
+      'pattern': 'The namespace and name of the image repository, separated by a \'/\'. Use \'_\' for an empty namespace. Currently, only Quay.io and Docker Hub are supported third-party platforms.'
+    },
+    'label': {
+      'maxlength': 'Labels string is too long. (Max 512 characters.)',
+      'pattern': 'Labels are comma-delimited, and may only contain alphanumerical characters and internal hyphens.'
+    },
+    'testParameterFilePath': {
+      'required': 'This field cannot be empty.',
+      'minlength': 'Test parameter file path is too short. (Min. 3 characters.)',
+      'maxlength': 'Test parameter file path is too long. (Max 256 characters.)',
+      'pattern': 'Invalid Test parameter file format. Test parameter file path must begin with \'/\' and end with end with \'*.json\', \'*.yml\', or \'*.yaml\'.'
+    },
+    'toolName': {
+      'maxlength': 'Tool Name is too long. (Max 256 characters.)',
+      'pattern': 'A Tool Name may only consist of alphanumeric characters and internal underscores or hyphens.'
+    },
+  };
+  userForm: any;
   constructor(
     private paramfilesService: ParamfilesService,
     private viewService: ViewService,
@@ -49,6 +107,7 @@ export class ViewContainerComponent extends View implements OnInit {
   getSizeString(size) {
     return this.viewService.getSizeString(size);
   }
+
 
   onSubmit() {
     switch (this.mode) {
@@ -68,6 +127,41 @@ export class ViewContainerComponent extends View implements OnInit {
   addTag() {
     console.log('Saving tag...');
   }
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  tagEditorForm: NgForm;
+  @ViewChild('tagEditorForm') currentForm: NgForm;
+
+  formChanged() {
+    if (this.currentForm === this.tagEditorForm) { return; }
+    this.tagEditorForm = this.currentForm;
+    if (this.tagEditorForm) {
+      this.tagEditorForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+    }
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.tagEditorForm) { return; }
+    const form = this.tagEditorForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+
 
   editTag() {
     console.log('Editing tag...');
