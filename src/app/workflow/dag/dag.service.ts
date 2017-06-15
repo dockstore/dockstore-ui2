1a9ae929eb8d8b40c414fedc4b8cb48af3d807f6
garyluu <garylau.work@gmail.com>
2017-06-15 11:30:21 -0400

    Remove test code, move style to service

--- src/app/workflow/dag/dag.service.ts
+++ src/app/workflow/dag/dag.service.ts
@@ -11,42 +11,92 @@ export class DagService {
     validVersions: Subject<any> = new Subject<any>();
     currentWorkflowId: Subject<number> = new Subject<number>();
     currentVersion: Subject<any> = new Subject<any>();
-    currentDagResults: Subject<any> = new BehaviorSubject<any>({
-        nodes: [
-            { data: { id: 'n0' } },
-            { data: { id: 'n1' } },
-            { data: { id: 'n2' } },
-            { data: { id: 'n3' } },
-            { data: { id: 'n4' } },
-            { data: { id: 'n5' } },
-            { data: { id: 'n6' } },
-            { data: { id: 'n7' } },
-            { data: { id: 'n8' } },
-            { data: { id: 'n9' } },
-            { data: { id: 'n10' } },
-            { data: { id: 'n11' } },
-            { data: { id: 'n12' } },
-            { data: { id: 'n13' } },
-            { data: { id: 'n14' } },
-            { data: { id: 'n15' } },
-            { data: { id: 'n16' } }
-        ],
-        edges: [
-            { data: { source: 'n0', target: 'n1' } },
-            { data: { source: 'n1', target: 'n2' } },
-            { data: { source: 'n1', target: 'n3' } },
-            { data: { source: 'n4', target: 'n5' } },
-            { data: { source: 'n4', target: 'n6' } },
-            { data: { source: 'n6', target: 'n7' } },
-            { data: { source: 'n6', target: 'n8' } },
-            { data: { source: 'n8', target: 'n9' } },
-            { data: { source: 'n8', target: 'n10' } },
-            { data: { source: 'n11', target: 'n12' } },
-            { data: { source: 'n12', target: 'n13' } },
-            { data: { source: 'n13', target: 'n14' } },
-            { data: { source: 'n13', target: 'n15' } },
-        ]
-    });
+    currentDagResults: Subject<any> = new Subject<any>();
+    style = [
+        {
+            selector: 'node',
+            style: {
+                'content': 'data(name)',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#7a88a9'
+            }
+        },
+
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'target-arrow-shape': 'triangle',
+                'line-color': '#9dbaea',
+                'target-arrow-color': '#9dbaea',
+                'curve-style': 'bezier'
+            }
+        },
+
+        {
+            selector: 'node[id = "UniqueBeginKey"]',
+            style: {
+                'content': 'Start',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#4caf50'
+            }
+        },
+
+        {
+            selector: 'node[id = "UniqueEndKey"]',
+            style: {
+                'content': 'End',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#f44336'
+            }
+        },
+
+        {
+            selector: 'node[type = "workflow"]',
+            style: {
+                'content': 'data(name)',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#4ab4a9'
+            }
+        },
+
+        {
+            selector: 'node[type = "tool"]',
+            style: {
+                'content': 'data(name)',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#51aad8'
+            }
+        },
+
+        {
+            selector: 'node[type = "expressionTool"]',
+            style: {
+                'content': 'data(name)',
+                'font-size': '16px',
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': '#9966FF'
+            }
+        },
+
+        {
+            selector: 'edge.notselected',
+            style: {
+                'opacity': '0.4'
+            }
+        }
+    ];
 
     constructor(private httpService: HttpService) {
     }
