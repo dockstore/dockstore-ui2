a597636a3cb68ace17eeb9d0568ad02bdb5678b0
Denis Yuen <denis.yuen@oicr.on.ca>
2017-06-19 16:47:32 -0400

    Demo of single facet

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Client } from 'elasticsearch';
-import SearchResponse = Elasticsearch.SearchResponse;
+import {Component, OnInit} from '@angular/core';
+import {Client} from 'elasticsearch';
 import {CommunicatorService} from '../shared/communicator.service';
-
+import SearchResponse = Elasticsearch.SearchResponse;
 
 @Component({
   selector: 'app-search',
@@ -14,9 +13,24 @@ export class SearchComponent implements OnInit {
   private _client: Client;
   private hits: Object[];
   private aggs: Object;
+  private buckets: Map<string, Map<string, string>> = new Map<string, Map<string, string>>();
+  private initialQuery = '{"aggs":{"_type":{"terms":{"field":"_type","size":10000}},"registry":{"terms":{"field":"registry","size":10000}},"private_access":{"terms":{"field":"private_access","size":10000}},"tags_verified":{"terms":{"field":"tags.verified","size":10000}},"author":{"terms":{"field":"author","size":10000}},"namespace":{"terms":{"field":"namespace","size":10000}},"labels_value":{"terms":{"field":"labels.value","size":10000}},"tags_verifiedSource":{"terms":{"field":"tags.verifiedSource","size":10000}}},"query":{"match_all":{}}}';
+  private filters: Map<String, String> = new Map<String, String>();
+  private bucketStubs = new Map([
+    ['Entry Type', '_type'],
+    ['Registry', 'registry'],
+    ['Private Access', 'private_access'],
+    ['Verified', 'tags.verified'],
+    ['Author', 'author'],
+    ['Organization', 'namespace'],
+    ['Labels', 'labels.value'],
+    ['Verified Source', 'tags.verifiedSource'],
+  ]);
 
 
-  constructor( private communicatorService: CommunicatorService) {
+
+
+  constructor(private communicatorService: CommunicatorService) {
     this._client = new Client({
       host: 'http://localhost:8080/api/ga4gh/v1/extended',
       apiVersion: '2.4',
@@ -24,40 +38,139 @@ export class SearchComponent implements OnInit {
     });
   }
 
-  getContent(): void {
-    this._client.search({
-      index: 'tools',
-      type: 'entry',
-      body: {
-        query: {
-          match_all : {}
-        }
-      }
-    }).then(hits => this.hits = hits.hits.hits);
-  }
 
   ngOnInit() {
-    this.getContent();
+    this.onEnter(this.initialQuery);
   }
 
-  /* use this as an example query
-   {"aggs":{"_type_0":{"terms":{"field":"_type","size":10000}},"registry_1":{"terms":{"field":"registry","size":10000}},"private_access_2":{"terms":{"field":"private_access","size":10000}},"tags_verified_3":{"terms":{"field":"tags.verified","size":10000}},"author_4":{"terms":{"field":"author","size":10000}},"namespace_5":{"terms":{"field":"namespace","size":10000}},"labels_value_6":{"terms":{"field":"labels.value","size":10000}},"tags_verifiedSource_7":{"terms":{"field":"tags.verifiedSource","size":10000}}},"query":{"match_all":{}}}
 
-   or (one author only)
-
-   {"aggs":{"filtered__type_0":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"_type_0":{"terms":{"field":"_type","size":10000}}}},"filtered_registry_1":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"registry_1":{"terms":{"field":"registry","size":10000}}}},"filtered_private_access_2":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"private_access_2":{"terms":{"field":"private_access","size":10000}}}},"filtered_tags_verified_3":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"tags_verified_3":{"terms":{"field":"tags.verified","size":10000}}}},"filtered_author_4":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"author_4":{"terms":{"field":"author","size":10000}}}},"filtered_namespace_5":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"namespace_5":{"terms":{"field":"namespace","size":10000}}}},"filtered_labels_value_6":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"labels_value_6":{"terms":{"field":"labels.value","size":10000}}}},"filtered_tags_verifiedSource_7":{"filter":{"terms":{"author":["Andrey Kartashov"]}},"aggs":{"tags_verifiedSource_7":{"terms":{"field":"tags.verifiedSource","size":10000}}}}},"filter":{"terms":{"author":["Andrey Kartashov"]}},"query":{"match_all":{}}}
-   */
   onEnter(value: string) {
     this._client.search({
       index: 'tools',
       type: 'entry',
       body: value
     }).then(hits => {
-      this.hits = hits.hits.hits; this.aggs = JSON.stringify(hits.aggregations, null, 2);
+      this.hits = hits.hits.hits;
+      this.aggs = JSON.stringify(hits.aggregations, null, 2);
+      for (const property in hits.aggregations) {
+        if (hits.aggregations.hasOwnProperty(property)) {
+          // loop through contents buckets
+          console.log(property);
+          const category = hits.aggregations[property];
+          // look for top level buckets (no filtering)
+          if (category.buckets != null) {
+            category.buckets.forEach(bucket => {
+              if (this.buckets.get(property) == null) {
+                this.buckets.set(property, new Map<string, string>());
+              }
+              this.buckets.get(property).set(bucket.key, bucket.doc_count);
+            });
+          }
+
+          // look for second level buckets (with filtering)
+          for (const nestedProperty in category) {
+            if (category.hasOwnProperty(nestedProperty)) {
+              // this is copied and pasted, make this better
+              const nestedCategory = category[nestedProperty];
+              // look for top level buckets (no filtering)
+              if (nestedCategory != null && nestedCategory.buckets != null) {
+                nestedCategory.buckets.forEach(bucket => {
+                  if (this.buckets.get(nestedProperty) == null) {
+                    this.buckets.set(nestedProperty, new Map<string, string>());
+                  }
+                  this.buckets.get(nestedProperty).set(bucket.key, bucket.doc_count);
+                });
+              }
+            }
+          }
+        }
+      }
     });
   }
+
+  onClick(value1: string, value2: string) {
+    console.log(value1 + ' ' + value2);
+    if (this.filters.has(value1)) {
+      this.filters.delete(value1);
+    } else {
+      this.filters.set(value1, value2);
+    }
+    // assemble a query and pretend to click
+    const queryWrapper = new QueryWrapper();
+
+    if (this.filters.size === 1) {
+      queryWrapper.filter = new SingleFilter();
+      const t = {};
+      const modifiedFilterValue = value1.substring(0, 1) + value1.substring(1).replace('_', '.')
+      t[modifiedFilterValue] = [];
+      t[modifiedFilterValue][0] = [];
+      t[modifiedFilterValue][0] = value2;
+      queryWrapper.filter.terms = t;
+      // go through buckets
+      queryWrapper.aggs = {};
+
+      this.bucketStubs.forEach( key => {
+        const modifiedKey = key.replace('.', '_');
+        queryWrapper.aggs[modifiedKey] = {};
+        queryWrapper.aggs[modifiedKey].aggs = {};
+        queryWrapper.aggs[modifiedKey].aggs[modifiedKey] = {};
+        queryWrapper.aggs[modifiedKey].aggs[modifiedKey].terms = new AggTerms();
+        queryWrapper.aggs[modifiedKey].aggs[modifiedKey].terms.field = key;
+        // next, add the filtering clauses
+        queryWrapper.aggs[modifiedKey].filter = {};
+        queryWrapper.aggs[modifiedKey].filter.terms = t;
+      });
+    } else if (this.filters.size > 1) {
+
+    }
+    this.buckets.clear();
+    const query = JSON.stringify(queryWrapper, null, 2);
+    console.log(query);
+    this.onEnter(query);
+  }
+
+  resetFilters() {
+    this.filters.clear();
+    this.onEnter(this.initialQuery);
+  }
+
+
 
   sendToolInfo(tool) {
     this.communicatorService.setTool(tool);
   }
+
+
+}
+
+
+
+export class Match {}
+
+export class Query {
+  match_all: Match = new Match();
+}
+
+export class NestedTerms {
+  terms: AggTerms[] = [];
+}
+
+export class AggTerms {
+  field: String;
+  size = 10000;
+}
+
+export class SingleFilter {
+  terms = [];
+}
+
+export class BoolFilter {
+
+}
+
+
+export class QueryWrapper {
+  aggs;
+  filter;
+  query = new Query();
 }
