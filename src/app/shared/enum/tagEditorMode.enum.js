d69ee751b457674db079aa071386be2e743df52f
Denis Yuen <denis.yuen@oicr.on.ca>
2017-06-22 15:46:44 -0400

    CLean up redundant js files

--- src/app/shared/enum/tagEditorMode.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TagEditorMode;
-(function (TagEditorMode) {
-    TagEditorMode[TagEditorMode["Edit"] = 0] = "Edit";
-    TagEditorMode[TagEditorMode["View"] = 1] = "View";
-    TagEditorMode[TagEditorMode["Add"] = 2] = "Add";
-})(TagEditorMode = exports.TagEditorMode || (exports.TagEditorMode = {}));
