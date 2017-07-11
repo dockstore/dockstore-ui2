57ac5313e0691b3ebfe44ca7a9f2cb4aab079bc0
garyluu <garylau.work@gmail.com>
2017-07-11 11:55:25 -0400

    Reverting dockstore.model.ts

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -1,17 +1,16 @@
 export class Dockstore {
 
-  static readonly API_URI = 'http://10.11.9.73:8080';
-  // static readonly API_URI = 'https://staging.dockstore.org:8443';
+  static readonly API_URI = 'http://localhost:8080';
 
-  static readonly GITHUB_CLIENT_ID = '4e52429e7506af0a634c';
+  static readonly GITHUB_CLIENT_ID = 'fill this in';
   static readonly GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
-  static readonly GITHUB_REDIRECT_URI = 'http://10.11.9.73:4200/auth/tokens/github';
+  static readonly GITHUB_REDIRECT_URI = 'http://fill this in:4200/auth/tokens/github';
   static readonly GITHUB_SCOPE = 'user,user:email';
 
   static readonly QUAYIO_AUTH_URL = 'https://quay.io/oauth/authorize';
-  static readonly QUAYIO_REDIRECT_URI = 'http://10.11.9.73:4200/auth/quay';
+  static readonly QUAYIO_REDIRECT_URI = 'http://fill this in:4200/auth/quay';
   static readonly QUAYIO_SCOPE = 'repo:read,user:read';
-  static readonly QUAYIO_CLIENT_ID = 'HVOYCL8Y0JMZ06EV0LX2';
+  static readonly QUAYIO_CLIENT_ID = 'fill this in';
 
   static readonly BITBUCKET_AUTH_URL = 'https://bitbucket.org/site/oauth2/authorize';
   static readonly BITBUCKET_CLIENT_ID = 'fill this in';
