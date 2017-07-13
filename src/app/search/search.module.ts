929cff05c67bc6533914571fd0d408372c6683e1
j2wu <Jennifer.Wu@oicr.on.ca>
2017-07-13 16:47:47 -0400

    updated elasticsearch version from 2.4 to 5.5

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { SearchComponent } from './search.component';
 import { ListentryModule } from '../listentry/listentry.module';
+import { AdvancedsearchComponent } from './advancedsearch/advancedsearch.component';
 @NgModule({
   imports: [
     CommonModule,
     ListentryModule,
-    AccordionModule.forRoot(),
+    AccordionModule.forRoot()
   ]
 })
 export class SearchModule { }
