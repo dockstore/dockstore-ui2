9417d10439a0d203748273410055ee6d757054f9
garyluu <garylau.work@gmail.com>
2017-07-17 16:08:13 -0400

    Add endpoint partially generated from swagger

--- src/app/shared/webservice/workflow-web.service.ts
+++ src/app/shared/webservice/workflow-web.service.ts
@@ -1,13 +1,23 @@
 import { PublishRequest } from './../models/PublishRequest';
-import { RequestMethod, URLSearchParams } from '@angular/http';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { Http, Headers, URLSearchParams } from '@angular/http';
+import { RequestMethod, RequestOptions, RequestOptionsArgs } from '@angular/http';
+import { Response, ResponseContentType } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import * as models from '../model/models';
 import { Dockstore } from './../dockstore.model';
 import { HttpService } from './../http.service';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WorkflowWebService {
-
-    constructor(private httpService: HttpService) { }
+    protected basePath = Dockstore.API_URI;
+    public defaultHeaders: Headers = new Headers();
+    constructor(protected http: Http, private httpService: HttpService) {
+        this.defaultHeaders = this.httpService.getHeader(this.httpService.getDockstoreToken());
+     }
     /**
     *
     * @method
@@ -56,6 +66,69 @@ export class WorkflowWebService {
         const uri = `/workflows/${workflowId}/refresh`;
         const url = Dockstore.API_URI + uri;
         return this.httpService.getAuthResponse(url);
+    }
+
+    /**
+       * Update the workflow with the given workflow.
+       *
+       * @param workflowId Workflow to modify.
+       * @param body Workflow with updated information
+       */
+    public updateWorkflow(workflowId: number, body: models.Workflow, extraHttpRequestParams?: any): Observable<models.Workflow> {
+        return this.updateWorkflowWithHttpInfo(workflowId, body, extraHttpRequestParams)
+            .map((response: Response) => {
+                if (response.status === 204) {
+                    return undefined;
+                } else {
+                    return response.json();
+                }
+            });
+    }
+
+    /**
+   * Update the workflow with the given workflow.
+   *
+   * @param workflowId Workflow to modify.
+   * @param body Workflow with updated information
+   */
+    public updateWorkflowWithHttpInfo(workflowId: number, body: models.Workflow, extraHttpRequestParams?: any): Observable<Response> {
+        const path = this.basePath + `/workflows/${workflowId}`;
+
+        const queryParameters = new URLSearchParams();
+        const headers = new Headers(this.defaultHeaders.toJSON()); // https://github.com/angular/angular/issues/6845
+        // verify required parameter 'workflowId' is not null or undefined
+        if (workflowId === null || workflowId === undefined) {
+            throw new Error('Required parameter workflowId was null or undefined when calling updateWorkflow.');
+        }
+        // verify required parameter 'body' is not null or undefined
+        if (body === null || body === undefined) {
+            throw new Error('Required parameter body was null or undefined when calling updateWorkflow.');
+        }
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+
+        // to determine the Accept header
+        const produces: string[] = [
+            'application/json'
+        ];
+
+        headers.set('Content-Type', 'application/json');
+
+        let requestOptions: RequestOptionsArgs = new RequestOptions({
+            method: RequestMethod.Put,
+            headers: headers,
+            body: body == null ? '' : JSON.stringify(body), // https://github.com/angular/angular/issues/10612
+            search: queryParameters
+        });
+
+        // https://github.com/swagger-api/swagger-codegen/issues/4037
+        if (extraHttpRequestParams) {
+            requestOptions = (<any>Object).assign(requestOptions, extraHttpRequestParams);
+        }
+
+        return this.http.request(path, requestOptions);
     }
 
     /**
