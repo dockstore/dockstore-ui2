d6383ea688c9c42d48eb01ff733b66c741fda0dd
garyluu <garylau.work@gmail.com>
2017-07-17 15:51:02 -0400

    Add default version selection to tools

--- src/app/container/versions/versions.component.ts
+++ src/app/container/versions/versions.component.ts
@@ -1,3 +1,5 @@
+import { ContainersWebService } from './../../shared/webservice/containers-web.service';
+import { ContainerService } from './../../shared/container.service';
 import { StateService } from './../../shared/state.service';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
@@ -14,20 +16,35 @@ import { DockstoreService } from '../../shared/dockstore.service';
 export class VersionsContainerComponent extends Versions implements OnInit {
   verifiedLink: string;
   publicPage: boolean;
+  defaultVersion: string;
   @Input() versions: Array<any>;
   @Input() verifiedSource: Array<any>;
-
+  tool: any;
   setNoOrderCols(): Array<number> {
     return [ 5, 6 ];
   }
   constructor(dockstoreService: DockstoreService,
-              dateService: DateService, private stateService: StateService) {
+    dateService: DateService,
+              private stateService: StateService,
+              private containerService: ContainerService,
+              private containersWebService: ContainersWebService) {
     super(dockstoreService, dateService);
     this.verifiedLink = dateService.getVerifiedLink();
   }
 
   ngOnInit() {
     this.stateService.publicPage.subscribe(publicPage => this.publicPage = publicPage);
+    this.containerService.tool$.subscribe(tool => {
+      this.tool = tool;
+      if (tool) {
+        this.defaultVersion = tool.defaultVersion;
+      }
+    });
+  }
+
+  updateDefaultVersion(newDefaultVersion: string) {
+    this.tool.defaultVersion = newDefaultVersion;
+    this.containersWebService.updateContainer(this.tool.id, this.tool).subscribe(response => this.containerService.setTool(response));
   }
 
   getVerifiedSource(name: string) {
