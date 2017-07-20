e2c03b0f2efe943a9761ee538ec168ef42dc5851
garyluu <garylau.work@gmail.com>
2017-07-20 15:11:43 -0400

    Allow tool default path editing

--- src/app/container/info-tab/info-tab.component.ts
+++ src/app/container/info-tab/info-tab.component.ts
@@ -1,18 +1,54 @@
+import { ContainersService } from './../../shared/swagger/api/containers.service';
+import { StateService } from './../../shared/state.service';
+import { validationPatterns } from './../../shared/validationMessages.model';
+import { InfoTabService } from './info-tab.service';
 import { ContainerService } from './../../shared/container.service';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-tab',
   templateUrl: './info-tab.component.html',
-  styleUrls: ['./info-tab.component.css']
+  styleUrls: ['./info-tab.component.css'],
+  providers: [InfoTabService]
 })
 export class InfoTabComponent implements OnInit {
   @Input() validVersions;
   @Input() defaultVersion;
   tool: any;
-  constructor(private containerService: ContainerService) {}
+  public validationPatterns = validationPatterns;
+  dockerFileEditing: boolean;
+  cwlPathEditing: boolean;
+  wdlPathEditing: boolean;
+  isPublic: boolean;
+  constructor(private containerService: ContainerService, private infoTabService: InfoTabService, private StateService: StateService,
+    private containersService: ContainersService) { }
 
   ngOnInit() {
     this.containerService.tool$.subscribe(tool => this.tool = tool);
+    this.infoTabService.dockerFileEditing$.subscribe(editing => this.dockerFileEditing = editing);
+    this.infoTabService.cwlPathEditing$.subscribe(editing => this.cwlPathEditing = editing);
+    this.infoTabService.wdlPathEditing$.subscribe(editing => this.wdlPathEditing = editing);
+    this.StateService.publicPage.subscribe(publicPage => this.isPublic = publicPage);
+  }
+
+  toggleEditDockerFile() {
+    if (this.dockerFileEditing) {
+      this.containersService.updateContainer(this.tool.id, this.tool).subscribe();
+    }
+    this.infoTabService.setDockerFileEditing(!this.dockerFileEditing);
+  }
+
+  toggleEditCWLPath() {
+    if (this.cwlPathEditing) {
+      this.containersService.updateContainer(this.tool.id, this.tool).subscribe();
+    }
+    this.infoTabService.setCWLPathEditing(!this.cwlPathEditing);
+  }
+
+  toggleEditWDLPath() {
+    if (this.wdlPathEditing) {
+      this.containersService.updateContainer(this.tool.id, this.tool).subscribe();
+    }
+    this.infoTabService.setWDLPathEditing(!this.wdlPathEditing);
   }
 }
