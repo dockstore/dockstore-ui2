6fa5c54c53c9c2114b21ca282484c43e15c37e17
garyluu <garylau.work@gmail.com>
2017-07-21 12:06:42 -0400

    Add default test parameter file editing for info tab

--- src/app/container/info-tab/info-tab.component.ts
+++ src/app/container/info-tab/info-tab.component.ts
@@ -1,3 +1,5 @@
+import { DockstoreTool } from './../../shared/swagger/model/dockstoreTool';
+import { Tool } from './../../shared/swagger/model/tool';
 import { ContainersService } from './../../shared/swagger/api/containers.service';
 import { StateService } from './../../shared/state.service';
 import { validationPatterns } from './../../shared/validationMessages.model';
@@ -13,20 +15,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InfoTabComponent implements OnInit {
   @Input() validVersions;
   @Input() defaultVersion;
-  tool: any;
+  tool: DockstoreTool;
   public validationPatterns = validationPatterns;
   dockerFileEditing: boolean;
   cwlPathEditing: boolean;
   wdlPathEditing: boolean;
+  cwlTestPathEditing: boolean;
+  wdlTestPathEditing: boolean;
   isPublic: boolean;
   constructor(private containerService: ContainerService, private infoTabService: InfoTabService, private stateService: StateService,
-    private containersService: ContainersService) { }
+    private containersService: ContainersService) {
+    }
 
   ngOnInit() {
     this.containerService.tool$.subscribe(tool => this.tool = tool);
     this.infoTabService.dockerFileEditing$.subscribe(editing => this.dockerFileEditing = editing);
     this.infoTabService.cwlPathEditing$.subscribe(editing => this.cwlPathEditing = editing);
     this.infoTabService.wdlPathEditing$.subscribe(editing => this.wdlPathEditing = editing);
+    this.infoTabService.cwlTestPathEditing$.subscribe(editing => this.cwlTestPathEditing = editing);
+    this.infoTabService.wdlTestPathEditing$.subscribe(editing => this.wdlTestPathEditing = editing);
     this.stateService.publicPage.subscribe(publicPage => this.isPublic = publicPage);
   }
 
@@ -50,4 +57,22 @@ export class InfoTabComponent implements OnInit {
     }
     this.infoTabService.setWDLPathEditing(!this.wdlPathEditing);
   }
+
+  toggleEditCWLTestPath() {
+    if (this.cwlTestPathEditing) {
+      this.infoTabService.updateAndRefresh(this.tool);
+    }
+    this.infoTabService.setCWLTestPathEditing(!this.cwlTestPathEditing);
+  }
+  toggleEditWDLTestPath() {
+    if (this.wdlTestPathEditing) {
+      this.infoTabService.updateAndRefresh(this.tool);
+    }
+    this.infoTabService.setWDLTestPathEditing(!this.wdlTestPathEditing);
+  }
+
+  somethingIsBeingEdited(): boolean {
+    return this.dockerFileEditing || this.cwlPathEditing || this.wdlPathEditing || this.cwlTestPathEditing || this.wdlTestPathEditing;
+  }
+
 }
