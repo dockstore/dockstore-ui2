7bab424af2004bc5a5bee071fbef84d013072bf3
Gary Luu <garylau.work@gmail.com>
2017-07-25 10:19:37 -0400

    Feature/workflow stub version editor (#53)
    * Remove old workflow model file
    
    * Fix swaagger generated model files
    
    * Restubbing
    
    * web service additions from swagger API
    
    * Add version editor modal
    
    * Update to newest swagger code gen due to recommendations
    
    * Use swagger api for all workflow endpoint calls
    
    * Fix for when no DAG exists
    
    * Remove console log debug

--- src/app/workflow/view/view.component.ts
+++ src/app/workflow/view/view.component.ts
@@ -1,4 +1,7 @@
-import {AfterViewChecked, AfterViewInit, Component, Input} from '@angular/core';
+import { StateService } from './../../shared/state.service';
+import { VersionModalService } from './../version-modal/version-modal.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { AfterViewChecked, AfterViewInit, Component, Input, ViewChild, OnInit } from '@angular/core';
 
 import { View } from '../../shared/view';
 
@@ -11,14 +14,28 @@ import { WorkflowService } from '../../shared/workflow.service';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewWorkflowComponent extends View implements AfterViewInit {
+export class ViewWorkflowComponent extends View implements OnInit, AfterViewInit {
   @Input() workflowId: string;
   items: any[];
+  isPublic: boolean;
   constructor(private viewService: ViewService,
               private workflowService: WorkflowService,
+              private versionModalService: VersionModalService,
+              private stateService: StateService,
               dateService: DateService) {
     super(dateService);
   }
+
+  showVersionModal() {
+    this.versionModalService.setVersion(this.version);
+    this.workflowService.getTestJson(this.workflowId, this.version.name)
+        .subscribe(items => {
+            this.items = items;
+            this.versionModalService.setTestParameterFiles(this.items);
+          });
+    this.versionModalService.setIsModalShown(true);
+  }
+
   initItems() {
     if (this.version) {
       this.workflowService.getTestJson(this.workflowId, this.version.name)
@@ -32,5 +49,9 @@ export class ViewWorkflowComponent extends View implements AfterViewInit {
   }
   ngAfterViewInit() {
     this.initItems();
+  }
+
+  ngOnInit() {
+    this.stateService.publicPage.subscribe(isPublic => this.isPublic = isPublic);
   }
 }
