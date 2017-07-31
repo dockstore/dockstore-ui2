f5e820deb0c5262fa6b2316f094c410c86e92cbf
j2wu <Jennifer.Wu@oicr.on.ca>
2017-07-31 13:05:41 -0400

    added uncommit file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
-import { SearchModule } from './search/search.module';
-import { Configuration } from './shared/swagger/configuration';
-import { ApiModule } from './shared/swagger/api.module';
+import { SearchService } from './search/search.service';
 import { StateService } from './shared/state.service';
 /* Angular Modules */
 import { NgModule } from '@angular/core';
@@ -68,11 +66,13 @@ import { ToolService } from './shared/tool.service';
 import { TrackLoginService } from './shared/track-login.service';
 import { SponsorsComponent } from './sponsors/sponsors.component';
 import { ToolDetailsComponent } from './tool-details/tool-details.component';
+import { SearchComponent } from './search/search.component';
 import { OrderByModule } from './shared/modules/orderby.module';
 import { StarredEntriesComponent } from './starredentries/starredentries.component';
 import { StarringModule } from './starring/starring.module';
 import { StargazersModule } from './stargazers/stargazers.module';
 import { ListentryModule } from './listentry/listentry.module';
+import { AdvancedSearchComponent } from './search/advancedsearch/advancedsearch.component';
 import { DownloadCLIClientComponent } from './loginComponents/onboarding/downloadcliclient/downloadcliclient.component';
 import { SetupCompleteComponent } from './loginComponents/onboarding/setupcomplete/setupcomplete.component';
 
@@ -82,6 +82,7 @@ import { SetupCompleteComponent } from './loginComponents/onboarding/setupcomple
     SponsorsComponent,
     NavbarComponent,
     HomeComponent,
+    SearchComponent,
     FooterComponent,
     HomeFootNoteComponent,
     ToolDetailsComponent,
@@ -93,6 +94,7 @@ import { SetupCompleteComponent } from './loginComponents/onboarding/setupcomple
     AuthComponent,
     TokensComponent,
     StarredEntriesComponent,
+    AdvancedSearchComponent,
     DownloadCLIClientComponent,
     SetupCompleteComponent
 ],
@@ -118,9 +120,8 @@ import { SetupCompleteComponent } from './loginComponents/onboarding/setupcomple
     ModalModule.forRoot(),
     StargazersModule,
     ListentryModule,
-    MarkdownModule.forRoot(),
-    SearchModule,
-    ApiModule.forConfig(getApiConfig)
+    TagCloudModule,
+    MarkdownModule.forRoot()
   ],
   providers: [
     AuthService,
@@ -148,13 +149,4 @@ import { SetupCompleteComponent } from './loginComponents/onboarding/setupcomple
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
-}
-
-export const apiConfig = new Configuration({
-  accessToken: '',
-  basePath: ''
-});
-
-export function getApiConfig() {
-  return apiConfig;
 }
