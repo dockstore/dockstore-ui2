be8c2312965997bed081e953f32c9b54a8cbb3d0
garyluu <garylau.work@gmail.com>
2017-08-25 11:15:35 -0400

    Docs component test

--- src/app/docs/docs.component.spec.ts
+++ src/app/docs/docs.component.spec.ts
@@ -1,14 +1,17 @@
+import { By } from '@angular/platform-browser';
+import { RouterLinkStubDirective, RouterOutletStubComponent } from './../test/router-stubs';
+import { HeaderModule } from './../shared/modules/header.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
 import { DocsComponent } from './docs.component';
 
 describe('DocsComponent', () => {
   let component: DocsComponent;
   let fixture: ComponentFixture<DocsComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DocsComponent ]
+      declarations: [ DocsComponent, RouterLinkStubDirective, RouterOutletStubComponent ],
+      imports: [HeaderModule]
     })
     .compileComponents();
   }));
@@ -21,5 +24,10 @@ describe('DocsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render subject', () => {
+    const h3 = fixture.debugElement.query(By.css('h3'));
+    expect(h3.nativeElement.innerText).toBe(' Documentation');
   });
 });
