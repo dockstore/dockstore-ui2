5890723915d5ae0c596d429e12692bc0e805c79f
garyluu <garylau.work@gmail.com>
2017-08-30 16:54:10 -0400

    Docs tests and partial login test

---
+++ src/app/login/login.service.spec.ts
@@ -0,0 +1,26 @@
+import { LoginApi } from './login.api';
+import { LoginService } from './login.service';
+import { Configuration } from './../shared/swagger/configuration';
+import { UsersStubService, ConfigurationStub } from './../test/service-stubs';
+import { UsersService } from '../shared/swagger';
+import { AuthStubService } from '../test/service-stubs';
+import { AuthService } from 'ng2-ui-auth/commonjs/auth.service';
+import { TestBed, inject } from '@angular/core/testing';
+
+describe('LoginService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [LoginService,
+                { provide: AuthService, useClass: AuthStubService},
+                { provide: LoginApi, useClass: LoginApi}
+            ],
+        });
+    });
+
+    it('should be created', inject([LoginService], (service: LoginService) => {
+        expect(service).toBeTruthy();
+    }));
+    it('should be able to authenticate', inject([LoginService], (service: LoginService) => {
+        expect(service.authenticate('github')).toBeTruthy();
+    }));
+});
