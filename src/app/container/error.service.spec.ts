59825b703b38169ad1633c15c0ce3d7318c14ba9
garyluu <garylau.work@gmail.com>
2017-08-30 17:03:34 -0400

    Error service test

---
+++ src/app/container/error.service.spec.ts
@@ -0,0 +1,23 @@
+import { ErrorService } from './error.service';
+import { TestBed, inject } from '@angular/core/testing';
+
+describe('ErrorService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ ErrorService ],
+        });
+    });
+
+    it('should be created', inject([ErrorService], (service: ErrorService) => {
+        expect(service).toBeTruthy();
+    }));
+    it('should have multiple docs', inject([ErrorService], (service: ErrorService) => {
+        service.setToolRegisterError({'status': 'status', 'statusText': 'statusText', '_body': '_body'});
+        const errorObj: any = {
+            message: 'The webservice encountered an error trying to create modify.',
+            errorDetails: '[HTTP ' + 'status' + '] ' + 'statusText' + ': ' +
+            '_body'
+        };
+        expect(service.toolError.getValue()).toEqual(errorObj);
+    }));
+});
