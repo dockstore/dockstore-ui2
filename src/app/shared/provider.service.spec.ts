7ca70ba9b12a6281bd25baabd5cf2ac965daca98
garyluu <garylau.work@gmail.com>
2017-08-30 10:31:21 -0400

    Provider service test

---
+++ src/app/shared/provider.service.spec.ts
@@ -0,0 +1,49 @@
+import { register } from 'ts-node/dist';
+import { DockstoreTool } from './swagger/model/dockstoreTool';
+import { ProviderService } from './provider.service';
+import { TestBed, inject } from '@angular/core/testing';
+
+describe('AsdfService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [ProviderService]
+        });
+    });
+
+    it('should be created', inject([ProviderService], (service: ProviderService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('should set up provider', inject([ProviderService], (service: ProviderService) => {
+        const tool: DockstoreTool = {
+            default_cwl_path: '',
+            default_dockerfile_path: '',
+            default_wdl_path: '',
+            gitUrl: 'https://github.com/mr-c/khmer',
+            mode: DockstoreTool.ModeEnum.AUTODETECTQUAYTAGSAUTOMATEDBUILDS,
+            name: '',
+            namespace: '',
+            private_access: false,
+            registry: DockstoreTool.RegistryEnum.QUAYIO,
+            toolname: ''
+        };
+        expect(service.setUpProvider(tool).providerUrl).toBeFalsy();
+        const tool2 = tool;
+        tool2.gitUrl = 'git@github.com:denis-yuen/dockstore-tool-bamstats.git';
+        expect(service.setUpProvider(tool2).providerUrl).toContain('https://github.com/');
+        const tool3 = tool;
+        tool3.gitUrl = 'https://garyluu@bitbucket.org/garyluu/dockstore-tool-md5sum.git';
+        expect(service.setUpProvider(tool3).providerUrl).toContain('https://bitbucket.org/');
+        const tool4 = tool;
+        tool4.gitUrl = 'https://gitlab.com/garyluu/dockstore-tool-md5sum.git';
+        expect(service.setUpProvider(tool3).providerUrl).toContain('https://gitlab.com/');
+        const tool5 = tool;
+        tool5.gitUrl = '';
+        expect(service.setUpProvider(tool3).providerUrl).toBeFalsy();
+        expect(service.setUpProvider(tool3).provider).toBeFalsy();
+        const tool6 = tool;
+        tool5.gitUrl = 'https://someprivateregistry.com/garyluu/dockstore-tool-md5sum.git';
+        expect(service.setUpProvider(tool3).providerUrl).toBeFalsy();
+        expect(service.setUpProvider(tool3).provider).toBeFalsy();
+    }));
+});
