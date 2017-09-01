5db922de0cc837f63af924bbe597d774cf05093b
garyluu <garylau.work@gmail.com>
2017-09-01 10:41:37 -0400

    Partially test date service

---
+++ src/app/shared/date.service.spec.ts
@@ -0,0 +1,29 @@
+import { DateService } from './date.service';
+import * as path from 'path';
+import { DockstoreTool } from './swagger/model/dockstoreTool';
+import { ContainersStubService } from '../test/service-stubs';
+import { ContainersService } from './swagger/api/containers.service';
+import { ImageProviderService } from './image-provider.service';
+import { TestBed, inject } from '@angular/core/testing';
+
+describe('DateService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [DateService]
+        });
+    });
+
+    it('should be created', inject([DateService], (service: DateService) => {
+        expect(service).toBeTruthy();
+    }));
+    it('should be getDateTimeMessage', inject([DateService], (service: DateService) => {
+        expect(service.getDateTimeMessage(1504214211322)).toEqual('Aug. 31 2017 at 17:16:51');
+    }));
+    it('should be getAgoMessage', inject([DateService], (service: DateService) => {
+        expect(service.getAgoMessage(null)).toEqual('n/a');
+        expect(service.getAgoMessage(1498675698000)).toContain(' days ago');
+    }));
+    it('should be getVerifiedLink', inject([DateService], (service: DateService) => {
+        expect(service.getVerifiedLink()).toEqual('/docs/faq#what-is-a-verified-tool-or-workflow-');
+    }));
+});
