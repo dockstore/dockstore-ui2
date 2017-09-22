8421d1e4ac065b43945c5616c44e15ff1e033db4
garyluu <garylau.work@gmail.com>
2017-09-22 12:18:29 -0400

    Read all GitHub organization so workflows can be added for all of them

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -6,7 +6,7 @@ export class Dockstore {
   static readonly GITHUB_CLIENT_ID = 'fill this in';
   static readonly GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
   static readonly GITHUB_REDIRECT_URI = 'http://fill this in/auth/tokens/github';
-  static readonly GITHUB_SCOPE = 'user,user:email';
+  static readonly GITHUB_SCOPE = 'read:org,user,user:email';
 
   static readonly QUAYIO_AUTH_URL = 'https://quay.io/oauth/authorize';
   static readonly QUAYIO_REDIRECT_URI = 'http://fill this in:4200/auth/quay';
