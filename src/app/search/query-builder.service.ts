076790276cca9ced07959e32b2f3c9b7381e2b68
garyluu <garylau.work@gmail.com>
2017-10-04 12:37:54 -0400

    Comment and formatting

--- src/app/search/query-builder.service.ts
+++ src/app/search/query-builder.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import bodybuilder from 'bodybuilder';
+
+/**
+ * This service constructs all the querys and should be only class that interacts with the bodybuilder library.
+ *
+ * @export
+ * @class QueryBuilderService
+ */
 @Injectable()
 export class QueryBuilderService {
 
-constructor() { }
+    constructor() { }
 
-getTagCloudQuery(type: string): string {
-    let body = bodybuilder().size();
-    body = body.query('match', '_type', type);
-    body = body.aggregation('significant_terms', 'description', 'tagcloud', { size: 20 }).build();
-    const toolQuery = JSON.stringify(body, null, 1);
-    return toolQuery;
-}
+    getTagCloudQuery(type: string): string {
+        let body = bodybuilder().size();
+        body = body.query('match', '_type', type);
+        body = body.aggregation('significant_terms', 'description', 'tagcloud', { size: 20 }).build();
+        const toolQuery = JSON.stringify(body, null, 1);
+        return toolQuery;
+    }
 }
