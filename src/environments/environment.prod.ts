9258f697516aba1b9e7fab593cc5d994b894a4c1
garyluu <garylau.work@gmail.com>
2017-10-05 12:59:18 -0400

    Utilize the environment files, added qa environment for Travis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// This is for production only
+
 export const environment = {
-  production: true
+  production: true,
+  HOSTNAME: 'http://localhost fill this in for production',
+  API_PORT: '8080 fill this in for production',
+  UI_PORT: '4200 fill this in for production',
+  UI1_PORT: '9000 fill this in for production',
+
+  GITHUB_CLIENT_ID: 'fill this in for production',
+  QUAYIO_CLIENT_ID: 'fille this in for production',
+  BITBUCKET_CLIENT_ID: 'fill this in for production',
+  GITLAB_CLIENT_ID: 'fill this in for production',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// This is for development, do not commit
+
 export const environment = {
-  production: false
+  production: false,
+  HOSTNAME: 'http://##.##.##.## change this to your own IP',
+  API_PORT: '8080 change this to your own port',
+  UI_PORT: '4200 change this to your own port',
+  UI1_PORT: '9000 change this to your own port',
+
+  GITHUB_CLIENT_ID: 'fill this in with your own',
+  QUAYIO_CLIENT_ID: 'fill this in with your own',
+  BITBUCKET_CLIENT_ID: 'fill this in with your own',
+  GITLAB_CLIENT_ID: 'fill this in with your own',
 };
