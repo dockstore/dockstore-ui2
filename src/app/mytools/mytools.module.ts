2751ed215d011b3e17ac63429a40ac598bac622a
Gary Luu <garylau.work@gmail.com>
2017-10-10 10:19:17 -0400

    Feature/refresh by org (#92)
    * User Id observable
    
    * Add refresh organization for both my-tools and my-workflows
    
    * Tests for non-extended components
    
    * Moved the extended component into only one module
    
    * Disable button when refreshing
    
    * Use develop's swagger.yaml

--- src/app/mytools/mytools.module.ts
+++ src/app/mytools/mytools.module.ts
@@ -1,26 +1,26 @@
-import { RegisterToolService } from './../container/register-tool/register-tool.service';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-/* Bootstrap */
+import { FormsModule } from '@angular/forms';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
-/* Inner Module, Component, Routing, and Service */
+import {
+    RefreshToolOrganizationComponent,
+} from '../container/refresh-tool-organization/refresh-tool-organization.component';
 import { ContainerModule } from '../shared/modules/container.module';
-import { ContainerService } from '../shared/container.service';
 import { HeaderModule } from '../shared/modules/header.module';
+import { RegisterToolComponent } from './../container/register-tool/register-tool.component';
+import { RegisterToolService } from './../container/register-tool/register-tool.service';
 import { MyToolsComponent } from './mytools.component';
 import { mytoolsRouting } from './mytools.routing';
-import { RegisterToolComponent } from './../container/register-tool/register-tool.component';
 
 @NgModule({
   declarations: [
     MyToolsComponent,
-    RegisterToolComponent
+    RegisterToolComponent,
+    RefreshToolOrganizationComponent
   ],
   imports: [
     CommonModule,
--- src/app/myworkflows/myworkflows.module.ts
+++ src/app/myworkflows/myworkflows.module.ts
@@ -1,24 +1,25 @@
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { RegisterWorkflowModalComponent } from './../workflow/register-workflow-modal/register-workflow-modal.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-/* Bootstrap */
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
-/* Module */
+import { HeaderModule } from '../shared/modules/header.module';
 import { WorkflowModule } from '../shared/modules/workflow.module';
+import {
+    RefreshWorkflowOrganizationComponent,
+} from './../workflow/refresh-workflow-organization/refresh-workflow-organization.component';
+import { RegisterWorkflowModalComponent } from './../workflow/register-workflow-modal/register-workflow-modal.component';
 import { MyWorkflowsComponent } from './myworkflows.component';
 import { myworkflowRouting } from './myworkflows.routing';
-import { HeaderModule } from '../shared/modules/header.module';
 
 @NgModule({
   declarations: [
     MyWorkflowsComponent,
-    RegisterWorkflowModalComponent
+    RegisterWorkflowModalComponent,
+    RefreshWorkflowOrganizationComponent
   ],
   imports: [
     CommonModule,
