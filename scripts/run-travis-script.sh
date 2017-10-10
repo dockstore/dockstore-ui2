79fceb7a5afd41814c6cff9e7167a4d06f409523
Denis Yuen <denis.yuen@oicr.on.ca>
2017-10-10 15:40:35 -0400

    ts-node needed as a production dependency

--- scripts/run-travis-script.sh
+++ scripts/run-travis-script.sh
@@ -7,10 +7,12 @@ echo "${TRAVIS_PULL_REQUEST_BRANCH}"
 echo "${TRAVIS_PULL_REQUEST_SHA}"
 
 if [ ${#RUN_PROD} == "true" ]; then
+  npm run-script prebuild
   ng build --prod
   npm shrinkwrap
 else
   # Only run main tests if not prod
+  npm run-script prebuild
   ng build
   ng serve --silent &
   ng test --watch=false --code-coverage
