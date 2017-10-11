7d8b05ad76ba3bc947f7726f8c43d2602def4f8f
Denis Yuen <denis.yuen@oicr.on.ca>
2017-10-11 13:50:15 -0400

    Because the current length of the log is unwieldly

--- scripts/run-travis-script.sh
+++ scripts/run-travis-script.sh
@@ -6,7 +6,7 @@ set -o xtrace
 
 if [ "$RUN_PROD" = true ]; then
   npm run-script prebuild
-  ng build --prod
+  ng build --progress false --prod
   # silly hack needed, take out when angular-tag-cloud-module is updated
   perl -pi -e 's/\^2.4.3/4.3.6/g' node_modules/angular-tag-cloud-module/package.json
   npm shrinkwrap
@@ -14,7 +14,7 @@ else
   # Only run main tests if not prod
   npm run-script prebuild
   ng lint
-  ng build
+  ng build --progress false
   ng serve --silent &
   ng test --watch=false --code-coverage
   cypress run --record --config defaultCommandTimeout=10000
