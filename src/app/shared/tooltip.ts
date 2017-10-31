6229b4ecbf549b492b93dfbc93615abb9ede1a0a
garyluu <garylau.work@gmail.com>
2017-10-31 14:07:15 -0400

    Convert all container.module tooltips to auto placement

--- src/app/shared/tooltip.ts
+++ src/app/shared/tooltip.ts
@@ -1,5 +1,10 @@
+import { TooltipConfig } from 'ngx-bootstrap/tooltip';
 export const Tooltip = {
     'testParameterFile' : 'Relative path to a WDL/CWL Test Parameter File in the Git repository.',
     'workflowPath': 'Path in Git repository to main descriptor file'
 };
+
+export function getTooltipConfig(): TooltipConfig {
+    return Object.assign(new TooltipConfig(), {placement: 'right'});
+  }
 
