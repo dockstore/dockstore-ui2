30e1360d84c6dda76190c93f34911c92a8df47ec
Gary Luu <garylau.work@gmail.com>
2017-11-10 15:23:20 -0500

    Feature/incorrect redirect url handling (#138)
    * Return Observable instead of just null
    
    * Add karma test
    
    * Handle all errors
    
    * Cypress test for invalid IDs and invalid URLs
    
    * Dynamic ID for test

---
+++ src/app/loginComponents/auth/auth.component.spec.ts
@@ -0,0 +1,30 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { TokensStubService, TokenStubService } from './../../test/service-stubs';
+import { TokenService } from '../token.service';
+import { AuthComponent } from './auth.component';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+
+
+describe('AuthComponent', () => {
+  let component: AuthComponent;
+  let fixture: ComponentFixture<AuthComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AuthComponent],
+      imports: [ RouterTestingModule ],
+      providers: [{provide: TokenService, useClass: TokenStubService}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
