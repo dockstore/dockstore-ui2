f8f74a8afc62758784f63a467133fa638a7320b7
Andrew Duncan <andrew.duncan@oicr.on.ca>
2017-11-14 15:53:49 -0500

    Feature/doc redirect (#144)
    * added doc redirecting
    * removed documents

--- src/app/docs/docs.routing.ts
+++ src/app/docs/docs.routing.ts
@@ -17,15 +17,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { DocsComponent } from './docs.component';
-import { MainComponent } from './main/main.component';
-import { PageComponent } from './page/page.component';
 
 const DOC_ROUTES: Routes = [
   {
-    path: '', component: DocsComponent, children: [
-      { path: '', component: MainComponent },
-      { path: ':slug', component: PageComponent }
-    ]
+    path: '**', component: DocsComponent
   }
 ];
 
