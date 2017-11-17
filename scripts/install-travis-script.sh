7674aa700f82a7edd3c55656c7239c2823e7e8a5
Denis Yuen <denis.yuen@oicr.on.ca>
2017-11-17 11:20:53 -0500

    Killing bower for ga4gh/dockstore#1030 (#148)
    * Killing bower for ga4gh/dockstore#1030

--- scripts/install-travis-script.sh
+++ scripts/install-travis-script.sh
@@ -5,14 +5,10 @@ set -o nounset
 set -o xtrace
 
 if [ "$RUN_PROD" = true ]; then
-  npm install -g bower
   npm install --production
-  bower install
   ng version
 else
-  npm install -g bower
   npm install
-  bower install
   ng version
   npm install codecov
 fi
