29b10a84582b8d0ce5aee227ae09d18b3f996190
garyluu <garylau.work@gmail.com>
2018-02-12 12:48:16 -0500

    Add an additional tool to database

--- cypress/integration/ToolsSearch.js
+++ cypress/integration/ToolsSearch.js
@@ -6,11 +6,26 @@ describe('Dockstore tool search page', function() {
   });
 
   describe('Select a tool', function() {
-    it('Should have three tools and no hidden row)', function() {
+    it('Should display the correct url', function() {
+      cy.get('tbody')
+      .children('tr')
+      .find('a')
+      .contains('a')
+      .should('have.attr', 'href', '/containers/quay.io/A2/a')
+      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20a')
+      cy.get('tbody')
+      .children('tr')
+      .find('a')
+      .contains('b3')
+      .should('have.attr', 'href', '/containers/quay.io/A2/b3')
+      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20b3')
+    });
+    
+    it('Should have 4 tools', function() {
       cy
         .get('tbody')
         .children('tr')
-        .should('have.length', 3)
+        .should('have.length', 4)
     });
 
     it('Select dockstore-tool-imports', function() {
