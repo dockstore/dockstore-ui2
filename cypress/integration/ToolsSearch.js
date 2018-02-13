823da8170713b11a05159956ce5ae2777b35e303
garyluu <garylau.work@gmail.com>
2018-02-13 13:50:07 -0500

    Hotfix conflict fix

--- cypress/integration/ToolsSearch.js
+++ cypress/integration/ToolsSearch.js
@@ -6,21 +6,6 @@ describe('Dockstore tool search page', function() {
   });
 
   describe('Select a tool', function() {
-    it('Should display the correct url', function() {
-      cy.get('tbody')
-      .children('tr')
-      .find('a')
-      .contains('a')
-      .should('have.attr', 'href', '/containers/quay.io/A2/a')
-      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20a')
-      cy.get('tbody')
-      .children('tr')
-      .find('a')
-      .contains('b3')
-      .should('have.attr', 'href', '/containers/quay.io/A2/b3')
-      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20b3')
-    });
-    
     it('Should have 4 tools', function() {
       cy
         .get('tbody')
