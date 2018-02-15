7008c63ebc139954691baeafc09c829575a7c45d
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-02-15 09:20:27 -0500

    fixes for tests

--- cypress/integration/ToolsSearch.js
+++ cypress/integration/ToolsSearch.js
@@ -11,16 +11,14 @@ describe('Dockstore tool search page', function() {
       .children('tr')
       .find('a')
       .contains('a')
-      .should('have.attr', 'href', '/containers/quay.io/A2/a')
-      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20a')
+      .should('have.attr', 'href', '/containers/quay.io%20A2%20a')
       cy.get('tbody')
       .children('tr')
       .find('a')
       .contains('b3')
-      .should('have.attr', 'href', '/containers/quay.io/A2/b3')
-      .should('not.have.attr', 'href', '/containers/quay.io%20A2%20b3')
+      .should('have.attr', 'href', '/containers/quay.io%20A2%20b3')
     });
-    
+
     it('Should have 4 tools', function() {
       cy
         .get('tbody')
