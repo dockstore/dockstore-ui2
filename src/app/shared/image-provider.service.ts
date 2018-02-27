886a2481b0b49ef4f45a4ed9d0ab032f0e95f49e
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-02-27 09:43:10 -0500

    fixed failing image provider test

--- src/app/shared/image-provider.service.ts
+++ src/app/shared/image-provider.service.ts
@@ -94,7 +94,7 @@ export class ImageProviderService {
     if (!this.dockerRegistryList) {
       console.log('This should not be necessary');
       this.containersService.getDockerRegistries().subscribe(registryList => {
-        const dockerReg = registryList.find(x => x._enum === tool.registry);
+        const dockerReg = registryList.find(x => x._enum === tool.registry_provider);
         if (dockerReg) {
           return dockerReg.privateOnly === 'true';
         } else {
@@ -102,7 +102,7 @@ export class ImageProviderService {
         }
       });
     } else {
-    const dockerReg = this.dockerRegistryList.find(x => x.enum === tool.registry);
+    const dockerReg = this.dockerRegistryList.find(x => x.enum === tool.registry_provider);
     if (dockerReg) {
       return dockerReg.privateOnly === 'true';
     } else {
