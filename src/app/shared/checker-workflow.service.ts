bc1e034fb4d4197054e83cc036e198e0ff97214b
garyluu <garylau.work@gmail.com>
2018-03-14 16:52:24 -0400

    Tidy up

--- src/app/shared/checker-workflow.service.ts
+++ src/app/shared/checker-workflow.service.ts
@@ -150,7 +150,7 @@ export class CheckerWorkflowService {
      * Returns true if entry is a workflow, returns false if entry is not a workflow (i.e. a tool)
      * @param entry The entry to check
      */
-    private isEntryAWorkflow(entry: Entry) {
+    private isEntryAWorkflow(entry: Entry): boolean {
         if (!entry) {
             return null;
         }
