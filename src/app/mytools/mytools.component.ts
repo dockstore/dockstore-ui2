fd1f1105fb3b1e8bde49fefd00afefcf81fcc1aa
garyluu <garylau.work@gmail.com>
2018-03-16 11:45:06 -0400

    Revert "This will be reverted in the next PR"
    This reverts commit 963ae1cdc6c97e6301a8a5dc3d146fdd324929bf.

--- src/app/mytools/mytools.component.ts
+++ src/app/mytools/mytools.component.ts
@@ -119,6 +119,7 @@ export class MyToolsComponent implements OnInit {
   }
   goToTool(tool: ExtendedDockstoreTool) {
     this.containerService.setTool(tool);
+    // this.router.navigateByUrl('/my-tools/' + tool.tool_path);
   }
 
   setIsFirstOpen() {
