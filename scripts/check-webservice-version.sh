20b1fb1c0b6fd12287d84ad59933c7ccd7b62ed6
garyluu <garylau.work@gmail.com>
2018-04-18 13:14:42 -0400

    Update

--- scripts/check-webservice-version.sh
+++ scripts/check-webservice-version.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
 LATEST_WEBSERVICE=$(curl --silent "https://api.github.com/repos/ga4gh/dockstore/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 echo $LATEST_WEBSERVICE
 if [ "$LATEST_WEBSERVICE" == "$WEBSERVICE_VERSION" ]
