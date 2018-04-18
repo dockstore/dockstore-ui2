a0a3f93dd6d6ba85189f3d1b4a24d3f121e0a63e
garyluu <garylau.work@gmail.com>
2018-04-18 12:09:27 -0400

    Echo when provided version is not the latest

---
+++ scripts/check-webservice-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+LATEST_WEBSERVICE=$(curl --silent "https://api.github.com/repos/ga4gh/dockstore/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+echo $LATEST_WEBSERVICE
+if [ "$LATEST_WEBSERVICE" == "$WEBSERVICE_VERSION" ]
+then
+  echo "The supplied webservice version is the latest"
+else 
+  echo "The supplied webservice version is not the latest."
+  echo "Latest:" $LATEST_WEBSERVICE
+  echo "Current:" $WEBSERVICE_VERSION
+fi
