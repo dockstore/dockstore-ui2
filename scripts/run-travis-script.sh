bf86aa7cfdaf77ea7fd654ed4706599c6fcbf3c4
garyluu <garylau.work@gmail.com>
2018-04-19 12:10:43 -0400

    Extend npm scripts, run scripts in Travis

--- scripts/run-travis-script.sh
+++ scripts/run-travis-script.sh
@@ -5,13 +5,11 @@ set -o nounset
 set -o xtrace
 
 if [ "$RUN_PROD" = true ]; then
-  npm run-script prebuild
-  ng build --progress false --prod
+  npm run build.prod
 else
   # Only run main tests if not prod
-  npm run-script prebuild
   ng lint
-  ng build --progress false
+  npm run build
   ng serve --silent &
   ng test --watch=false --code-coverage --browsers ChromeHeadless
   cypress run --record --config defaultCommandTimeout=10000
