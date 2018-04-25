4d6b579f481491c24048a776da22e4a8f5652912
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-04-25 10:26:24 -0400

    show error if there is a mismatch between the webservice used to build the UI and the one it is talking to

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { GA4GHService } from '../../../src/app/shared/swagger/api/gA4GH.service';
+import { Metadata } from './../shared/swagger/model/metadata';
+import { versions } from '../footer/versions';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -9,10 +13,32 @@ import { environment } from '../../environments/environment';
 })
 export class BannerComponent implements OnInit {
   showBanner: boolean;
-  constructor() { }
+  mismatchedVersion: boolean = false;
+  versionFromAPI: string;
+  versionBuiltWith: string;
+
+  constructor(private gA4GHService: GA4GHService) { }
 
   ngOnInit() {
     this.showBanner = environment.staging;
+
+    this.gA4GHService.metadataGet()
+      .subscribe(
+        (metadata: Metadata) => {
+          if (metadata.hasOwnProperty('version')) {
+            this.versionFromAPI = metadata['version'];
+            this.versionBuiltWith = versions.version;
+            if (this.versionFromAPI !== this.versionBuiltWith) {
+              this.mismatchedVersion = true;
+            }
+          } else {
+            throw new Error('Version undefined');
+          }
+        }, (error: HttpErrorResponse) => {
+          console.log(error);
+          this.mismatchedVersion = true;
+        }
+      );
   }
 
 }
