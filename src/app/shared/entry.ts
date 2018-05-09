e211b7ad083e6c2b6c1046f4551fc6fa7bdfdd49
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-05-09 15:10:33 -0400

    fix for failing test

--- src/app/shared/entry.ts
+++ src/app/shared/entry.ts
@@ -200,7 +200,7 @@ export abstract class Entry implements OnInit, OnDestroy, AfterViewInit {
 
     // If no url tag or default version, select first element in the dropdown
     if (useFirstTag && versions.length > 0) {
-      selectedVersion = versions[0];
+      selectedVersion = versions[versions.length - 1];
     }
     return selectedVersion;
   }
