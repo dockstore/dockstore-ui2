0c4c16632c9c69c80a28a5c1f07cd31630119042
garyluu <garylau.work@gmail.com>
2018-05-24 17:18:38 -0400

    Test and fix other tests

--- src/app/starring/starring.component.ts
+++ src/app/starring/starring.component.ts
@@ -85,7 +85,7 @@ export class StarringComponent implements OnInit {
   }
 
   calculateRate(starredUsers: User[]): boolean {
-    if (!this.user) {
+    if (!this.user || !starredUsers) {
       return false;
     } else {
       let matchingUser: User;
