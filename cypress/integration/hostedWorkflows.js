9df548e9ee62b64a361f89579f905c61ce6645f3
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-05-24 10:53:36 -0400

    added some comments

--- cypress/integration/hostedWorkflows.js
+++ cypress/integration/hostedWorkflows.js
@@ -8,49 +8,56 @@ describe('Dockstore my workflows', function() {
     // Ensure tabs are correct for the hosted workflow, try adding a version
     describe('Should be able to register a hosted workflow and add files to it', function() {
       it('Register the workflow', function() {
-          cy.wait(500)
-          cy
-              .contains('dockstore.org')
-              .parent()
-              .parent()
-              .should('be.visible')
-              .click()
-          cy
-              .get('accordion')
-              .contains('hosted-workflow')
-              .click()
-          // Check content of the info tab
-          cy
-              .contains('Mode: HOSTED')
+        // Select the hosted workflow
+        cy.wait(500)
+        cy
+          .contains('dockstore.org')
+          .parent()
+          .parent()
+          .should('be.visible')
+          .click()
+        cy
+          .get('accordion')
+          .contains('hosted-workflow')
+          .click()
 
-          // Check content of the version tab
-          cy
-              .get('.nav-link')
-              .contains('Versions')
-              .parent()
-              .click()
-              .get('table > tbody')
-              .find('tr')
-              .should('have.length', 2)
-          // Add a new version
-          cy
-              .get('.nav-link')
-              .contains('Files')
-              .parent()
-              .click()
-          cy
-              .get('#deleteVersionButton')
-              .should('be.disabled')
-          cy
-              .get('#editFilesButton')
-              .click()
-          cy
-              .contains('Add File')
-              .click()
+        // Check content of the info tab
+        cy
+          .contains('Mode: HOSTED')
 
-          // Versions tab should include new version
-          // Should be able to publish and unpublish
-          // Add a new version with a test parameter file
+        // Check content of the version tab
+        cy
+          .get('.nav-link')
+          .contains('Versions')
+          .parent()
+          .click()
+          .get('table > tbody')
+          .find('tr')
+          .should('have.length', 2)
+
+        // Add a new version
+        cy
+          .get('.nav-link')
+          .contains('Files')
+          .parent()
+          .click()
+        cy
+          .get('#deleteVersionButton')
+          .should('be.disabled')
+        cy
+          .get('#editFilesButton')
+          .click()
+        cy
+          .contains('Add File')
+          .click()
+
+        cy
+          .get('.ace_text-input')
+          .type('hello')
+
+        // Versions tab should include new version
+        // Should be able to publish and unpublish
+        // Add a new version with a test parameter file
       });
     });
 
