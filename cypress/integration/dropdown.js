48cc83f35a76ed9bbe0cbbcbf2adb0fb628b7fe3
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-06-04 15:00:00 -0400

    wait for gitlab

--- cypress/integration/dropdown.js
+++ cypress/integration/dropdown.js
@@ -59,6 +59,8 @@ describe('Dropdown test', function() {
             cy.visit(String(global.baseUrl) + '/auth/gitlab.com?code=somefakeid', {'failOnStatusCode': false}).then((resp) => {
                 expect(resp.status).to.eq('')
             })
+            // TODO: Gitlab is being very slow, hopefully one day we can remove this
+            cy.wait(10000);
             everythingOk();
             cy.visit(String(global.baseUrl) + '/auth/bitbucket.org?code=somefakeid', {'failOnStatusCode': false}).then((resp) => {
                 expect(resp.status).to.eq('')
