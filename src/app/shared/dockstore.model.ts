0a0f10520876978637544ca277079a008ab14578
Denis Yuen <denis.yuen@oicr.on.ca>
2018-06-14 14:41:32 -0400

    Update dockstore.model.ts

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2017 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 import { Provider } from './enum/provider.enum';
 
 // If you add new properties to this class, you need to also update
@@ -5,7 +21,7 @@ import { Provider } from './enum/provider.enum';
 
 export class Dockstore {
   // Please fill in HOSTNAME with your address
-  static readonly HOSTNAME = 'http://10.11.8.80';
+  static readonly HOSTNAME = 'http://localhost';
   static readonly API_PORT = '8080';
   static readonly UI_PORT = '4200';
 
@@ -17,21 +33,22 @@ export class Dockstore {
   static readonly DNASTACK_IMPORT_URL= 'https://app.dnastack.com/#/app/workflow/import/dockstore';
   static readonly FIRECLOUD_IMPORT_URL= 'https://portal.firecloud.org/#import/dockstore';
 
-  static readonly GITHUB_CLIENT_ID = 'b2877aac203be39fc6b0';
+  static readonly GITHUB_CLIENT_ID = 'fill_this_in';
   static readonly GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
+
   static readonly GITHUB_REDIRECT_URI = Dockstore.LOCAL_URI + '/auth/' + Provider.GITHUB;
   static readonly GITHUB_SCOPE = 'read:org,user:email';
 
   static readonly QUAYIO_AUTH_URL = 'https://quay.io/oauth/authorize';
   static readonly QUAYIO_REDIRECT_URI = Dockstore.LOCAL_URI + '/auth/' + Provider.QUAY;
   static readonly QUAYIO_SCOPE = 'repo:read,user:read';
-  static readonly QUAYIO_CLIENT_ID = '80KHN1Y3LEOWWL80MQSK';
+  static readonly QUAYIO_CLIENT_ID = 'fill_this_in';
 
   static readonly BITBUCKET_AUTH_URL = 'https://bitbucket.org/site/oauth2/authorize';
-  static readonly BITBUCKET_CLIENT_ID = 'fill this in';
+  static readonly BITBUCKET_CLIENT_ID = 'fill_this_in';
 
   static readonly GITLAB_AUTH_URL = 'https://gitlab.com/oauth/authorize';
-  static readonly GITLAB_CLIENT_ID = 'fe7f5e23ca3ed43b8ec2e26f11e9d3cadb7fbd43f39cc851ba6ed850803d9fe9';
+  static readonly GITLAB_CLIENT_ID = 'fill_this_in';
   static readonly GITLAB_REDIRECT_URI = Dockstore.LOCAL_URI + '/auth/' + Provider.GITLAB;
 
   static readonly CWL_VISUALIZER_URI = 'https://view.commonwl.org';
