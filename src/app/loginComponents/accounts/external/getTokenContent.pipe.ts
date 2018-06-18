eb2e44681edd254293382cd0626f47ee14e24046
garyluu <garylau.work@gmail.com>
2018-06-18 12:53:31 -0400

    Add comments

--- src/app/loginComponents/accounts/external/getTokenContent.pipe.ts
+++ src/app/loginComponents/accounts/external/getTokenContent.pipe.ts
@@ -6,6 +6,15 @@ import { Token } from '../../../shared/swagger';
   name: 'getTokenContent'
 })
 export class GetTokenContentPipe implements PipeTransform {
+  /**
+   * Given the token source ('github.com') and the users current list of tokens,
+   * get the token content associated with that source.
+   *
+   * @param {string} source  The token source ('github.com', etc)
+   * @param {Array<Token>} tokens  The user's current list of tokens
+   * @returns {string}  The actual access token to the 3rd party site (some random string of characters)
+   * @memberof GetTokenContentPipe
+   */
   transform(source: string, tokens: Array<Token>): string {
     const tokenFound: Token = tokens.find(token => token.tokenSource === source);
     if (tokenFound) {
--- src/app/loginComponents/accounts/external/getTokenUsername.pipe.ts
+++ src/app/loginComponents/accounts/external/getTokenUsername.pipe.ts
@@ -6,6 +6,15 @@ import { Token } from './../../../shared/swagger/model/token';
   name: 'getTokenUsername'
 })
 export class GetTokenUsernamePipe implements PipeTransform {
+  /**
+   * Given the token source ('github.com') and the users current list of tokens,
+   * get the token's username associated with that source.
+   *
+   * @param {string} source  The token source ('github.com', etc)
+   * @param {Array<Token>} tokens  The user's current list of tokens
+   * @returns {string}  The username extracted using that token in a 3rd party site
+   * @memberof GetTokenUsernamePipe
+   */
   transform(source: string, tokens: Array<Token>): string {
     const tokenFound: Token = tokens.find(token => token.tokenSource === source);
     if (tokenFound) {
