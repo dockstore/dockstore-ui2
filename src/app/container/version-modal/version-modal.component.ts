fcae8f4f4b9172c271567516296f6ce3eabd531a
garyluu <garylau.work@gmail.com>
2018-06-20 16:32:18 -0400

    New codegen swapped parameters...good thing we have tests for this

--- src/app/container/version-modal/version-modal.component.ts
+++ src/app/container/version-modal/version-modal.component.ts
@@ -115,7 +115,6 @@ export class VersionModalComponent implements OnInit, AfterViewChecked {
     this.stateService.setRefreshMessage(message + '...');
     const id = this.tool.id;
     const tagName = this.version.name;
-
     // Store the unsaved test files if valid and exist
     if (this.unsavedTestCWLFile.length > 0) {
       this.addTestParameterFile(DescriptorType.CWL);
@@ -128,7 +127,7 @@ export class VersionModalComponent implements OnInit, AfterViewChecked {
     const CWL = 'CWL';
     const WDL = 'WDL';
     if (newCWL && newCWL.length > 0) {
-      this.containersService.addTestParameterFiles(id, newCWL, CWL, null, tagName).subscribe(response => {},
+      this.containersService.addTestParameterFiles(id, newCWL, CWL, tagName).subscribe(response => {},
         err => this.refreshService.handleError(message, err) );
     }
     const missingCWL = this.savedCWLTestParameterFilePaths.filter(x => !this.unsavedCWLTestParameterFilePaths.includes(x));
@@ -138,7 +137,7 @@ export class VersionModalComponent implements OnInit, AfterViewChecked {
     }
     const newWDL = this.unsavedWDLTestParameterFilePaths.filter(x => !this.savedWDLTestParameterFilePaths.includes(x));
     if (newWDL && newWDL.length > 0) {
-      this.containersService.addTestParameterFiles(id, newWDL, WDL, null, tagName).subscribe(response => {},
+      this.containersService.addTestParameterFiles(id, newWDL, WDL, tagName).subscribe(response => {},
         err => this.refreshService.handleError(message, err) );
     }
     const missingWDL = this.savedWDLTestParameterFilePaths.filter(x => !this.unsavedWDLTestParameterFilePaths.includes(x));
