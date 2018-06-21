aa11a7b0de8ca3f9e57a965d129b29af63302036
garyluu <garylau.work@gmail.com>
2018-06-21 13:07:37 -0400

    Add commit IDs and links to tool/workflows

---
+++ src/app/shared/entry/commit-url.pipe.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CommitUrlPipe } from './commit-url.pipe';
+
+describe('Pipe: CommitUrle', () => {
+  it('create an instance', () => {
+    const pipe = new CommitUrlPipe();
+    expect(pipe).toBeTruthy();
+    expect(pipe.transform('868a1c6b02344dd679eb0548889de03e6affef99', 'https://github.com/garyluu/example_cwl_workflow'))
+      .toBe('https://github.com/garyluu/example_cwl_workflow/tree/868a1c6b02344dd679eb0548889de03e6affef99');
+  });
+});
