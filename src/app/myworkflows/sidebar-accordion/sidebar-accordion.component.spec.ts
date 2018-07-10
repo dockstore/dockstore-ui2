000c1c7c94652af5f5aedf8beb38120b80987ec6
garyluu <garylau.work@gmail.com>
2018-07-10 12:28:11 -0400

    Fix ng test

--- src/app/myworkflows/sidebar-accordion/sidebar-accordion.component.spec.ts
+++ src/app/myworkflows/sidebar-accordion/sidebar-accordion.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SidebarAccordionComponent } from './sidebar-accordion.component';
-import {
-  RefreshWorkflowOrganizationComponent
-} from './../../workflow/refresh-workflow-organization/refresh-workflow-organization.component';
-import { RegisterWorkflowModalStubService } from './../../test/service-stubs';
-import { RegisterWorkflowModalService } from './../../workflow/register-workflow-modal/register-workflow-modal.service';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { WorkflowService } from '../../shared/workflow.service';
+import { RegisterWorkflowModalStubService, WorkflowStubService } from './../../test/service-stubs';
+import {
+  RefreshWorkflowOrganizationComponent,
+} from './../../workflow/refresh-workflow-organization/refresh-workflow-organization.component';
+import { RegisterWorkflowModalService } from './../../workflow/register-workflow-modal/register-workflow-modal.service';
+import { SidebarAccordionComponent } from './sidebar-accordion.component';
 
 describe('SidebarAccordionComponent', () => {
   let component: SidebarAccordionComponent;
@@ -17,9 +19,10 @@ describe('SidebarAccordionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SidebarAccordionComponent, RefreshWorkflowOrganizationComponent ],
-      imports: [ AccordionModule.forRoot(), ModalModule.forRoot(), TabsModule.forRoot() ],
+      imports: [ AccordionModule.forRoot(), ModalModule.forRoot(), TabsModule.forRoot(), RouterTestingModule],
       providers: [
-        { provide: RegisterWorkflowModalService, useClass: RegisterWorkflowModalStubService }
+        { provide: RegisterWorkflowModalService, useClass: RegisterWorkflowModalStubService },
+        { provide: WorkflowService, useClass: WorkflowStubService }
       ]
     })
     .compileComponents();
