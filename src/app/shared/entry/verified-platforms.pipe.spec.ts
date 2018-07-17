2c4b6a9f076fdcd7fa5799516d5672e269c4092f
garyluu <garylau.work@gmail.com>
2018-07-17 14:33:19 -0400

    Fix

--- src/app/shared/entry/verified-platforms.pipe.spec.ts
+++ src/app/shared/entry/verified-platforms.pipe.spec.ts
@@ -1,7 +1,7 @@
 import { testSourceFiles } from '../../test/mocked-objects';
 import { VerifiedPlatformsPipe } from './verified-platforms.pipe';
 
-describe('Pipe: VerifiedPlatformse', () => {
+describe('Pipe: VerifiedPlatforms', () => {
   it('create an instance', () => {
     const pipe = new VerifiedPlatformsPipe();
     expect(pipe.transform(testSourceFiles)).toEqual('Dockstore CLI');
