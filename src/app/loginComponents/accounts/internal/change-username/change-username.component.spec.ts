a94fddb6fb9f91980bcd0c333630fafa6366bb83
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-08-08 14:48:55 -0400

    fixed failing ng test

--- src/app/loginComponents/accounts/internal/change-username/change-username.component.spec.ts
+++ src/app/loginComponents/accounts/internal/change-username/change-username.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChangeUsernameComponent } from './change-username.component';
 
+import { UserService } from '../../../../loginComponents/user.service';
+import { UsersService } from './../../../../shared/swagger/api/users.service';
+import { UsersStubService, UserStubService } from './../../../../test/service-stubs';
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatProgressSpinnerModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
 describe('ChangeUsernameComponent', () => {
   let component: ChangeUsernameComponent;
   let fixture: ComponentFixture<ChangeUsernameComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChangeUsernameComponent ]
+      declarations: [ ChangeUsernameComponent ],
+      imports: [
+        MatIconModule,
+        MatButtonModule,
+        MatTooltipModule,
+        MatProgressSpinnerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: UserService, useClass: UserStubService },
+        { provide: UsersService, useClass: UsersStubService }
+      ]
     })
     .compileComponents();
   }));
