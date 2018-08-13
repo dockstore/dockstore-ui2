eb33addb6541d7dc41ead21f16c3ea2600f13999
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-08-13 15:27:33 -0400

    fix ng test

--- src/app/loginComponents/accounts/controls/controls.component.spec.ts
+++ src/app/loginComponents/accounts/controls/controls.component.spec.ts
@@ -1,9 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CustomMaterialModule } from '../../../shared/modules/material.module';
 import { ControlsComponent } from './controls.component';
+import { ChangeUsernameComponent } from '../internal/change-username/change-username.component';
 import { UserService } from '../../user.service';
-import { UserStubService, UsersStubService } from '../../../test/service-stubs';
+import { RefreshService } from './../../../shared/refresh.service';
+import { UserStubService, UsersStubService, RefreshStubService } from '../../../test/service-stubs';
 import { UsersService } from '../../../shared/swagger';
 
 describe('ControlsComponent', () => {
@@ -12,10 +15,11 @@ describe('ControlsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ControlsComponent ],
-      imports: [CustomMaterialModule],
+      declarations: [ ControlsComponent, ChangeUsernameComponent ],
+      imports: [CustomMaterialModule, BrowserAnimationsModule],
       providers: [{provide: UserService, useClass: UserStubService},
-      {provide: UsersService, useClass: UsersStubService}
+      {provide: UsersService, useClass: UsersStubService},
+      { provide: RefreshService, useClass: RefreshStubService }
     ]
     })
     .compileComponents();
