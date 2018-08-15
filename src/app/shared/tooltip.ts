4975dfc0ebb06e7def1bffc5619674526335347b
garyluu <garylau.work@gmail.com>
2018-08-15 16:07:24 -0400

    PR changes

--- src/app/shared/tooltip.ts
+++ src/app/shared/tooltip.ts
@@ -4,7 +4,8 @@ export const Tooltip = {
     'workflowPath': 'Path in Git repository to main descriptor file',
     'defaultVersionUser': 'The branch that the tool/workflow author intends others to use',
     'defaultVersionAuthor': 'Set default version. The default version denotes the version of the tool/workflow you intend others to use. ' +
-    'The default version determines what is displayed in the Info tab (Description, Launch With, etc)'
+    'The default version determines what is displayed in the Info tab (Description, Launch With, etc)',
+    'workflowName': 'Name to distinguish between multiple workflows within the same repository'
 };
 
 export function getTooltipConfig(): TooltipConfig {
