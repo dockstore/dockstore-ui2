e36ba3e7b7c7e54fc8251c552413aac504423e6f
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-08-16 13:00:49 -0400

    added fixed register entry button, no longer have one for each org and the main one after the accordion

--- src/app/myworkflows/sidebar-accordion/sidebar-accordion.component.ts
+++ src/app/myworkflows/sidebar-accordion/sidebar-accordion.component.ts
@@ -2,9 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Observable } from 'rxjs';
 
-import { RegisterWorkflowModalComponent } from '../../workflow/register-workflow-modal/register-workflow-modal.component';
 import { WorkflowService } from './../../shared/workflow.service';
-import { RegisterWorkflowModalService } from './../../workflow/register-workflow-modal/register-workflow-modal.service';
 
 @Component({
   selector: 'app-sidebar-accordion',
@@ -18,18 +16,10 @@ export class SidebarAccordionComponent implements OnInit {
   public workflowId$: Observable<number>;
   activeTab = 0;
 
-  constructor(private registerWorkflowModalService: RegisterWorkflowModalService, private workflowService: WorkflowService,
+  constructor(private workflowService: WorkflowService,
     public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.workflowId$ = this.workflowService.workflowId$;
-  }
-
-  setRegisterEntryModalInfo(gitURL: string): void {
-    this.registerWorkflowModalService.setWorkflowRepository(gitURL);
-  }
-
-  showRegisterEntryModal(): void {
-    const dialogRef = this.dialog.open(RegisterWorkflowModalComponent, {width: '600px'});
   }
 }
