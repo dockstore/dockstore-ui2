d8e40e0f3b376a6156158bbdee4bd06fad19f451
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-08-27 16:35:32 -0400

    revert to old ways

--- src/app/shared/launch.service.ts
+++ src/app/shared/launch.service.ts
@@ -109,8 +109,7 @@ export abstract class LaunchService {
       const outputFile = `-O Dockstore.json`;
       const encodedID = encodeURIComponent(`#workflow/${ workflowPath }`);
       const encodedVersion = encodeURIComponent(`${ versionName }`);
-      let url = `${Dockstore.API_URI}${ga4ghPath}/tools/${encodedID}/versions/${encodedVersion}/${urlType}/descriptor/${filePath}`;
-      url = decodeURIComponent(url);
+      const url = `${Dockstore.API_URI}${ga4ghPath}/tools/${encodedID}/versions/${encodedVersion}/${urlType}/descriptor/${filePath}`;
       return `${prefix}' ${url} ${outputFile}`;
     }
 }
--- src/app/workflow/launch/workflow-launch.service.spec.ts
+++ src/app/workflow/launch/workflow-launch.service.spec.ts
@@ -65,15 +65,15 @@ describe('WorkflowLaunchService', () => {
   it('should get the wget test parameter file command', inject([WorkflowLaunchService], (service: WorkflowLaunchService) => {
     expect(service.getTestJsonString('github.com/garyluu/example_cwl_workflow', 'v1.0', 'cwl', 'test.json'))
       .toBe(`$ wget --header='Accept: text/plain' ` +
-      `${Dockstore.API_URI}/api/ga4gh/v2/tools/#workflow/github.com/garyluu/example_cwl_workflow/versions/v1.0/PLAIN_CWL/descriptor/test.json ` +
+      `${Dockstore.API_URI}/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2Fgaryluu%2Fexample_cwl_workflow/versions/v1.0/PLAIN_CWL/descriptor/test.json ` +
       `-O Dockstore.json`);
     expect(service.getTestJsonString('github.com/garyluu/example_cwl_workflow', 'v1.0', 'wdl', 'test.json'))
       .toBe(`$ wget --header='Accept: text/plain' ` +
-      `${Dockstore.API_URI}/api/ga4gh/v2/tools/#workflow/github.com/garyluu/example_cwl_workflow/versions/v1.0/PLAIN_WDL/descriptor/test.json ` +
+      `${Dockstore.API_URI}/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2Fgaryluu%2Fexample_cwl_workflow/versions/v1.0/PLAIN_WDL/descriptor/test.json ` +
       `-O Dockstore.json`);
     expect(service.getTestJsonString('github.com/garyluu/example_cwl_workflow', 'v1.0', 'nfl', 'test.json'))
       .toBe(`$ wget --header='Accept: text/plain' ` +
-      `${Dockstore.API_URI}/api/ga4gh/v2/tools/#workflow/github.com/garyluu/example_cwl_workflow/versions/v1.0/PLAIN_NFL/descriptor/test.json ` +
+      `${Dockstore.API_URI}/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2Fgaryluu%2Fexample_cwl_workflow/versions/v1.0/PLAIN_NFL/descriptor/test.json ` +
       `-O Dockstore.json`);
     expect(service.getTestJsonString('github.com/garyluu/example_cwl_workflow', 'v1.0', 'potato', 'test.json'))
       .toBe(null);
