b26861b72f39b92a1e39673e0d6840c2ff63fd34
Gary Luu <garylau.work@gmail.com>
2018-08-27 13:14:47 -0400

    Feature/1743/user fix (#426)
    * Remove user when logging out
    
    * Fix stepper
    
    * Attempt to fix test

--- src/app/loginComponents/onboarding/onboarding.component.ts
+++ src/app/loginComponents/onboarding/onboarding.component.ts
@@ -1,24 +1,26 @@
-import { TokenService } from '../token.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Subject } from 'rxjs';
+import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
+
 import { ExtendedUserData, User, UsersService } from '../../shared/swagger';
-import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../token.service';
 import { UserService } from '../user.service';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html'
 })
-export class OnboardingComponent implements OnInit {
+export class OnboardingComponent implements OnInit, OnDestroy {
   public tokenSetComplete;
   protected ngUnsubscribe: Subject<{}> = new Subject();
   extendedUser: ExtendedUserData;
   user: User;
+  ready = false;
   constructor(private userService: UserService, private usersService: UsersService, private tokenService: TokenService) {
   }
   ngOnInit() {
     localStorage.setItem('page', '/onboarding');
-    this.tokenService.tokens$.subscribe(
+    this.tokenService.tokens$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(
       tokens => {
         if (tokens) {
           const tokenStatusSet = this.tokenService.getUserTokenStatusSet(tokens);
@@ -28,7 +30,21 @@ export class OnboardingComponent implements OnInit {
         }
       }
     );
-    this.userService.user$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(user => this.user = user);
-    this.userService.extendedUser$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(extendedUser => this.extendedUser = extendedUser);
+    combineLatest(this.userService.user$, this.userService.extendedUser$).pipe(
+      distinctUntilChanged(), takeUntil(this.ngUnsubscribe)).subscribe(
+      ([user, extendedUser]) => {
+        this.user = user;
+        this.extendedUser = extendedUser;
+        if (user && extendedUser) {
+          this.ready = true;
+        }
+      }, error => {
+        console.error('Error combining user$ and extendedUser$.  This should never happen: ' + error);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
