b0bee8a62bd212e643ff4d7f749c44c474674da9
garyluu <garylau.work@gmail.com>
2018-09-07 11:54:58 -0400

    Fix for table tool content called twice

--- src/app/workflow/tool-tab/tool-tab.component.ts
+++ src/app/workflow/tool-tab/tool-tab.component.ts
@@ -13,51 +13,60 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-import { Component, Input, OnInit } from '@angular/core';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { Component, Input } from '@angular/core';
+import { first } from 'rxjs/operators';
 
+import { EntryTab } from '../../shared/entry/entry-tab';
+import { WorkflowVersion } from '../../shared/swagger';
 import { WorkflowsService } from './../../shared/swagger/api/workflows.service';
 import { Workflow } from './../../shared/swagger/model/workflow';
 import { WorkflowService } from './../../shared/workflow.service';
-import { EntryTab } from '../../shared/entry/entry-tab';
-import { WorkflowVersion } from '../../shared/swagger';
 
 @Component({
   selector: 'app-tool-tab',
   templateUrl: './tool-tab.component.html',
   styleUrls: ['./tool-tab.component.css']
 })
-export class ToolTabComponent extends EntryTab implements OnInit {
+export class ToolTabComponent extends EntryTab {
   workflow: Workflow;
-  toolsContent: any;
+  toolsContent: string = null;
   _selectedVersion: WorkflowVersion;
   @Input() set selectedVersion(value: WorkflowVersion) {
     if (value != null) {
-      this._selectedVersion = value;
-      this.onChange();
+      this.workflowService.workflow$.pipe(first()).subscribe((workflow: Workflow) => {
+        this.workflow = workflow;
+        if (workflow) {
+          this.updateTableToolContent(workflow.id, value.id);
+        } else {
+          console.error('Should not be able to select version without a workflow');
+        }
+      }, error => {
+        console.error('Something has gone terribly wrong with the workflow$ observable.');
+      });
+    } else {
+      this.toolsContent = null;
     }
   }
   constructor(private workflowService: WorkflowService, private workflowsService: WorkflowsService) {
     super();
   }
 
-  ngOnInit() {
-    this.workflowService.workflow$.pipe(distinctUntilChanged()).subscribe(workflow => {
-      if (workflow) {
-        this.workflow = workflow;
-        if (workflow.workflowVersions) {
-          this.onChange();
-        }
-      }
-    });
-  }
-
-  onChange() {
-    if (this._selectedVersion && this.workflow) {
-      this.workflowsService.getTableToolContent(this.workflow.id, this._selectedVersion.id).subscribe(
+  /**
+   * Update the table tool contents for the current workflow and workflow version
+   *
+   * @param {number} workflowId  The workflow Id
+   * @param {number} versionId   The workflowVersion Id
+   * @memberof ToolTabComponent
+   */
+  updateTableToolContent(workflowId: number, versionId: number): void {
+    if (workflowId && versionId) {
+      this.workflowsService.getTableToolContent(workflowId, versionId).subscribe(
         (toolContent) => {
           this.toolsContent = toolContent;
-        }, error => this.toolsContent = null);
+        }, error => {
+          console.log('Could not retrieve table tool content');
+          this.toolsContent = null;
+        });
     } else {
       this.toolsContent = null;
     }
