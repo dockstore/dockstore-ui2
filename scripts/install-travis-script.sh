9b401933f8fc09038a2dd7f63cc2dee3c50620ba
garyluu <garylau.work@gmail.com>
2018-11-06 17:21:23 -0500

    Move merge prod test with unit test and lint

--- scripts/install-travis-script.sh
+++ scripts/install-travis-script.sh
@@ -6,18 +6,14 @@ set -o xtrace
 
 npm ci
 
-if [ "$RUN_PROD" = true ]; then
-  ng version
-else
-  export CHROME_BIN=/usr/bin/google-chrome
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
-  wget --no-verbose --tries=10 https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${WEBSERVICE_VERSION}/dockstore-webservice-${WEBSERVICE_VERSION}.jar
-  chmod u+x dockstore-webservice-${WEBSERVICE_VERSION}.jar
-  sleep 3 # https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI
-  ng version
-  npm install codecov
-fi
+export CHROME_BIN=/usr/bin/google-chrome
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+wget --no-verbose --tries=10 https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${WEBSERVICE_VERSION}/dockstore-webservice-${WEBSERVICE_VERSION}.jar
+chmod u+x dockstore-webservice-${WEBSERVICE_VERSION}.jar
+sleep 3 # https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI
+ng version
+npm install codecov
 
 wget --no-verbose http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/3.3.1/openapi-generator-cli-3.3.1.jar -O swagger-codegen-cli.jar
 java -jar swagger-codegen-cli.jar generate -i https://raw.githubusercontent.com/ga4gh/dockstore/${WEBSERVICE_VERSION}/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
