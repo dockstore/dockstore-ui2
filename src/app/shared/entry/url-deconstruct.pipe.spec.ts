38ac04a46582f899583c5878eecd5ae49305ff8c
Ryan Bautista <rjbautis1@gmail.com>
2018-11-06 15:06:52 -0800

    * Removes usages of URL library in favor of working directly with string * Calls urlDeconstruct pipe on external links for Docker images * Provides more thorough unit tests for the pipe

--- src/app/shared/entry/url-deconstruct.pipe.spec.ts
+++ src/app/shared/entry/url-deconstruct.pipe.spec.ts
@@ -1,4 +1,4 @@
-/*
+/**
  * Unit test for urlDeconstruct pipe
  */
 
@@ -11,9 +11,24 @@ describe('Pipe: urlDeconstruct', () => {
     expect(pipe).toBeTruthy();
     expect(pipe.transform('https://github.com/agduncan94/hello-dockstore-workflow', ''))
       .toBe('agduncan94/hello-dockstore-workflow');
+    expect(pipe.transform('https://github.com/agduncan94/hello-dockstore-workflow', null))
+      .toBe('agduncan94/hello-dockstore-workflow');
+    expect(pipe.transform('https://bitbucket.org/agduncan94/hello-dockstore-workflow', undefined))
+      .toBe('agduncan94/hello-dockstore-workflow');
+
     expect(pipe.transform('https://github.com/agduncan94/hello-dockstore-workflow', 'master'))
       .toBe('agduncan94/hello-dockstore-workflow/tree/master');
     expect(pipe.transform('https://bitbucket.org/rjbautis/hello-dockstore', 'develop'))
       .toBe('rjbautis/hello-dockstore/src/develop');
+
+    expect(pipe.transform('https://hub.docker.com/r/dockstore/hello-dockstore-image', ''))
+      .toBe('dockstore/hello-dockstore-image');
+    expect(pipe.transform('https://quay.io/repository/dockstore/hello-dockstore-example', ''))
+      .toBe('dockstore/hello-dockstore-example');
+
+    expect(pipe.transform('https://website.com/pathname/hello-world', 'example'))
+      .toBe('pathname/hello-world');
+    expect(pipe.transform('http://website.com/pathname/hello-world', 'example'))
+      .toBe('pathname/hello-world');
   });
 });
--- src/app/shared/entry/url-deconstruct.pipe.ts
+++ src/app/shared/entry/url-deconstruct.pipe.ts
@@ -1,17 +1,37 @@
+/**
+ * Pipe for deconstructing URLs, represented as string objects, into just the important information.
+ *
+ * E.G. https://github.com/agduncan94/hello-dockstore-workflow should be shortened to agduncan94/hello-dockstore-workflow
+ *
+ * @param {string}    providerUrl
+ * @param {string}    versionName
+ *
+ * @return {string}   The pathname of providerUrl
+ */
+
 import { Pipe, PipeTransform } from '@angular/core';
 
-@ Pipe({
+@Pipe({
   name: 'urlDeconstruct'
 })
 export class UrlDeconstructPipe implements PipeTransform {
 
   transform(providerUrl: string, versionName: string): any {
-    if (providerUrl.includes('github.com') && versionName !== '') {
-      return (new URL(providerUrl).pathname).substr(1) + '/tree/' + versionName;
+    const PATHNAME = 3;
+
+    if (providerUrl.includes('github.com') && versionName) {
+      return providerUrl.split('github.com/').pop() + '/tree/' + versionName;
     }
-    if (providerUrl.includes('bitbucket.org') && versionName !== '') {
-      return (new URL(providerUrl).pathname).substr(1) + '/src/' + versionName;
+    if (providerUrl.includes('bitbucket.org') && versionName) {
+      return providerUrl.split('bitbucket.org/').pop() + '/src/' + versionName;
     }
-    return (new URL(providerUrl).pathname).substr(1);
+    if (providerUrl.includes('quay.io')) {
+      return providerUrl.split('/repository/').pop();
+    }
+    if (providerUrl.includes('hub.docker.com')) {
+      return providerUrl.split('/r/').pop();
+    }
+    // Return the url starting from its pathname
+    return providerUrl.split('/').slice(PATHNAME).join('/');
   }
 }
