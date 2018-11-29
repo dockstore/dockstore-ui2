bb460f0a6a2038ee251226befdffd2d2515af081
garyluu <garylau.work@gmail.com>
2018-11-29 13:40:26 -0500

    Update OICR logo, remove BW folder by using css

--- src/app/sponsors/sponsor.model.ts
+++ src/app/sponsors/sponsor.model.ts
@@ -15,28 +15,14 @@
  */
 
 export class Sponsor {
-
-  private static bwPath = '../assets/images/sponsors/bw/';
   private static colouredPath = '../assets/images/sponsors/coloured/';
 
   private current: string;
-  private bw: string;
   private coloured: string;
   private url: URL;
 
   constructor(image: string, url: URL) {
-    this.bw = Sponsor.bwPath + image;
-    this.current = this.bw;
-
-    this.coloured = Sponsor.colouredPath + image;
+    this.current = Sponsor.colouredPath + image;
     this.url = url;
-  }
-
-  setToBW() {
-    this.current = this.bw;
-  }
-
-  setToColoured() {
-    this.current = this.coloured;
   }
 }
