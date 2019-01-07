b96f661b8fe0f7632d883ad1384df8ca77f8437f
garyluu <garylau.work@gmail.com>
2019-01-07 14:42:41 -0500

    Add more test

--- src/app/organizations/state/organizations.query.spec.ts
+++ src/app/organizations/state/organizations.query.spec.ts
@@ -1,6 +1,6 @@
+import { Organisation } from '../../shared/swagger';
 import { OrganizationsQuery } from './organizations.query';
 import { OrganizationsStore } from './organizations.store';
-import { Organisation } from '../../shared/swagger';
 
 describe('OrganizationsQuery', () => {
   let query: OrganizationsQuery;
@@ -15,19 +15,28 @@ describe('OrganizationsQuery', () => {
 
   it('should filter organizations', () => {
     const exampleOrganizations: Array<Organisation> = [
-      {name: 'potato', approved: true},
-      {name: 'beef', approved: true},
-      {name: 'chicken', approved: true},
-      {name: 'pork', approved: true},
-      {name: 'mutton', approved: true},
-      {name: 'duck', approved: true},
+      { name: 'potato', approved: true },
+      { name: 'beef', approved: true },
+      { name: 'chicken', approved: true },
+      { name: 'pork', approved: true },
+      { name: 'mutton', approved: true },
+      { name: 'duck', approved: true },
     ];
-    expect(query.filterOrganizations(exampleOrganizations, 'potato')).toEqual([{name: 'potato', approved: true}]);
-    expect(query.filterOrganizations(exampleOrganizations, 'po')).toEqual([{name: 'potato', approved: true}, {name: 'pork', approved: true}]);
-    expect(query.filterOrganizations(exampleOrganizations, 'POTATO')).toEqual([{name: 'potato', approved: true}]);
-    expect(query.filterOrganizations(exampleOrganizations, 'PO')).toEqual([{name: 'potato', approved: true}, {name: 'pork', approved: true}]);
-    expect(query.filterOrganizations(exampleOrganizations, 'ck')).toEqual([{name: 'chicken', approved: true}, {name: 'duck', approved: true}]);
-    expect(query.filterOrganizations(exampleOrganizations, 'CK')).toEqual([{name: 'chicken', approved: true}, {name: 'duck', approved: true}]);
+    expect(query.filterOrganizations(exampleOrganizations, 'potato')).toEqual([{ name: 'potato', approved: true }]);
+    expect(query.filterOrganizations(exampleOrganizations, 'po'))
+      .toEqual([{ name: 'potato', approved: true }, { name: 'pork', approved: true }]);
+    expect(query.filterOrganizations(exampleOrganizations, 'POTATO'))
+      .toEqual([{ name: 'potato', approved: true }]);
+    expect(query.filterOrganizations(exampleOrganizations, 'PO'))
+      .toEqual([{ name: 'potato', approved: true }, { name: 'pork', approved: true }]);
+    expect(query.filterOrganizations(exampleOrganizations, 'ck'))
+      .toEqual([{ name: 'chicken', approved: true }, { name: 'duck', approved: true }]);
+    expect(query.filterOrganizations(exampleOrganizations, 'CK'))
+      .toEqual([{ name: 'chicken', approved: true }, { name: 'duck', approved: true }]);
+      expect(query.filterOrganizations(null, 'CK'))
+      .toEqual(null);
+      expect(query.filterOrganizations([], 'CK'))
+      .toEqual([]);
   });
 
 });
