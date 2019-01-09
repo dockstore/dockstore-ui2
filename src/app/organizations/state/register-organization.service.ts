575107f45fa15447c6fb53eafe4b6eae06af01b7
garyluu <garylau.work@gmail.com>
2019-01-09 12:52:10 -0500

    Redirect to specific organization page

--- src/app/organizations/state/register-organization.service.ts
+++ src/app/organizations/state/register-organization.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 
 import { AlertService } from '../../shared/alert/state/alert.service';
 import { Organisation, OrganisationsService } from '../../shared/swagger';
@@ -11,7 +12,8 @@ export class RegisterOrganizationService {
 
   readonly urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
-  constructor(private organisationsService: OrganisationsService, private alertService: AlertService, private matDialog: MatDialog) {
+  constructor(private organisationsService: OrganisationsService, private alertService: AlertService, private matDialog: MatDialog,
+    private router: Router) {
   }
 
   /**
@@ -20,7 +22,7 @@ export class RegisterOrganizationService {
    * @param {FormsState['organization']} organizationFormState  The organization form state
    * @memberof RegisterOrganizationService
    */
-  addOrganization(organizationFormState: FormsState['registerOrganization']) {
+  addOrganization(organizationFormState: FormsState['registerOrganization']): void {
     if (!organizationFormState) {
       console.error('Something has gone terribly wrong with the form manager');
       return;
@@ -37,9 +39,14 @@ export class RegisterOrganizationService {
       };
       this.alertService.start('Adding organization');
       this.organisationsService.createOrganisation(newOrganization).subscribe((organization: Organisation) => {
-        this.alertService.simpleSuccess();
-        // TODO: Redirect to new unapproved organization site using the organization object returned
         this.matDialog.closeAll();
+        if (organization) {
+          this.router.navigate(['/organizations', organization.name]);
+          this.alertService.simpleSuccess();
+        } else {
+          console.error('No idea how it would successfully return no organization');
+          this.alertService.simpleError();
+        }
       }, (error: HttpErrorResponse) => {
         this.alertService.detailedError(error);
       });
