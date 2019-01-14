0e8d76609a8f0ad2e8a827a45468d11bcc993f46
Natalie Perez <nperez9@ucsc.edu>
2019-01-14 13:31:00 -0800

    Added simple success and detailed error messages when a workflow is starred/unstarred and began integration test file

---
+++ cypress/integration/group3/starErrorMessage.ts
@@ -0,0 +1,43 @@
+/*
+ *     Copyright 2018 OICR
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License")
+ *     you may not use this file except in compliance with the License
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+import { resetDB, setTokenUserViewPort } from '../../support/commands';
+
+describe('Workflow unstarring error message', () => {
+  resetDB();
+  setTokenUserViewPort();
+  beforeEach(() => {
+    cy.visit('/workflows/github.com/A/l');
+
+    cy
+      .server();
+
+    cy
+      .route({
+        method: 'DELETE',
+        url: /workflows\/11\/unstar/,
+        response: ['DELETE']
+      }).as('unstarWorkflow');
+  });
+
+  describe('Workflow unstarring error message', () => {
+    it('Workflow cannot be unstarred if not already starred.', () => {
+      cy.wait('@unstarWorkflow');
+
+    });
+  });
+});
+
+
