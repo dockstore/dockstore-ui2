40925ce96a8cc7e3eb43647add46238a33bc737a
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-01-15 09:47:15 -0500

    fix ng test

--- src/app/sitemap/sitemap.component.spec.ts
+++ src/app/sitemap/sitemap.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SitemapComponent } from './sitemap.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SitemapComponent', () => {
   let component: SitemapComponent;
@@ -8,7 +8,8 @@ describe('SitemapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SitemapComponent ]
+      declarations: [ SitemapComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
