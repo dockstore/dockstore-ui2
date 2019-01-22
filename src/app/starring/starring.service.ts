a246ff693c0c8f6a463df68d684f901c1ba756c2
Natalie Perez <nperez9@ucsc.edu>
2019-01-22 14:11:29 -0800

    Added alertService.start to update message in snackbar error popups.

--- src/app/starring/starring.service.ts
+++ src/app/starring/starring.service.ts
@@ -21,28 +21,42 @@ import { Injectable } from '@angular/core';
 
 import { ContainersService, WorkflowsService } from '../shared/swagger';
 import { UsersService } from './../shared/swagger/api/users.service';
+import { AlertService } from './../shared/alert/state/alert.service';
 
 @Injectable()
 export class StarringService {
   constructor(private usersService: UsersService, private containersService: ContainersService,
-    private workflowsService: WorkflowsService) { }
+    private workflowsService: WorkflowsService, private alertService: AlertService,) { }
+
   setUnstar(entryID: number, entryType: string): any {
+    const message1 = "Unstarring workflow"
+    const message2 = "Unstarring tool"
+
     if (entryType === 'workflows') {
+      this.alertService.start(message1);
       return this.workflowsService.unstarEntry(entryID);
     } else {
+      this.alertService.start(message2);
       return this.containersService.unstarEntry(entryID);
     }
   }
+
   setStar(entryID: number, entryType: string): any {
     const body: StarRequest = {
       star: true
     };
+    const message1 = "Starring workflow"
+    const message2 = "Starring tool"
+
     if (entryType === 'workflows') {
+      this.alertService.start(message1);
       return this.workflowsService.starEntry(entryID, body);
     } else {
+      this.alertService.start(message2);
       return this.containersService.starEntry(entryID, body);
     }
   }
+
   getStarring(entryID: number, entryType: string): Observable<Array<User>> {
     if (entryType === 'workflows') {
       return this.workflowsService.getStarredUsers(entryID);
