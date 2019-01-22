c1fc8345f60f520563473799cc403b429f13eedb
garyluu <garylau.work@gmail.com>
2019-01-22 11:24:55 -0500

    Add hammerjs to get rid of warning

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -67,3 +67,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+import 'hammerjs/hammer';
