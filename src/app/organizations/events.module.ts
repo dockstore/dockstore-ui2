b9ee40f203c139292a508ba7ed764b92a672455e
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-01-29 11:29:45 -0500

    fixed layout of events

--- src/app/organizations/events.module.ts
+++ src/app/organizations/events.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventsComponent } from './events/events.component';
 import { CustomMaterialModule } from './../shared/modules/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
-  imports: [ CommonModule, CustomMaterialModule ],
+  imports: [ CommonModule, CustomMaterialModule, FlexLayoutModule ],
   declarations: [ EventsComponent ],
   exports: [ EventsComponent ]
 })
