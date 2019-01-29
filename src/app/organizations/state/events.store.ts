4c0a96c93611c8ba768345fb3f499b49e7021435
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-01-29 10:55:54 -0500

    Feature/organizations events view (#530)
    * added events view to organizations

---
+++ src/app/organizations/state/events.store.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
+import { Event } from '../../shared/swagger';
+
+export interface EventsState extends EntityState<Event> {
+}
+
+@Injectable({ providedIn: 'root' })
+@StoreConfig({ name: 'events' })
+export class EventsStore extends EntityStore<EventsState, Event> {
+
+  constructor() {
+    super();
+  }
+
+}
+
