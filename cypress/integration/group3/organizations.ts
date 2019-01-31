1f60db74f24670b1afebaf51143680a50e95e6fe
garyluu <garylau.work@gmail.com>
2019-01-31 15:18:16 -0500

    Pray

--- cypress/integration/group3/organizations.ts
+++ cypress/integration/group3/organizations.ts
@@ -109,6 +109,7 @@ describe('Dockstore my workflows', () => {
 
   describe('Should be able to update description', () => {
     it('be able to update an organization description with markdown', () => {
+      cy.contains('Readme').invoke('width').should('be.gt', 0);
       cy.contains('Readme').should('be.visible').click();
       cy.get('#editOrgDescription').click();
       cy.get('#updateOrganizationDescriptionButton').should('be.visible').should('not.be.disabled');
@@ -117,7 +118,8 @@ describe('Dockstore my workflows', () => {
       cy.contains('fake organization description');
       cy.contains('* fake organization description').should('not.exist');
       cy.get('#updateOrganizationDescriptionButton').should('be.visible').should('not.be.disabled').click();
-      cy.contains('Readme').click();
+      cy.contains('Readme').invoke('width').should('be.gt', 0);
+      cy.contains('Readme').should('be.visible').click();
       cy.contains('fake organization description');
       cy.contains('* fake organization description').should('not.exist');
     });
