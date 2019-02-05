431061459a0d04701fc3601a584ef4f44432afe5
garyluu <garylau.work@gmail.com>
2019-02-05 12:42:14 -0500

    Add loading to add entry, fix retainContent loading

--- src/app/organizations/collection/state/add-entry.service.ts
+++ src/app/organizations/collection/state/add-entry.service.ts
@@ -17,11 +17,14 @@ export class AddEntryService {
    * Updates the set of memberships for the logged in user
    */
   updateMemberships(): void {
+    this.addEntryStore.setLoading(true);
+    this.addEntryStore.setError(false);
     this.usersService.getUserMemberships().pipe(
       finalize(() => this.addEntryStore.setLoading(false)
       ))
       .subscribe((memberships: Array<OrganisationUser>) => {
         this.updateMembershipsState(memberships);
+        this.addEntryStore.setError(false);
       }, () => {
         this.updateMembershipsState(null);
         this.addEntryStore.setError(true);
@@ -46,11 +49,14 @@ export class AddEntryService {
    * @param orgId Id of organisation to grab collections
    */
   updateCollections(orgId: number): void {
+    this.addEntryStore.setLoading(true);
+    this.addEntryStore.setError(false);
     this.organisationsService.getCollectionsFromOrganisation(orgId).pipe(
       finalize(() => this.addEntryStore.setLoading(false)
       ))
       .subscribe((collections: Array<Collection>) => {
         this.updateCollectionsState(collections);
+        this.addEntryStore.setError(false);
       }, () => {
         this.updateCollectionsState(null);
         this.addEntryStore.setError(true);
