a4d710ca839992b21b07f6fa0af72010cd29ccd1
garyluu <garylau.work@gmail.com>
2019-02-05 12:02:27 -0500

    Use loading component in collections component

--- src/app/organizations/organization.module.ts
+++ src/app/organizations/organization.module.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatCardModule, MatExpansionModule, MatIconModule, MatProgressBarModule, MatTabsModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatProgressBarModule,
+  MatTabsModule,
+} from '@angular/material';
+import { NgxMdModule } from 'ngx-md';
 
+import { RefreshAlertModule } from '../shared/alert/alert.module';
 import { HeaderModule } from '../shared/modules/header.module';
 import { CollectionsModule } from './collections.module';
+import { EventsModule } from './events.module';
 import { OrganizationMembersModule } from './organization-members.module';
 import { OrganizationComponent } from './organization/organization.component';
-import { NgxMdModule } from 'ngx-md';
 import { UpdateOrganizationDescriptionModule } from './organization/update-organization-description.module';
-import { EventsModule } from './events.module';
 
 @NgModule({
   imports: [
@@ -26,7 +34,8 @@ import { EventsModule } from './events.module';
     NgxMdModule,
     OrganizationMembersModule,
     UpdateOrganizationDescriptionModule,
-    EventsModule
+    EventsModule,
+    RefreshAlertModule
   ],
   declarations: [ OrganizationComponent ],
   exports: [ OrganizationComponent ]
