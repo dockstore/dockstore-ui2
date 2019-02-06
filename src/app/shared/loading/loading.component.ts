61eba4153b466a249887afeff1bfc583c19a2651
garyluu <garylau.work@gmail.com>
2019-02-06 09:44:16 -0500

    Add comment on how to use the loading component

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,5 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ * To use this component, wrap your original component with this component's <app-loading> tags
+ * and set [loading] equal to whatever your component uses to determine whether it's loading or not.
+ * Example:
+ * <app-loading [loading]="true">My component</app-loading>
+ * @export
+ * @class LoadingComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
