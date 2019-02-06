a897582dc016d4b82106f42ee040b066b12c57f0
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-02-06 09:39:39 -0500

    fix test

--- src/app/organizations/state/organizations.query.spec.ts
+++ src/app/organizations/state/organizations.query.spec.ts
@@ -15,29 +15,29 @@ describe('OrganizationsQuery', () => {
 
   it('should filter organizations', () => {
     const exampleOrganizations: Array<Organisation> = [
-      { name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'beef', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'mutton', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [] },
+      { name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'beef', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'mutton', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
     ];
     expect(query.filterOrganizations(exampleOrganizations, 'potato')).toEqual(
-      [{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      [{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
     expect(query.filterOrganizations(exampleOrganizations, 'po'))
-      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
     expect(query.filterOrganizations(exampleOrganizations, 'POTATO'))
-      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
     expect(query.filterOrganizations(exampleOrganizations, 'PO'))
-      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      .toEqual([{ name: 'potato', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'pork', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
     expect(query.filterOrganizations(exampleOrganizations, 'ck'))
-      .toEqual([{ name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      .toEqual([{ name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
     expect(query.filterOrganizations(exampleOrganizations, 'CK'))
-      .toEqual([{ name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [] },
-      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [] }]);
+      .toEqual([{ name: 'chicken', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' },
+      { name: 'duck', status: Organisation.StatusEnum.APPROVED, users: [], topic: 'nothing relevant' }]);
       expect(query.filterOrganizations(null, 'CK'))
       .toEqual(null);
       expect(query.filterOrganizations([], 'CK'))
