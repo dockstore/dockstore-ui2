dc84ce1bec522c2dd5d8481bec610157f0f4efca
garyluu <garylau.work@gmail.com>
2019-02-08 16:55:26 -0500

    Filtered collections that can't be added

--- src/app/organizations/collection/state/add-entry.query.ts
+++ src/app/organizations/collection/state/add-entry.query.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
-import { AddEntryStore, AddEntryState } from './add-entry.store';
-import { Observable } from 'rxjs';
-import { Collection, OrganisationUser } from '../../../shared/swagger';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CurrentCollectionsQuery } from '../../../entry/state/current-collections.query';
+import { Collection, CollectionOrganization, OrganisationUser } from '../../../shared/swagger';
+import { AddEntryState, AddEntryStore } from './add-entry.store';
 
 @Injectable({ providedIn: 'root' })
 export class AddEntryQuery extends Query<AddEntryState> {
   memberships$: Observable<Array<OrganisationUser>> = this.select(state => state.memberships);
   collections$: Observable<Array<Collection>> = this.select(state => state.collections);
+  filteredCollections$: Observable<Array<Collection>> = combineLatest(this.collections$, this.currentCollectionsQuery.selectAll())
+    .pipe(map(([collections, collectionOrganisation]) => this.filteredCollections(collections, collectionOrganisation)));
   isLoading$: Observable<boolean> = this.selectLoading();
-  constructor(protected store: AddEntryStore) {
+  constructor(protected store: AddEntryStore, private currentCollectionsQuery: CurrentCollectionsQuery) {
     super(store);
   }
 
+  filteredCollections(collections: Array<Collection>, collectionOrganisations: Array<CollectionOrganization>): Array<Collection> {
+    if (collections) {
+    return collections.
+      filter(collection => !collectionOrganisations.some(collectionOrganisation => collectionOrganisation.collection.id === collection.id));
+    } else {
+      return null;
+    }
+  }
 }
