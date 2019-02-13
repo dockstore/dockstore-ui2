1bec2d7381795c794d8858da4a54654d75761748
NatalieEO <nperez9@ucsc.edu>
2019-02-13 09:21:43 -0800

    Added unsubscribe to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { filter, map, mergeMap} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private titleService: Title
   ) {}
 
+  private subscription: Subscription;
+
   ngOnInit() {
-    this.router.events.pipe(
+    this.subscription = this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(() => {
         let route = this.activatedRoute;
@@ -27,6 +30,12 @@ export class AppComponent implements OnInit {
       }),
       mergeMap((route) => route.data),
     ).subscribe((event) => this.titleService.setTitle(event['title']));
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
 
