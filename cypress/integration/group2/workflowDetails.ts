ef49c86954de10b025c7dd216c05a08b09626f96
garyluu <garylau.work@gmail.com>
2019-02-14 13:09:23 -0500

    Fix test

--- cypress/integration/group2/workflowDetails.ts
+++ cypress/integration/group2/workflowDetails.ts
@@ -114,8 +114,8 @@ describe('Dockstore Workflow Details', () => {
     });
 
     it('Change to fullscreen and back', () => {
-      cy.get('#dagDisplay').should('have.class', 'small');
-      cy.get('#dagDisplay').should('not.have.class', 'big');
+      cy.get('[data-cy=dag-holder]').should('have.class', 'small');
+      cy.get('[data-cy=dag-holder]').should('not.have.class', 'big');
       cy
         .get('#dag_fullscreen')
         .click();
