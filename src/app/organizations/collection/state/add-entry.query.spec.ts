1a555a5ffd0c564cc3a5139d160ca2c036e623f3
garyluu <garylau.work@gmail.com>
2019-03-01 09:51:48 -0500

    Fix test

--- src/app/organizations/collection/state/add-entry.query.spec.ts
+++ src/app/organizations/collection/state/add-entry.query.spec.ts
@@ -1,3 +1,5 @@
+import { CurrentCollectionsQuery } from '../../../entry/state/current-collections.query';
+import { CurrentCollectionsStore } from '../../../entry/state/current-collections.store';
 import { AddEntryQuery } from './add-entry.query';
 import { AddEntryStore } from './add-entry.store';
 
@@ -5,7 +7,7 @@ describe('AddEntryQuery', () => {
   let query: AddEntryQuery;
 
   beforeEach(() => {
-    query = new AddEntryQuery(new AddEntryStore);
+    query = new AddEntryQuery(new AddEntryStore, new CurrentCollectionsQuery(new CurrentCollectionsStore));
   });
 
   it('should create an instance', () => {
