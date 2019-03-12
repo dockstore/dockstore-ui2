7b22b8cc2eb95e632a711f474e16d85eec79f3ef
garyluu <garylau.work@gmail.com>
2019-03-12 17:19:49 -0400

    Don't wait for no reason

--- scripts/run-travis-script.sh
+++ scripts/run-travis-script.sh
@@ -10,7 +10,6 @@ psql -c 'create database webservice_test with owner = dockstore;' -U postgres
 psql -f travisci/db_dump.sql webservice_test
 java -jar dockstore-webservice-${WEBSERVICE_VERSION}.jar db migrate -i 1.5.0,1.6.0 travisci/web.yml
 java -jar dockstore-webservice-${WEBSERVICE_VERSION}.jar server travisci/web.yml 1>/dev/null &
-sleep 20
 
 npm run build
 ng serve --progress=false &
