55dab771129078bca9dc89429163c62b0f35992d
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-03-12 09:02:40 -0400

    only show subset of all events

--- src/app/organizations/state/events.service.ts
+++ src/app/organizations/state/events.service.ts
@@ -18,10 +18,24 @@ export class EventsService {
    */
   updateOrganizationEvents(id: number): void {
     this.eventsStore.setLoading(true);
-    this.organizationsService.getOrganizationEvents(id).pipe(
+    this.organizationsService.getOrganizationEvents(id, 0, 30).pipe(
       finalize(() => this.eventsStore.setLoading(false)
       ))
     .subscribe((organizationEvents: Array<Event>) => {
+      const EventType = Event.TypeEnum;
+      const importantEvents = [
+        EventType.CREATEORG,
+        EventType.APPROVEORGINVITE,
+        EventType.MODIFYORG,
+        EventType.CREATECOLLECTION,
+        EventType.MODIFYCOLLECTION,
+        EventType.ADDTOCOLLECTION,
+        EventType.REMOVEFROMCOLLECTION
+      ];
+      // Only return important events
+      organizationEvents = organizationEvents.filter((event: Event) => importantEvents.includes(event.type));
+      // Only return at most 10 events
+      organizationEvents = organizationEvents.slice(0, 10);
       this.eventsStore.set(organizationEvents);
     }, () => {
       this.eventsStore.setError(true);
