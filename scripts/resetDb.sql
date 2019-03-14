78211a606f65db4a7e70de3a6907d64d77acd87b
garyluu <garylau.work@gmail.com>
2019-03-14 12:34:18 -0400

    Attempt to resetDb again

--- scripts/resetDb.sql
+++ scripts/resetDb.sql
@@ -1,3 +1,4 @@
+SELECT pg_cancel_backend(pid) FROM pg_stat_activity WHERE pid IN (SELECT pid FROM pg_locks) AND query NOT LIKE '%pg_stat_activity%';
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 GRANT ALL ON SCHEMA public TO postgres;
