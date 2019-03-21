54da897a735448c4617cc49b2660be1d7dad131d
garyluu <garylau.work@gmail.com>
2019-03-21 12:47:42 -0400

    Optimize Travis

--- scripts/run-travis-script.sh
+++ scripts/run-travis-script.sh
@@ -4,14 +4,8 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-psql -c "create user dockstore with password 'dockstore' createdb;" -U postgres
-psql -c "ALTER USER dockstore WITH superuser;" -U postgres
-psql -c 'create database webservice_test with owner = dockstore;' -U postgres
-psql -f travisci/db_dump.sql webservice_test
-java -jar dockstore-webservice-${WEBSERVICE_VERSION}.jar db migrate -i 1.5.0,1.6.0 travisci/web.yml
-java -jar dockstore-webservice-${WEBSERVICE_VERSION}.jar server travisci/web.yml 1>/dev/null &
-
+npm run webservice
 npm run build
-ng serve --progress=false &
+npm run start &
 ./scripts/wait-for.sh
 cypress run --record --config defaultCommandTimeout=10000 --spec ${TEST}
