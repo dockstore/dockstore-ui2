b72ad9f38f7f88f8a8b97abc2432aa00cac457d1
garyluu <garylau.work@gmail.com>
2019-03-22 10:10:24 -0400

    Move configuration to config

--- scripts/run-webservice-script.sh
+++ scripts/run-webservice-script.sh
@@ -4,12 +4,12 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-wget --no-verbose --tries=10 https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${npm_package_wsv}/dockstore-webservice-${npm_package_wsv}.jar
-chmod u+x dockstore-webservice-${npm_package_wsv}.jar
+wget --no-verbose --tries=10 https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${npm_package_config_wsv}/dockstore-webservice-${npm_package_config_wsv}.jar
+chmod u+x dockstore-webservice-${npm_package_config_wsv}.jar
 psql -c "create user dockstore with password 'dockstore' createdb;" -U postgres
 psql -c "ALTER USER dockstore WITH superuser;" -U postgres
 psql -c 'create database webservice_test with owner = dockstore;' -U postgres
 psql -f travisci/db_dump.sql webservice_test
-java -jar dockstore-webservice-${npm_package_wsv}.jar db migrate -i 1.5.0,1.6.0 travisci/web.yml
-java -jar dockstore-webservice-${npm_package_wsv}.jar server travisci/web.yml 1>/dev/null &
+java -jar dockstore-webservice-${npm_package_config_wsv}.jar db migrate -i 1.5.0,1.6.0 travisci/web.yml
+java -jar dockstore-webservice-${npm_package_config_wsv}.jar server travisci/web.yml 1>/dev/null &
 
