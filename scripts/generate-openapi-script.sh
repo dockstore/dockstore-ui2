7bb646371cd68450dcd24a915c4e3383aee3cb05
Gary Luu <garylau.work@gmail.com>
2019-04-12 15:04:36 -0400

    Feature/preinstall (#601)
    * Preinstall script
    
    * Manually install global @angular/cli
    
    * Optimize Travis
    
    * Change timeout to more sane levels
    
    * Move configuration to config
    
    * Move openapi code generation to scripts
    
    * Silent install
    
    * Update README.md [skip ci]
    
    * nvm version file
    
    * Try using nvm version
    
    * Update README.md [skip ci]
    
    * What happens when you lint with broken imports
    
    * Rename config variable
    
    * Fix
    
    * Use variable
    
    * Comment [skip ci]
    
    * Just removing it to avoid confusion

---
+++ scripts/generate-openapi-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+GENERATOR_VERSION="3.3.4"
+wget --no-verbose http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
+rm -Rf src/app/shared/swagger
+java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/ga4gh/dockstore/$npm_package_config_webservice_version/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
