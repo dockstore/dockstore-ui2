41d5120533cf20df5f783f98e24bfbf733180728
garyluu <garylau.work@gmail.com>
2019-04-15 16:11:23 -0400

    npm i @ngx-share/core @ngx-share/button @ngx-share/buttons @fortawesome/fontawesome-svg-core @fortawesome/angular-fontawesome @fortawesome/free-solid-svg-icons @fortawesome/free-brands-svg-icons latest for all of them + fixes

--- src/app/shared/entry/entry.module.ts
+++ src/app/shared/entry/entry.module.ts
@@ -22,25 +22,22 @@ import { ShareButtonsModule } from '@ngx-share/buttons';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ClipboardModule } from 'ngx-clipboard';
-
+import { RefreshAlertModule } from '../alert/alert.module';
 import { CodeEditorListComponent } from '../code-editor-list/code-editor-list.component';
 import { CodeEditorComponent } from '../code-editor/code-editor.component';
 import { PublicFileDownloadPipe } from '../entry/public-file-download.pipe';
 import { CustomMaterialModule } from '../modules/material.module';
 import { CommitUrlPipe } from './commit-url.pipe';
-import {
-  InfoTabCheckerWorkflowPathComponent,
-} from './info-tab-checker-workflow-path/info-tab-checker-workflow-path.component';
+import { InfoTabCheckerWorkflowPathComponent } from './info-tab-checker-workflow-path/info-tab-checker-workflow-path.component';
 import { LaunchCheckerWorkflowComponent } from './launch-checker-workflow/launch-checker-workflow.component';
 import { PrivateFileDownloadPipe } from './private-file-download.pipe';
 import { PrivateFilePathPipe } from './private-file-path.pipe';
 import { RegisterCheckerWorkflowComponent } from './register-checker-workflow/register-checker-workflow.component';
+import { UrlDeconstructPipe } from './url-deconstruct.pipe';
 import { VerifiedByComponent } from './verified-by/verified-by.component';
 import { VerifiedDisplayComponent } from './verified-display/verified-display.component';
 import { VerifiedPlatformsPipe } from './verified-platforms.pipe';
 import { VersionProviderUrlPipe } from './versionProviderUrl.pipe';
-import { RefreshAlertModule } from '../alert/alert.module';
-import { UrlDeconstructPipe } from './url-deconstruct.pipe';
 
 @NgModule({
   imports: [
@@ -53,7 +50,7 @@ import { UrlDeconstructPipe } from './url-deconstruct.pipe';
     ClipboardModule,
     RouterModule,
     RefreshAlertModule,
-    ShareButtonsModule.forRoot()
+    ShareButtonsModule
   ],
   declarations: [
     InfoTabCheckerWorkflowPathComponent,
