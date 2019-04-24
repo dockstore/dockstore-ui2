b9270eee15760502813ff8f1db3752c2c29879e0
garyluu <garylau.work@gmail.com>
2019-04-24 14:49:21 -0400

    Remove generator after use

--- scripts/generate-openapi-script.sh
+++ scripts/generate-openapi-script.sh
@@ -8,3 +8,4 @@ GENERATOR_VERSION="3.3.4"
 wget --no-verbose http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 rm -Rf src/app/shared/swagger
 java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/ga4gh/dockstore/$npm_package_config_webservice_version/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
+rm -Rf openapi-generator-cli.jar
