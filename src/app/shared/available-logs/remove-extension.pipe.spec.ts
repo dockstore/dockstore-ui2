ff7962e4e6be042a627834dadc777e9a187a3fab
garyluu <garylau.work@gmail.com>
2019-04-26 10:48:52 -0400

    Add test for remove extension pipe

--- src/app/shared/available-logs/remove-extension.pipe.spec.ts
+++ src/app/shared/available-logs/remove-extension.pipe.spec.ts
@@ -1,11 +1,11 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
 import { RemoveExtensionPipe } from './remove-extension.pipe';
 
 describe('Pipe: RemoveExtensione', () => {
   it('create an instance', () => {
-    let pipe = new RemoveExtensionPipe();
+    const pipe = new RemoveExtensionPipe();
     expect(pipe).toBeTruthy();
+    expect(pipe.transform('1556226034.log')).toBe('1556226034');
   });
 });
