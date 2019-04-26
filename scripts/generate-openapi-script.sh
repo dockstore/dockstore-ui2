b761cd4c07815b368682605d9384d311de1ff3c3
garyluu <garylau.work@gmail.com>
2019-04-26 17:28:25 -0400

    Use feature branch

--- scripts/generate-openapi-script.sh
+++ scripts/generate-openapi-script.sh
@@ -8,6 +8,6 @@ GENERATOR_VERSION="3.3.4"
 wget --no-verbose http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 rm -Rf src/app/shared/swagger
 rm -Rf src/app/shared/openapi
-java -jar openapi-generator-cli.jar generate -i ~/dockstore/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
-java -jar openapi-generator-cli.jar generate -i ~/dockstore/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
+java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/feature/2348/awsLogs/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
+java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/feature/2348/awsLogs/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
 rm -Rf openapi-generator-cli.jar
