f7f83b429c5191c8bbc5e10265b56a11d776eda7
garyluu <garylau.work@gmail.com>
2019-04-26 10:18:09 -0400

    Fix

--- src/app/shared/available-logs/remove-extension.pipe.ts
+++ src/app/shared/available-logs/remove-extension.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'remove-extension'
+  name: 'removeExtension'
 })
 export class RemoveExtensionPipe implements PipeTransform {
 
