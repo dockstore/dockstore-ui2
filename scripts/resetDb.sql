ed8379ebe49067d3653563fbe0134a0ab8ab6290
garyluu <garylau.work@gmail.com>
2019-04-29 15:56:31 -0400

    Test everything on circle CI

--- scripts/resetDb.sql
+++ scripts/resetDb.sql
@@ -15,7 +15,7 @@
  */
 BEGIN;
     /* Remove all locks except for this one process */
-    SELECT pg_cancel_backend(pid) FROM pg_stat_activity WHERE pid IN (SELECT pid FROM pg_locks) AND query NOT LIKE '%pg_stat_activity%';
+    SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pid IN (SELECT pid FROM pg_locks) AND query NOT LIKE '%pg_stat_activity%';
     /* Ultimately drops all tables (better than dropping database on a running webservice connected to it) */
     DROP SCHEMA public CASCADE;
     /* Recreate the schema for the tables */
