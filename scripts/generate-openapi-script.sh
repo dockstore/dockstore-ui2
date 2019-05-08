eb83167778e971a51c8e8f2945d1213d99496aee
garyluu <garylau.work@gmail.com>
2019-05-08 10:17:39 -0400

    Actually use the real webservice jar

--- scripts/generate-openapi-script.sh
+++ scripts/generate-openapi-script.sh
@@ -8,6 +8,7 @@ GENERATOR_VERSION="3.3.4"
 wget --no-verbose http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 rm -Rf src/app/shared/swagger
 rm -Rf src/app/shared/openapi
-java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/feature/2348/awsLogs/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
-java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/feature/2348/awsLogs/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
-rm -Rf openapi-generator-cli.jar
+java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/$npm_package_config_webservice_version/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
+java -jar openapi-generator-cli.jar generate -i https://raw.githubusercontent.com/dockstore/dockstore/$npm_package_config_webservice_version/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
+rm openapi-generator-cli.jar
+
