cfe992bac20115321316d4b45556253fdc89cd5e
Charles Overbeck <coverbec@ucsc.edu>
2019-05-21 15:52:32 -0700

    Fix lint errors

--- src/app/loginComponents/accounts/external/links.model.ts
+++ src/app/loginComponents/accounts/external/links.model.ts
@@ -5,7 +5,7 @@ export class Links {
   static readonly GITHUB = () => `${ Dockstore.GITHUB_AUTH_URL }
                             ?client_id=${ Dockstore.GITHUB_CLIENT_ID }
                             &redirect_uri=${ Dockstore.GITHUB_REDIRECT_URI }
-                            &scope=${ Dockstore.GITHUB_SCOPE }`;
+                            &scope=${ Dockstore.GITHUB_SCOPE }`
 
   static readonly QUAY = () => { return `${ Dockstore.QUAYIO_AUTH_URL }
                           ?client_id=${ Dockstore.QUAYIO_CLIENT_ID }
@@ -16,13 +16,13 @@ export class Links {
 
   static readonly BITBUCKET = () => `${ Dockstore.BITBUCKET_AUTH_URL }
                                ?client_id=${ Dockstore.BITBUCKET_CLIENT_ID }
-                               &response_type=code`;
+                               &response_type=code`
 
   /** need this strange construction since gitlab scopes are space-separated */
   static readonly GITLAB = () => `${ Dockstore.GITLAB_AUTH_URL }
                             ?client_id=${ Dockstore.GITLAB_CLIENT_ID }
                             &redirect_uri=${ Dockstore.GITLAB_REDIRECT_URI }
                             &response_type=code`.replace(/\s/g, '') +
-                            `&scope=${ Dockstore.GITLAB_SCOPE }`;
+                            `&scope=${ Dockstore.GITLAB_SCOPE }`
 
 }
