ca6c23b7f347a95b5c3d500c654241860defa398
Ryan Bautista <rjbautis1@gmail.com>
2019-05-29 11:19:05 -0700

    * Another linting fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -22,7 +22,7 @@ export function unregisterServiceWorkers() {
           registrations.forEach((registration) => {
             registration.unregister();
           })
-        })
+        });
     }
   });
 }
