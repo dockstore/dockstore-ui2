c52bfad65110c414de4e4ca3348932df5203b2a3
Walt Shands <jshands@ucsc.edu>
2019-05-29 17:38:30 -0700

    add zenodo account linking

--- src/app/shared/enum/provider.enum.ts
+++ src/app/shared/enum/provider.enum.ts
@@ -3,5 +3,6 @@ export enum Provider {
     GITHUB = 'github.com', // This does not need to match anything
     QUAY = 'quay.io', // this does not need to match anything, it can be whatever
     BITBUCKET = 'bitbucket.org', // This can be anything as long as it matches the redirect URI on bitbucket
-    GITLAB = 'gitlab.com' // This must match the redirect URL in the dockstore.yml and GitLab authorized applications
+    GITLAB = 'gitlab.com', // This must match the redirect URL in the dockstore.yml and GitLab authorized applications
+    ZENODO = 'zenodo.org'
 }
