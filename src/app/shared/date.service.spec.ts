dede57197ce9cbf6e8c1ddcc324796bdd80056ea
Shreya Radesh <sradesh@ucsc.edu>
2019-06-19 16:35:30 -0700

    added unit tests for date spec

--- src/app/shared/date.service.spec.ts
+++ src/app/shared/date.service.spec.ts
@@ -42,8 +42,12 @@ describe('DateService', () => {
     it('should be getVerifiedLink', inject([DateService], (service: DateService) => {
         expect(service.getVerifiedLink()).toEqual('https://docs.dockstore.org/faq/#what-is-a-verified-tool-or-workflow');
     }));
-    it('should be ISO8601Format', inject([DateService], (service: DateService) => {
-        expect(service.getISO8601Format(1498675698000)).toEqual('2017-06-28T18:48:18.000Z');
+    it('should be ISO8601Format when given number', inject([DateService], (service: DateService) => {
+        expect(service.getISO8601FormatFromNumber(1498675698000)).toEqual('2017-06-28T18:48:18.000Z');
+    }));
+    it('should be ISO8601Format when given Date', inject([DateService], (service: DateService) => {
+        const tsAsDate = new Date(1498675698000);
+        expect(service.getISO8601FormatFromDate(tsAsDate)).toEqual('2017-06-28T18:48:18.000Z');
     }));
 });
 
