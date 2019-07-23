fdb791bb5a45a3756c8c25f725b56798aee66efc
Emily Soth <esoth@ucsc.edu>
2019-07-23 14:03:28 -0700

    pr changes

--- src/app/logged-in-banner/logged-in-banner.component.ts
+++ src/app/logged-in-banner/logged-in-banner.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../shared/swagger';
-import { UserQuery } from '../shared/user/user.query';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logged-in-banner',
   templateUrl: './logged-in-banner.component.html',
   styleUrls: ['./logged-in-banner.component.scss']
 })
-export class LoggedInBannerComponent implements OnInit {
-  constructor(private userQuery: UserQuery) {}
-  public user$: Observable<User>;
-
-  ngOnInit() {
-    this.user$ = this.userQuery.user$;
-  }
+export class LoggedInBannerComponent {
+  constructor() {}
 }
