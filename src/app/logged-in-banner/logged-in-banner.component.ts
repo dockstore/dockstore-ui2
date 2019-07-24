cf56448b9fc65d27a25f07a2fc11d1b3661b95cc
emlys <43770515+emlys@users.noreply.github.com>
2019-07-24 15:01:50 -0700

    dockstore/dockstore#2604 created banner component (#708)
    dockstore/dockstore#2604
    added logged-in-banner component

---
+++ src/app/logged-in-banner/logged-in-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-in-banner',
+  templateUrl: './logged-in-banner.component.html',
+  styleUrls: ['./logged-in-banner.component.scss']
+})
+export class LoggedInBannerComponent {
+  constructor() {}
+}
