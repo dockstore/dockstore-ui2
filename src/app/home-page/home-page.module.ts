27b74795bbf212d779b13fd48c44d69043be7490
garyluu <garylau.work@gmail.com>
2019-07-31 15:22:59 -0400

    Add back old home page, revert later

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -5,11 +5,16 @@ import { RouterModule } from '@angular/router';
 import { HomeLoggedInComponent } from 'app/home-page/home-logged-in/home-logged-in.component';
 import { HomeComponent } from 'app/home-page/home-logged-out/home.component';
 import { LoggedInBannerComponent } from 'app/home-page/logged-in-banner/logged-in-banner.component';
+import { ListContainersModule } from 'app/shared/modules/list-containers.module';
+import { ListWorkflowsModule } from 'app/shared/modules/list-workflows.module';
 import { CustomMaterialModule } from 'app/shared/modules/material.module';
+import { TabsModule } from 'ngx-bootstrap';
 import { HomePageComponent } from './home-page.component';
+import { OldHomePageComponent, YoutubeComponent } from './old-home-page/old-home-page.component';
 
 @NgModule({
-  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule],
-  declarations: [HomePageComponent, LoggedInBannerComponent, HomeComponent, HomeLoggedInComponent]
+  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule, ListWorkflowsModule, ListContainersModule, TabsModule],
+  declarations: [HomePageComponent, LoggedInBannerComponent, HomeComponent, HomeLoggedInComponent, OldHomePageComponent, YoutubeComponent],
+  entryComponents: [YoutubeComponent]
 })
 export class HomePageModule {}
