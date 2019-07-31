f8cd12f9edaf7c0cde21c247425e7ca09a1cefbf
garyluu <garylau.work@gmail.com>
2019-07-31 14:52:22 -0400

    Fix broken links

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 import { HomeLoggedInComponent } from 'app/home-page/home-logged-in/home-logged-in.component';
 import { HomeComponent } from 'app/home-page/home-logged-out/home.component';
 import { LoggedInBannerComponent } from 'app/home-page/logged-in-banner/logged-in-banner.component';
@@ -8,7 +9,7 @@ import { CustomMaterialModule } from 'app/shared/modules/material.module';
 import { HomePageComponent } from './home-page.component';
 
 @NgModule({
-  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule],
+  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule],
   declarations: [HomePageComponent, LoggedInBannerComponent, HomeComponent, HomeLoggedInComponent]
 })
 export class HomePageModule {}
