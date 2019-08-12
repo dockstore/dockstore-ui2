07a87d9f4b11a2f661b017ca66e7a750e95af993
Charles Overbeck <coverbec@ucsc.edu>
2019-08-12 15:12:36 -0700

    Add missing import.

--- cypress/integration/immutableDatabaseTests/services.ts
+++ cypress/integration/immutableDatabaseTests/services.ts
@@ -13,7 +13,13 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-import { assertNoTab, assertVisibleTab, goToTab, setTokenUserViewPort } from '../../support/commands';
+import {
+  assertNoTab,
+  assertVisibleTab,
+  goToTab,
+  setTokenUserViewPort,
+  setTokenUserViewPortCurator
+} from '../../support/commands';
 
 describe('Dockstore Home', () => {
   describe('GitHub App Callback Routing', () => {
