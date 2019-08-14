4e42194ee640d2c7a8b41e93f80ea9ccdd7d15e3
garyluu <garylau.work@gmail.com>
2019-08-14 17:26:52 -0400

    Fix test

--- src/app/search/basic-search/basic-search.component.spec.ts
+++ src/app/search/basic-search/basic-search.component.spec.ts
@@ -3,10 +3,10 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatAutocompleteModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { ProviderService } from '../../shared/provider.service';
-import { AdvancedSearchStubService } from '../../test/service-stubs';
+import { AdvancedSearchStubService, SearchStubService } from '../../test/service-stubs';
 import { AdvancedSearchService } from '../advancedsearch/advanced-search.service';
+import { SearchService } from '../state/search.service';
 import { BasicSearchComponent } from './basic-search.component';
 
 describe('BasicSearchComponent', () => {
@@ -18,7 +18,11 @@ describe('BasicSearchComponent', () => {
       schemas: [NO_ERRORS_SCHEMA],
       imports: [MatAutocompleteModule, RouterTestingModule],
       declarations: [BasicSearchComponent],
-      providers: [ProviderService, { provide: AdvancedSearchService, useClass: AdvancedSearchStubService }]
+      providers: [
+        ProviderService,
+        { provide: AdvancedSearchService, useClass: AdvancedSearchStubService },
+        { provide: SearchService, useClass: SearchStubService }
+      ]
     }).compileComponents();
   }));
 
