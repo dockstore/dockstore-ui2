705888a8327f0bc71580cf61bd7503e2800ffa75
stefanijaorbovic <stefanija.orbovic@sbgenomics.com>
2019-08-28 15:22:56 +0200

    Add CGC bbutton for CWL workflows

--- src/app/shared/dockstore.model.ts
+++ src/app/shared/dockstore.model.ts
@@ -32,6 +32,7 @@ export class Dockstore {
   static DNASTACK_IMPORT_URL = 'https://app.dnastack.com/#/app/workflow/import/dockstore';
   static DNANEXUS_IMPORT_URL = 'https://platform.dnanexus.com/panx/tools/import-workflow';
   static TERRA_IMPORT_URL = 'https://app.terra.bio/#import-tool/dockstore';
+  static CGC_IMPORT_URL = 'https://cgc.sbgenomics.com/integration/trs/import';
 
   static GITHUB_CLIENT_ID = 'will be filled in by configuration.service';
   static GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
