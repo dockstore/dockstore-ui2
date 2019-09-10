730048f8c321d533845a673db994b0698b46c6ab
Emily Soth <esoth@ucsc.edu>
2019-09-10 16:04:02 -0700

    updated to google recommendation

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { devMode } from 'app/shared/constants';
 import { User } from 'app/shared/swagger';
 import { UserQuery } from 'app/shared/user/user.query';
 import { Observable } from 'rxjs';
+import { HomePageService } from './home-page.service';
 
 @Component({
   selector: 'app-home-page',
@@ -12,9 +13,13 @@ import { Observable } from 'rxjs';
 export class HomePageComponent implements OnInit {
   devMode = devMode;
   public user$: Observable<User>;
-  constructor(private userQuery: UserQuery) {}
+  public hpOrgSchema;
+  public hpWebsiteSchema;
+  constructor(private userQuery: UserQuery, private hpService: HomePageService) {}
 
   ngOnInit() {
     this.user$ = this.userQuery.user$;
+    this.hpOrgSchema = this.hpService.hpOrgSchema;
+    this.hpWebsiteSchema = this.hpService.hpWebsiteSchema;
   }
 }
