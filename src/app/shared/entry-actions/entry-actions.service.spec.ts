22e019ac7b534ceb9dbdff246bb09a5610106565
Andy Chen <achen81@ucsc.edu>
2019-09-17 13:34:06 -0700

    Added test case for a null entry

--- src/app/shared/entry-actions/entry-actions.service.spec.ts
+++ src/app/shared/entry-actions/entry-actions.service.spec.ts
@@ -120,5 +120,10 @@ describe('Service: EntryActionsService', () => {
     expect(service.getPublishMessage(entry, EntryType.BioWorkflow)).toBe('Unable to publish: No valid versions found');
     expect(service.getPublishMessage(entry, EntryType.Service)).toBe('Unable to publish: No valid versions found');
     expect(service.getPublishMessage(entry, null)).toBe('');
+    const nullEntry = null;
+    expect(service.getPublishMessage(nullEntry, EntryType.Tool)).toBe('');
+    expect(service.getPublishMessage(nullEntry, EntryType.BioWorkflow)).toBe('');
+    expect(service.getPublishMessage(nullEntry, EntryType.Service)).toBe('');
+    expect(service.getPublishMessage(nullEntry, null)).toBe('');
   }));
 });
