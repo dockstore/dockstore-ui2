e47ccd6a509a0a9edb9edd8f2d178db11f68d307
garyluu <garylau.work@gmail.com>
2019-09-26 13:41:07 -0400

    Test fixes

--- src/app/organizations/collections/state/create-collection.service.spec.ts
+++ src/app/organizations/collections/state/create-collection.service.spec.ts
@@ -67,7 +67,7 @@ describe('CreateCollectionService', () => {
   });
 
   it('should try to add a collection', () => {
-    organizationQuerySpy.getValue.and.returnValue({ organization: { id: 1 } });
+    organizationQuerySpy.getValue.and.returnValue(<any>{ organization: { id: 1 } });
     organizationsServiceSpy.createCollection.and.returnValue(observableOf(null));
     matDialogSpy.closeAll.and.returnValue(null);
 
@@ -80,7 +80,7 @@ describe('CreateCollectionService', () => {
   });
 
   it('should handle error when adding a collection', () => {
-    organizationQuerySpy.getValue.and.returnValue({ organization: { id: 1 } });
+    organizationQuerySpy.getValue.and.returnValue(<any>{ organization: { id: 1 } });
     organizationsServiceSpy.createCollection.and.returnValue(throwError('test 404 error'));
 
     createCollectionService.createCollection(exampleFormState);
@@ -92,7 +92,7 @@ describe('CreateCollectionService', () => {
   });
 
   it('should try to update a collection', () => {
-    organizationQuerySpy.getValue.and.returnValue({ organization: { id: 1 } });
+    organizationQuerySpy.getValue.and.returnValue(<any>{ organization: { id: 1 } });
     organizationsServiceSpy.updateCollection.and.returnValue(observableOf(null));
     matDialogSpy.closeAll.and.returnValue(null);
 
@@ -105,7 +105,7 @@ describe('CreateCollectionService', () => {
   });
 
   it('should handle error when updating a collection', () => {
-    organizationQuerySpy.getValue.and.returnValue({ organization: { id: 1 } });
+    organizationQuerySpy.getValue.and.returnValue(<any>{ organization: { id: 1 } });
     organizationsServiceSpy.updateCollection.and.returnValue(throwError('test 404 error'));
 
     createCollectionService.updateCollection(exampleFormState, 1, 'description');
