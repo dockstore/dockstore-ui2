eb5febc3e3fd4c36f54f67ed9f3bba9177e86839
Andy Chen <achen81@ucsc.edu>
2019-10-08 11:50:43 -0700

    Removed two unused imports in ExtendedWorkflow.ts

--- src/app/shared/models/ExtendedWorkflow.ts
+++ src/app/shared/models/ExtendedWorkflow.ts
@@ -1,7 +1,6 @@
 import { Workflow } from './../swagger/model/workflow';
-import { Service } from '../openapi/model/service';
-import { BioWorkflow } from '../openapi/model/bioWorkflow';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
 export interface ExtendedWorkflow extends Workflow {
   agoMessage?: string;
   // Stripped of 'mailto:'
