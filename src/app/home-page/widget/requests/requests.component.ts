bc7630f843bb15842e533f07ca6ec124a827f5ff
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-10-15 09:43:01 -0400

    updated org requests

--- src/app/home-page/widget/requests/requests.component.ts
+++ src/app/home-page/widget/requests/requests.component.ts
@@ -1,19 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService, OrganizationUser } from '../../../shared/swagger';
+import { UsersService, OrganizationUser, Organization, OrganizationsService } from '../../../shared/swagger';
 import { AlertService } from '../../../shared/alert/state/alert.service';
+import { UserQuery } from '../../../shared/user/user.query';
+import { Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Base } from '../../../shared/base';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
   styleUrls: ['./requests.component.scss']
 })
-export class RequestsComponent implements OnInit {
+export class RequestsComponent extends Base implements OnInit {
   public myOrganizationInvites;
   public myOrganizationRequests;
-  constructor(private usersService: UsersService, private alertService: AlertService) {}
+  public myPendingOrganizationRequests;
+
+  isAdmin$: Observable<boolean>;
+  isCurator$: Observable<boolean>;
+  constructor(
+    private usersService: UsersService,
+    private alertService: AlertService,
+    private userQuery: UserQuery,
+    private organizationsService: OrganizationsService
+  ) {
+    super();
+  }
 
   ngOnInit() {
     this.getMyMemberships();
+
+    this.isAdmin$ = this.userQuery.isAdmin$;
+    this.isCurator$ = this.userQuery.isCurator$;
+
+    this.userQuery.isAdminOrCurator$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(isAdminOrCurator => {
+      if (isAdminOrCurator) {
+        this.updateCuratorOrganizations(); // requires admin or curator permissions
+      }
+    });
+  }
+
+  updateCuratorOrganizations(): void {
+    this.organizationsService.getAllOrganizations('pending').subscribe(
+      (allPendingOrganizations: Array<Organization>) => {
+        this.myPendingOrganizationRequests = allPendingOrganizations;
+        this.alertService.simpleSuccess();
+      },
+      () => {
+        this.alertService.simpleError();
+      }
+    );
   }
 
   getMyMemberships(): void {
