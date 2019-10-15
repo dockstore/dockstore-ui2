1235d0adccc79ef282522194ebc146ebb1f10f6e
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-10-15 10:06:57 -0400

    fix ng test

--- src/app/home-page/widget/requests/requests.component.spec.ts
+++ src/app/home-page/widget/requests/requests.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RequestsComponent } from './requests.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CustomMaterialModule } from 'app/shared/modules/material.module';
+import { UsersService } from 'app/shared/swagger';
+import { UsersStubService } from 'app/test/service-stubs';
 
 describe('RequestsComponent', () => {
   let component: RequestsComponent;
@@ -8,7 +13,9 @@ describe('RequestsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [RequestsComponent]
+      declarations: [RequestsComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule, CustomMaterialModule],
+      providers: [{ provide: UsersService, useClass: UsersStubService }]
     }).compileComponents();
   }));
 
