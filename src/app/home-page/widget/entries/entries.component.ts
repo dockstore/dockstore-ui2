3f5925fc4d939f6b2245d6bf7f8a75a8df6c55f8
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-11-07 13:16:32 -0500

    fixes from pr

--- src/app/home-page/widget/entries/entries.component.ts
+++ src/app/home-page/widget/entries/entries.component.ts
@@ -1,56 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserQuery } from 'app/shared/user/user.query';
-import { Observable, Subject } from 'rxjs';
 import { DefaultService } from 'app/shared/openapi';
 import { EntryUpdateTime } from 'app/shared/openapi/model/entryUpdateTime';
 import { debounceTime, takeUntil } from 'rxjs/operators';
 import { formInputDebounceTime } from 'app/shared/constants';
+import { FilteredList } from '../filtered-list';
 
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.component.html',
   styleUrls: ['./entries.component.scss']
 })
-export class EntriesComponent implements OnInit {
-  public myEntries;
+export class EntriesComponent extends FilteredList {
   public entryTypeEnum = EntryUpdateTime.EntryTypeEnum;
-  public entryFilterText;
-  public hasEntries = false;
-  public firstCall = true;
-  userId$: Observable<number>;
-  protected ngUnsubscribe: Subject<{}> = new Subject();
-
-  constructor(private userQuery: UserQuery, private defaultService: DefaultService) {}
-
-  ngOnInit() {
-    this.userId$ = this.userQuery.userId$;
-    this.userQuery.userId$.subscribe((userId: number) => {
-      this.getMyEntries();
-    });
+  constructor(userQuery: UserQuery, defaultService: DefaultService) {
+    super(userQuery, defaultService);
   }
 
-  getMyEntries(): void {
+  getMyList(): void {
     this.defaultService
-      .getUserEntries(10, this.entryFilterText)
+      .getUserEntries(10, this.filterText)
       .pipe(
         debounceTime(formInputDebounceTime),
         takeUntil(this.ngUnsubscribe)
       )
       .subscribe(
         (myEntries: Array<EntryUpdateTime>) => {
-          this.myEntries = myEntries;
-          if (this.firstCall && this.myEntries && this.myEntries.length > 0) {
-            this.hasEntries = true;
+          this.myItems = myEntries;
+          if (this.firstCall && this.myItems && this.myItems.length > 0) {
+            this.hasItems = true;
             this.firstCall = false;
           }
         },
         () => {}
       );
-  }
-
-  onTextChange(event: any) {
-    this.userQuery.userId$.subscribe((userId: number) => {
-      this.getMyEntries();
-    });
   }
 }
--- src/app/home-page/widget/organizations/organizations.component.ts
+++ src/app/home-page/widget/organizations/organizations.component.ts
@@ -1,55 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Component } from '@angular/core';
 import { UserQuery } from 'app/shared/user/user.query';
 import { DefaultService } from 'app/shared/openapi';
 import { OrganizationUpdateTime } from 'app/shared/openapi/model/organizationUpdateTime';
 import { debounceTime, takeUntil } from 'rxjs/operators';
 import { formInputDebounceTime } from 'app/shared/constants';
+import { FilteredList } from '../filtered-list';
 
 @Component({
   selector: 'app-organizations',
   templateUrl: './organizations.component.html',
   styleUrls: ['./organizations.component.scss']
 })
-export class OrganizationsComponent implements OnInit {
-  public myOrganizations;
-  public organizationFilterText;
-  public hasOrganizations = false;
-  public firstCall = true;
-  userId$: Observable<number>;
-  protected ngUnsubscribe: Subject<{}> = new Subject();
-
-  constructor(private userQuery: UserQuery, private defaultService: DefaultService) {}
-
-  ngOnInit() {
-    this.userId$ = this.userQuery.userId$;
-    this.userQuery.userId$.subscribe((userId: number) => {
-      this.getMyOrganizations();
-    });
+export class OrganizationsComponent extends FilteredList {
+  constructor(userQuery: UserQuery, defaultService: DefaultService) {
+    super(userQuery, defaultService);
   }
 
-  getMyOrganizations(): void {
+  getMyList(): void {
     this.defaultService
-      .getUserDockstoreOrganizations(10, this.organizationFilterText)
+      .getUserDockstoreOrganizations(10, this.filterText)
       .pipe(
         debounceTime(formInputDebounceTime),
         takeUntil(this.ngUnsubscribe)
       )
       .subscribe(
         (myOrganizations: Array<OrganizationUpdateTime>) => {
-          this.myOrganizations = myOrganizations;
-          if (this.firstCall && this.myOrganizations && this.myOrganizations.length > 0) {
-            this.hasOrganizations = true;
+          this.myItems = myOrganizations;
+          if (this.firstCall && this.myItems && this.myItems.length > 0) {
+            this.hasItems = true;
             this.firstCall = false;
           }
         },
         () => {}
       );
-  }
-
-  onTextChange(event: any) {
-    this.userQuery.userId$.subscribe((userId: number) => {
-      this.getMyOrganizations();
-    });
   }
 }
