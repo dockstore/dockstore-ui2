b52616c3cd4349c952e0576df79b0600ff1c3ae5
Andy Chen <achen81@ucsc.edu>
2019-12-12 22:19:08 -0800

    Linting

--- src/app/notifications/state/notifications.query.ts
+++ src/app/notifications/state/notifications.query.ts
@@ -3,12 +3,10 @@ import { QueryEntity } from '@datorama/akita';
 import { NotificationsStore, NotificationsState } from './notifications.store';
 import { Notification } from '../../shared/swagger/model/notification';
 import { Observable } from 'rxjs';
-import { first, map, mergeMap, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationsQuery extends QueryEntity<NotificationsState> {
   allNotifications$: Observable<Array<Notification>> = this.select(state => state.notifications);
-  //mostRecentNotification$: Observable<Notification> = this.allNotifications$.pipe(first(), mergeMap(x => {console.log(x); return x;}));
 
   constructor(protected store: NotificationsStore) {
     super(store);
