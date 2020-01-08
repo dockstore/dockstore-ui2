b063c387b2f419bec1e21c0bb16cbdeb16ececa5
Gary Luu <garylau.work@gmail.com>
2020-01-08 12:22:31 -0500

    Feature/2966/back button fix (#863)
    * Set data after pagination to fix issues

--- src/app/search/search-entry-table.ts
+++ src/app/search/search-entry-table.ts
@@ -48,13 +48,12 @@ export abstract class SearchEntryTable extends Base implements OnInit {
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(([pageSize, pageIndex, entries]) => {
         this.dataSource.paginator.pageSize = pageSize;
-        if (entries) {
-          this.dataSource.data = entries;
-          this.dataSource.paginator.pageIndex = pageIndex;
-        }
+        this.dataSource.paginator.pageIndex = pageIndex;
+        // Must set data after paginator, just a material datatables thing.
+        this.dataSource.data = entries || [];
       });
     this.dataSource.sortData = (data: DockstoreTool[] | Workflow[], sort: MatSort) => {
-      return data.slice().sort((a, b) => {
+      return data.slice().sort((a: Workflow | DockstoreTool, b: Workflow | DockstoreTool) => {
         return this.searchService.compareAttributes(a, b, sort.active, sort.direction);
       });
     };
