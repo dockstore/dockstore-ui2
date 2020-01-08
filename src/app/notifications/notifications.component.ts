b87c7dbdf405cf902421380d3fbb52163162acab
Andy Chen <achen81@ucsc.edu>
2020-01-08 15:44:00 -0800

    [skip ci]

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Notification } from '../shared/swagger/model/notification';
 import { NotificationsQuery } from './state/notifications.query';
 import { NotificationsService } from './state/notifications.service';
 import { Observable } from 'rxjs';
-import { formatDate } from '@angular/common';
+import { map } from 'rxjs/operators';
 
 interface DismissedNotification {
   id: number;
@@ -13,13 +13,13 @@ interface DismissedNotification {
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
   constructor(private notificationsQuery: NotificationsQuery, private notificationsService: NotificationsService) {}
   message: string;
   public allNotifications$: Observable<Array<Notification>>;
+  public activeNotifications$: Observable<Array<Notification>>;
   private readonly storageKey = 'dismissedNotifications';
   public dismissedNotifications: Array<DismissedNotification> = JSON.parse(localStorage.getItem(this.storageKey) || '[]');
 
@@ -32,7 +32,7 @@ export class NotificationsComponent implements OnInit {
   removeExpiredDisabledNotifications(notificationsStored: Array<DismissedNotification>) {
     const today = Date.now();
     this.dismissedNotifications = this.dismissedNotifications.filter(notification => {
-      return notification.expiration.getTime() > today;
+      return new Date(notification.expiration).getTime() > today;
     });
     localStorage.setItem(this.storageKey, JSON.stringify(this.dismissedNotifications));
   }
@@ -40,6 +40,7 @@ export class NotificationsComponent implements OnInit {
   respondClick(notification: Notification) {
     this.dismissedNotifications.push({ id: notification.id, expiration: notification.expiration });
     localStorage.setItem(this.storageKey, JSON.stringify(this.dismissedNotifications));
+    this.notificationsService.remove(notification.id);
   }
 
   notificationDismissed(notification: Notification): boolean {
