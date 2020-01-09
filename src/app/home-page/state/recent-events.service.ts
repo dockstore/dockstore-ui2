5cf43162688c6310fb6afb52b9e61f3d8e4ad1a3
garyluu <garylau.work@gmail.com>
2020-01-09 11:16:14 -0500

    Update for starred organization events

--- src/app/home-page/state/recent-events.service.ts
+++ src/app/home-page/state/recent-events.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { Event, EventsService } from 'app/shared/openapi';
+import { forkJoin } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { RecentEventsStore } from './recent-events.store';
 
@@ -9,12 +10,26 @@ export class RecentEventsService {
   constructor(private recentEventsStore: RecentEventsStore, private eventsService: EventsService) {}
 
   get() {
-    return this.eventsService.getEvents('STARRED_ENTRIES', 10, 0).pipe(
-      tap(entities => {
-        entities.sort((a, b) => +b.dbCreateDate - +a.dbCreateDate);
-        this.recentEventsStore.set(entities);
+    return forkJoin([this.eventsService.getEvents('STARRED_ENTRIES'), this.eventsService.getEvents('STARRED_ORGANIZATION')]).pipe(
+      tap(([starredEntriesEvents, starredOrganizationEvents]) => {
+        const allStarredEvents: Array<Event> = this.cleanUpEvents(starredEntriesEvents, starredOrganizationEvents);
+        this.recentEventsStore.set(allStarredEvents);
       })
     );
+  }
+
+  private cleanUpEvents(starredEntriesEvents: Event[], starredOrganizationEvents: Event[]) {
+    let allStarredEvents: Array<Event> = [];
+    // TODO: Need a less terrible way of getting unique events
+    allStarredEvents = allStarredEvents.concat(starredEntriesEvents);
+    const ids = allStarredEvents.map(ent => ent.id);
+    starredOrganizationEvents.forEach(entity => {
+      if (!ids.includes(entity.id)) {
+        allStarredEvents.push(entity);
+      }
+    });
+    allStarredEvents.sort((a, b) => +b.dbCreateDate - +a.dbCreateDate);
+    return allStarredEvents;
   }
 
   add(recentEvent: Event) {
