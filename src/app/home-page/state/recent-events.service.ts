98d7c6c30a242340407ec1b7eba3d808290acf23
garyluu <garylau.work@gmail.com>
2020-01-09 15:30:20 -0500

    Use new enum instead

--- src/app/home-page/state/recent-events.service.ts
+++ src/app/home-page/state/recent-events.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { Event, EventsService } from 'app/shared/openapi';
-import { forkJoin } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { RecentEventsStore } from './recent-events.store';
 
@@ -10,26 +9,11 @@ export class RecentEventsService {
   constructor(private recentEventsStore: RecentEventsStore, private eventsService: EventsService) {}
 
   get() {
-    return forkJoin([this.eventsService.getEvents('STARRED_ENTRIES'), this.eventsService.getEvents('STARRED_ORGANIZATION')]).pipe(
-      tap(([starredEntriesEvents, starredOrganizationEvents]) => {
-        const allStarredEvents: Array<Event> = this.cleanUpEvents(starredEntriesEvents, starredOrganizationEvents);
+    return this.eventsService.getEvents('ALL_STARRED').pipe(
+      tap(allStarredEvents => {
         this.recentEventsStore.set(allStarredEvents);
       })
     );
-  }
-
-  private cleanUpEvents(starredEntriesEvents: Event[], starredOrganizationEvents: Event[]) {
-    let allStarredEvents: Array<Event> = [];
-    // TODO: Need a less terrible way of getting unique events
-    allStarredEvents = allStarredEvents.concat(starredEntriesEvents);
-    const ids = allStarredEvents.map(ent => ent.id);
-    starredOrganizationEvents.forEach(entity => {
-      if (!ids.includes(entity.id)) {
-        allStarredEvents.push(entity);
-      }
-    });
-    allStarredEvents.sort((a, b) => +b.dbCreateDate - +a.dbCreateDate);
-    return allStarredEvents;
   }
 
   add(recentEvent: Event) {
