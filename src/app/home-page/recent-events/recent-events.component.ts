95c3a384af3cad98a525838694143e522baf3ec1
garyluu <garylau.work@gmail.com>
2020-01-15 15:08:19 -0500

    Add links to everything

--- src/app/home-page/recent-events/recent-events.component.ts
+++ src/app/home-page/recent-events/recent-events.component.ts
@@ -9,8 +9,6 @@ import { RecentEventsService } from '../state/recent-events.service';
  * Shows recent events related to starred organization and entries
  * Only shows when a child has been added to parent (version added to entry, collection added to organization, entry added to collection)
  * TODO: collapse events with the same date (refresh all adding multiple versions at the same time)
- * TODO: Remove non-tag versions added to entry
- * TODO: Actually link out to the organization, collection, or entry when a child has been added to it
  */
 @Component({
   selector: 'recent-events',
