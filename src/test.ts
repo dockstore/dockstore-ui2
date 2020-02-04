63175e9967b96ea7260c917b653825fb56eb1e9c
garyluu <garylau.work@gmail.com>
2020-02-04 12:00:53 -0500

    Fix zone.js

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// zone.js must be at the top
+/* tslint:disable:ordered-imports */
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
 declare var require: any;
