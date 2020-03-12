d21e2a2f43944a7fc2f24d11c00756e04ee199f8
garyluu <garylau.work@gmail.com>
2020-03-12 16:10:39 -0400

    Flexbox the footer

--- src/app/container/refresh-wizard.module.ts
+++ src/app/container/refresh-wizard.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RefreshAlertModule } from 'app/shared/alert/alert.module';
 import { CustomMaterialModule } from 'app/shared/modules/material.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { RefreshWizardComponent } from './refresh-wizard/refresh-wizard.component';
-import { RefreshWizardQuery } from './state/refresh-wizard.query';
-import { RefreshWizardService } from './state/refresh-wizard.service';
-import { RefreshWizardStore } from './state/refresh-wizard.store';
 
 @NgModule({
-  imports: [CommonModule, RefreshAlertModule, CustomMaterialModule],
+  imports: [CommonModule, RefreshAlertModule, CustomMaterialModule, FlexLayoutModule],
   declarations: [RefreshWizardComponent],
   exports: [RefreshWizardComponent]
 })
