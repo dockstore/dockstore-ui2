a69765eaaa2015101bbcaed6bd1b1c972deeb64c
Charles Overbeck <coverbec@ucsc.edu>
2020-05-01 13:04:21 -0700

    Add logout page (#975)
    * Add logout page
    
    dockstore/dockstore#3363
    
    Verbiage warns user that they may not be as logged out as they think.
    
    In looking at code to understand how to do this, noticed an unused field
    in track-login.service.ts, so removed it. Also took IDE recommendation
    in that file to shorten import.
    
    PR Feedback -- Don't use Bootstrap

---
+++ src/app/logout/logout.component.spec.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoutComponent } from './logout.component';
+
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+class HeaderComponent {}
+
+describe('LogoutComponent', () => {
+  let component: LogoutComponent;
+  let fixture: ComponentFixture<LogoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LogoutComponent, HeaderComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
