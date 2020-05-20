a48d88907a66447b6bdcfcd9d08ed725f254dbf5
Emily Soth <esoth@ucsc.edu>
2020-05-20 09:15:43 -0700

    try to register a tool

--- cypress/integration/manualTests/auth-tests.ts
+++ cypress/integration/manualTests/auth-tests.ts
@@ -1,5 +1,13 @@
 describe('Test cypress env token', () => {
   it('is accessible in the browser', () => {
     cy.log(Cypress.env('TOKEN'));
+    window.localStorage.setItem('ng2-ui-auth_token', Cypress.env('TOKEN'));
+    cy.visit('/my-tools');
+    cy.contains('button', 'Add Tool').click();
+    cy.contains('mat-radio-button', 'Quickly register Quay.io tools').click();
+    cy.contains('button', 'Next').click();
+    cy.get('[data-cy=dropdownOrg]')
+      .first()
+      .click();
   });
 });
