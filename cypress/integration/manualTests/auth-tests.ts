4e2d311ae2a974cd978ed70847157fe58eaa624d
Emily Soth <esoth@ucsc.edu>
2020-05-20 11:38:17 -0700

    register, publish, unpublish, and delete a tool

--- cypress/integration/manualTests/auth-tests.ts
+++ cypress/integration/manualTests/auth-tests.ts
@@ -1,13 +1,27 @@
 describe('Test cypress env token', () => {
-  it('is accessible in the browser', () => {
+  it('register and publish a tool', () => {
     cy.log(Cypress.env('TOKEN'));
     window.localStorage.setItem('ng2-ui-auth_token', Cypress.env('TOKEN'));
     cy.visit('/my-tools');
-    cy.contains('button', 'Add Tool').click();
+    // click thru the steps of registering a tool
+    cy.get('#register_tool_button').click();
     cy.contains('mat-radio-button', 'Quickly register Quay.io tools').click();
     cy.contains('button', 'Next').click();
-    cy.get('[data-cy=dropdownOrg]')
-      .first()
-      .click();
+    cy.contains('mat-form-field', 'Select namespace').click();
+    cy.contains('mat-option', 'emlys').click();
+    cy.contains('div', 'dockstore-tool-md5sum').within(() => {
+      cy.get('[matTooltip="Refresh on Dockstore"]').click();
+    });
+    cy.contains('button', 'Finish').click();
+    cy.contains('button', 'Publish').click();
+  });
+  it('unpublish and delete the tool', () => {
+    cy.contains('button', 'Unpublish').click();
+    cy.contains('button', 'Delete').should('not.be.disabled');
+    cy.contains('button', 'Delete').click();
+    cy.contains('div', 'Are you sure you wish to delete this tool?').within(() => {
+      cy.contains('button', 'Delete').click();
+    });
+    cy.contains('There are currently no tools registered under this account');
   });
 });
