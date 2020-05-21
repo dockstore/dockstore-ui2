ac613c56685b9f99bf92e181f982f94718d3c672
garyluu <garylau.work@gmail.com>
2020-05-21 14:55:26 -0400

    Fix again

--- scripts/generate-openapi-script.sh
+++ scripts/generate-openapi-script.sh
@@ -8,14 +8,14 @@ GENERATOR_VERSION="3.3.4"
 BASE_PATH="https://raw.githubusercontent.com/dockstore/dockstore/$npm_package_config_webservice_version"
 # DOCKSTORE-2428 - demo how to add new workflow language, generate from local copy of swagger
 # BASE_PATH="../dockstore"
-PATH="https://3855-33383826-gh.circle-artifacts.com/0/tmp/artifacts"
+CIRCLE_CI_PATH="https://3855-33383826-gh.circle-artifacts.com/0/tmp/artifacts"
 
 wget --no-verbose https://repo.maven.apache.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 rm -Rf src/app/shared/swagger
 rm -Rf src/app/shared/openapi
 #java -jar openapi-generator-cli.jar generate -i ${BASE_PATH}/dockstore-webservice/src/main/resources/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
 #java -jar openapi-generator-cli.jar generate -i ${BASE_PATH}/dockstore-webservice/src/main/resources/openapi3/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
-java -jar openapi-generator-cli.jar generate -i ${PATH}/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
-java -jar openapi-generator-cli.jar generate -i ${PATH}/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
+java -jar openapi-generator-cli.jar generate -i ${CIRCLE_CI_PATH}/swagger.yaml -l typescript-angular -o src/app/shared/swagger -c swagger-config.json
+java -jar openapi-generator-cli.jar generate -i ${CIRCLE_CI_PATH}/openapi.yaml -l typescript-angular -o src/app/shared/openapi -c swagger-config.json
 rm openapi-generator-cli.jar
 
