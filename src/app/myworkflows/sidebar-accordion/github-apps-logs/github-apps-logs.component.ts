21b0eb6fa8873636e01a75a2482c09a04a8ef5b3
garyluu <garylau.work@gmail.com>
2020-06-03 12:37:32 -0400

    Add expanded message

--- src/app/myworkflows/sidebar-accordion/github-apps-logs/github-apps-logs.component.ts
+++ src/app/myworkflows/sidebar-accordion/github-apps-logs/github-apps-logs.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { LambdaEvent, LambdaEventsService } from 'app/shared/openapi';
@@ -5,13 +6,23 @@ import { LambdaEvent, LambdaEventsService } from 'app/shared/openapi';
 @Component({
   selector: 'app-github-apps-logs',
   templateUrl: './github-apps-logs.component.html',
-  styleUrls: ['./github-apps-logs.component.scss']
+  styleUrls: ['./github-apps-logs.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
+  ]
 })
 export class GithubAppsLogsComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'githubUsername', 'repository', 'reference', 'success', 'type', 'message'];
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string, private lambdaEventsService: LambdaEventsService) {}
+  columnsToDisplay: string[] = ['repository', 'reference', 'success', 'type'];
+  displayedColumns: string[] = ['dbCreateDate', 'githubUsername', ...this.columnsToDisplay];
   lambdaEvents: LambdaEvent[];
   public LambdaEvent = LambdaEvent;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: string, private lambdaEventsService: LambdaEventsService) {}
+  expandedElement: LambdaEvent | null;
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
 
   ngOnInit() {
     console.log(this.data);
