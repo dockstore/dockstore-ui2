64bc6e710135caf84da826482a92121b57f24307
NatalieEO <nperez9@ucsc.edu>
2020-06-03 16:15:58 -0700

    PR feedback

--- src/app/workflow/register-workflow-modal/register-workflow-modal.component.ts
+++ src/app/workflow/register-workflow-modal/register-workflow-modal.component.ts
@@ -54,7 +54,7 @@ export class RegisterWorkflowModalComponent implements OnInit, AfterViewChecked,
   public isModalShown: boolean;
   public isRefreshing$: Observable<boolean>;
   public descriptorValidationPattern;
-  public workflowPathError: String;
+  public workflowPathError: String | null;
   public descriptorLanguages$: Observable<Array<Workflow.DescriptorTypeEnum>>;
   public Tooltip = Tooltip;
   public workflowPathPlaceholder: string;
@@ -135,7 +135,6 @@ export class RegisterWorkflowModalComponent implements OnInit, AfterViewChecked,
       }
     });
     this.selectInitialSourceControlRepository();
-    this.workflowPathError = validationMessages.cwlPath.pattern;
   }
 
   /**
@@ -239,14 +238,22 @@ export class RegisterWorkflowModalComponent implements OnInit, AfterViewChecked,
     this.descriptorValidationPattern = DescriptorLanguageService.workflowDescriptorTypeEnumToExtendedDescriptorLanguageBean(
       descriptorType
     ).descriptorPathPattern;
-    if (descriptorType === Workflow.DescriptorTypeEnum.CWL) {
-      this.workflowPathError = validationMessages.cwlPath.pattern;
-    } else if (descriptorType === Workflow.DescriptorTypeEnum.WDL) {
-      this.workflowPathError = validationMessages.wdlPath.pattern;
-    } else if (descriptorType === Workflow.DescriptorTypeEnum.NFL) {
-      this.workflowPathError = validationMessages.nflPath.pattern;
-    } else if (descriptorType === Workflow.DescriptorTypeEnum.Gxformat2) {
-      this.workflowPathError = validationMessages.galaxyPath.pattern;
+    switch (descriptorType) {
+      case Workflow.DescriptorTypeEnum.CWL:
+        this.workflowPathError = validationMessages.cwlPath.pattern;
+        break;
+      case Workflow.DescriptorTypeEnum.WDL:
+        this.workflowPathError = validationMessages.wdlPath.pattern;
+        break;
+      case Workflow.DescriptorTypeEnum.NFL:
+        this.workflowPathError = validationMessages.nflPath.pattern;
+        break;
+      case Workflow.DescriptorTypeEnum.Gxformat2:
+        this.workflowPathError = validationMessages.galaxyPath.pattern;
+        break;
+      default:
+        console.error('Unknown descriptor type: ' + descriptorType);
+        this.workflowPathError = null;
     }
     this.workflowPathPlaceholder = this.getWorkflowPathPlaceholder(this.workflow.descriptorType);
   }
