c7a738866243fd35ffa53610b5341cd987a02c1b
NatalieEO <nperez9@ucsc.edu>
2020-07-30 15:25:46 -0700

    partial fixes to cypress tests

--- scripts/run-webservice-script.sh
+++ scripts/run-webservice-script.sh
@@ -6,7 +6,7 @@ set -o xtrace
 # Uncomment this to use the actual Dockstore webservice from the package.json
 #JAR_PATH="https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${npm_package_config_webservice_version}/dockstore-webservice-${npm_package_config_webservice_version}.jar"
 # Uncomment this to use the CircleCI jar 
-JAR_PATH="https://4398-33383826-gh.circle-artifacts.com/0/tmp/artifacts/dockstore-webservice-1.10.0-alpha.0-SNAPSHOT.jar"
+JAR_PATH="https://4430-33383826-gh.circle-artifacts.com/0/tmp/artifacts/dockstore-webservice-1.10.0-alpha.0-SNAPSHOT.jar"
 
 wget -O dockstore-webservice.jar --no-verbose --tries=10 ${JAR_PATH}
 chmod u+x dockstore-webservice.jar
