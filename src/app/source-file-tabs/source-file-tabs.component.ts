319f8ba4d3961a6c31705055c102072229ea928f
Gary Luu <garylau.work@gmail.com>
2020-08-12 13:06:51 -0400

    Use ngOnChanges (#1044)
    * Use ngOnChanges
    
    * Don't initialize component when version is null

--- src/app/source-file-tabs/source-file-tabs.component.ts
+++ src/app/source-file-tabs/source-file-tabs.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatSelectChange, MatTabChangeEvent } from '@angular/material';
+import { Component, Input, OnChanges } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 import { SafeUrl } from '@angular/platform-browser';
 import { FileService } from 'app/shared/file.service';
 import { SourceFile, ToolDescriptor, WorkflowVersion } from 'app/shared/openapi';
@@ -12,18 +13,13 @@ import { SourceFileTabsService } from './source-file-tabs.service';
   templateUrl: './source-file-tabs.component.html',
   styleUrls: ['./source-file-tabs.component.scss']
 })
-export class SourceFileTabsComponent implements OnInit {
+export class SourceFileTabsComponent implements OnChanges {
   @Input() workflowId: number;
   @Input() descriptorType: ToolDescriptor.TypeEnum;
-  @Input() set version(value: WorkflowVersion) {
-    if (value != null) {
-      this.currentVersion = value;
-      this.setupVersionFileTabs();
-    }
-  }
+  // Version is strictly non-null because everything that uses this component has a truthy-check guard
+  @Input() version: WorkflowVersion;
   loading = true;
   displayError = false;
-  currentVersion: WorkflowVersion;
   files: SourceFile[];
   filteredFiles: SourceFile[];
   currentFile: SourceFile;
@@ -36,7 +32,7 @@ export class SourceFileTabsComponent implements OnInit {
 
   constructor(private fileService: FileService, private sourceFileTabsService: SourceFileTabsService) {}
 
-  ngOnInit() {
+  ngOnChanges() {
     this.setupVersionFileTabs();
   }
 
@@ -44,7 +40,7 @@ export class SourceFileTabsComponent implements OnInit {
     this.loading = true;
     this.displayError = false;
     this.sourceFileTabsService
-      .getSourceFiles(this.workflowId, this.currentVersion.id)
+      .getSourceFiles(this.workflowId, this.version.id)
       .pipe(
         finalize(() => {
           this.loading = false;
@@ -59,7 +55,7 @@ export class SourceFileTabsComponent implements OnInit {
           this.files = sourceFiles;
           this.fileTypes = fileTypes;
         },
-        error => {
+        () => {
           this.displayError = true;
         }
       );
@@ -71,7 +67,7 @@ export class SourceFileTabsComponent implements OnInit {
    */
   changeFileType(fileType: SourceFile.TypeEnum, files: SourceFile[]) {
     let validationMessage = null;
-    this.currentVersion.validations.forEach((validation: Validation) => {
+    this.version.validations.forEach((validation: Validation) => {
       if (validation.type === fileType && !validation.valid) {
         validationMessage = JSON.parse(validation.message);
       }
@@ -92,7 +88,7 @@ export class SourceFileTabsComponent implements OnInit {
   selectFile(file: SourceFile) {
     this.customDownloadHREF = this.fileService.getFileData(file.content);
     this.customDownloadPath = this.fileService.getFileName(file.path);
-    this.filePath = this.sourceFileTabsService.getDescriptorPath(this.descriptorType, file.path, this.currentVersion.name);
+    this.filePath = this.sourceFileTabsService.getDescriptorPath(this.descriptorType, file.path, this.version.name);
     this.currentFile = file;
   }
 
