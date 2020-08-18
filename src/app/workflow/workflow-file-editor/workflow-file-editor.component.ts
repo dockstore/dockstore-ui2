5d6b6f81e31b18ac7d5c357d0157912fb3972c76
Andy Chen <achen81@ucsc.edu>
2020-08-18 12:37:37 -0700

    Changed the message to be more consistent

--- src/app/workflow/workflow-file-editor/workflow-file-editor.component.ts
+++ src/app/workflow/workflow-file-editor/workflow-file-editor.component.ts
@@ -142,7 +142,7 @@ export class WorkflowFileEditorComponent extends FileEditing {
       },
       (error: HttpErrorResponse) => {
         if (error.status === 413) {
-          this.alertService.detailedError(error, 'Cannot upload new version: new version would exceed the 100K byte limit');
+          this.alertService.detailedError(error, 'Cannot upload new version: new version would exceed the 60 kilobyte limit');
         } else if (error) {
           this.alertService.detailedError(error);
         }
