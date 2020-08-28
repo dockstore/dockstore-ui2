fe0fa64decebc95c454788c8a090da2257bd6e1a
Andy Chen <achen81@ucsc.edu>
2020-08-28 15:17:51 -0700

    Removed unused import

--- src/app/loginComponents/requests/requests.component.ts
+++ src/app/loginComponents/requests/requests.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { ConfirmationDialogData } from '../../confirmation-dialog/confirmation-dialog.component';
 import { ConfirmationDialogService } from '../../confirmation-dialog/confirmation-dialog.service';
 import { Base } from '../../shared/base';
