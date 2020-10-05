2c5a1811ba4ad2efd926c2995541f546d7786b3a
Richard Hansen <RichardHansen@genomicss-mbp.lan>
2020-10-05 09:02:33 -0700

    Changed iD to credentials

--- src/app/loginComponents/accounts/external/accounts.component.ts
+++ src/app/loginComponents/accounts/external/accounts.component.ts
@@ -89,7 +89,7 @@ export class AccountsExternalComponent implements OnInit, OnDestroy {
       name: 'ORCID',
       source: TokenSource.ORCID,
       bold: '',
-      message: 'ORCID iDs are used for providing links from your Dockstore contributions to your ORCID account.',
+      message: 'ORCID credentials are used for providing links from your Dockstore contributions to your ORCID account.',
       show: false,
       logo: 'orcid.svg',
     },
