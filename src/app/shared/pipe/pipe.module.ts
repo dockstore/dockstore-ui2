8de4d47ccbc25e286ea961f892c564524222ac8c
nrzhao4 <50606789+nrzhao4@users.noreply.github.com>
2020-11-16 13:37:51 -0500

    Feature/2062/facet search (#1107)
    * search and autocomplete
    
    * calculate number of hidden items
    
    * update comment
    
    * change autocomplete logic
    
    * create consts

--- src/app/shared/pipe/pipe.module.ts
+++ src/app/shared/pipe/pipe.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 import { GithubNameToIdPipe } from 'app/github-name-to-id.pipe';
 import { FilePathPipe } from '../../entry/file-path.pipe';
 import { TimeAgoMsgPipe } from '../../organizations/organization/time-ago-msg.pipe';
+import { GetFacetSearchUpdatePipe } from '../../search/facet-search/facet-search-update.pipe';
+import { GetFacetSearchResultsPipe } from '../../search/facet-search/facet-search.pipe';
 import { GetHistogramWidthPipe } from '../../search/get-histogram-width.pipe';
 import { MapFriendlyValuesPipe } from '../../search/map-friendly-values.pipe';
 import { SelectTabPipe } from '../entry/select-tab.pipe';
 
-const DECLARATIONS: any[] = [FilePathPipe, MapFriendlyValuesPipe, SelectTabPipe, TimeAgoMsgPipe, GithubNameToIdPipe, GetHistogramWidthPipe];
+const DECLARATIONS: any[] = [
+  FilePathPipe,
+  MapFriendlyValuesPipe,
+  SelectTabPipe,
+  TimeAgoMsgPipe,
+  GithubNameToIdPipe,
+  GetHistogramWidthPipe,
+  GetFacetSearchResultsPipe,
+  GetFacetSearchUpdatePipe,
+];
 @NgModule({
   imports: [CommonModule],
   declarations: DECLARATIONS,
