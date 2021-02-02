bab5ae4615e5338427b60f48a59b6ce938189b7a
NatalieEO <nperez9@ucsc.edu>
2021-02-02 12:27:09 -0800

    fix license checker script, definitely over thought this originally

--- scripts/detect-package-json-changes.sh
+++ scripts/detect-package-json-changes.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-packageJsonStatus="$(git diff --name-status develop -- package.json)"
-packageLockJsonStatus="$(git diff --name-status develop -- package-lock.json)"
+license-checker  --excludePackages dockstore-ui2@2.3.0 --csv > CIRCLE-THIRD-PARTY-LICENSES.csv
+licenseFileStatus="$(diff CIRCLE-THIRD-PARTY-LICENSES.csv THIRD-PARTY-LICENSES.csv)"
 
-if [ -n "$packageJsonStatus" ] && [ -n "$packageLockJsonStatus" ]
-then
-    licenseFileStatus="$(git diff --name-status develop -- THIRD-PARTY-LICENSES.csv)"
-    if [ -z "$licenseFileStatus" ]
+if [ -n "$licenseFileStatus" ]
     then
-        echo "The package.json and package-lock.json files have changed, but the THIRD-PARTY-LICENSES.csv file has not been updated. Update by running 'npm run license'. Review changes being made before committing and pushing."
+        echo "The THIRD-PARTY-LICENSES.csv file has not been updated. Update the file by running 'npm run license'."
         exit 1
     fi
-fi
