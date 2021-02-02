68a3bfb6def2013e962f6bee3b6d8fee08098e2b
NatalieEO <nperez9@ucsc.edu>
2021-02-02 13:30:03 -0800

    try using command from package.json

--- scripts/detect-package-json-changes.sh
+++ scripts/detect-package-json-changes.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-npx license-checker  --excludePackages dockstore-ui2@2.3.0 --csv > CIRCLE-THIRD-PARTY-LICENSES.csv
 licenseFileStatus="$(diff CIRCLE-THIRD-PARTY-LICENSES.csv THIRD-PARTY-LICENSES.csv)"
 
 if [ -n "$licenseFileStatus" ]
     then
-        echo "The THIRD-PARTY-LICENSES.csv file has not been updated. Update the file by running 'npm run license'."
+        echo "The THIRD-PARTY-LICENSES.csv file has not been updated. Update the file by running 'npm run license > THIRD-PARTY-LICENSES.csv'."
         exit 1
     fi
