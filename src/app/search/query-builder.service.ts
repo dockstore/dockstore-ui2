5fc16bc7e518d048456b5dc463174e8128569f68
Gary Luu <garylau.work@gmail.com>
2021-02-08 18:40:27 -0500

    Fix for text search

--- src/app/search/query-builder.service.ts
+++ src/app/search/query-builder.service.ts
@@ -198,23 +198,24 @@ export class QueryBuilderService {
   /**
    * Appends search-everything filter to the query
    * Currently searches sourcefiles, description, labels, author, and path
-   * @param {*} body The original body
-   * @param {string} searchString the search string
-   * @memberof QueryBuilderService
+   * Some requiremnts:
+   * 1. Need to be able to match substring (ex. "chicken pot pie" should match "pot")
+   * 2. Need to be able to handle slashes (ex. "beef/stew" should match "beef/stew")
+   * A query (instead of filter) is used to handle #1 because wildcard does work for filters
+   * The paths use keyword instead of string because #2 wouldn't work otherwise
+   *
+   * @param body Body from the Bodybuilder package which will be mutated
+   * @param searchString The string entered into the basic search bar by the user
    */
-  searchEverything(body: any, searchString: string): void {
-    body = body.filter('bool', (filter) =>
-      filter
-        .orFilter('bool', (workflowVersionsFileContent) =>
-          workflowVersionsFileContent.filter('match_phrase', 'workflowVersions.sourceFiles.content', searchString)
-        )
-        .orFilter('bool', (tagsFileContent) => tagsFileContent.filter('match_phrase', 'tags.sourceFiles.content', searchString))
-        .orFilter('bool', (descriptionFilter) => descriptionFilter.filter('match_phrase', 'description', searchString))
-        .orFilter('bool', (labelsFilter) => labelsFilter.filter('match_phrase', 'labels', searchString))
-        .orFilter('bool', (authorFilter) => authorFilter.filter('match_phrase', 'author', searchString))
-        .orFilter('bool', (pathFilter) => pathFilter.filter('match_phrase', 'tool_path', searchString))
-        .orFilter('bool', (pathFilter) => pathFilter.filter('match_phrase', 'full_workflow_path', searchString))
-    );
+  searchEverything(body: bodybuilder.Bodybuilder, searchString: string): void {
+    body = body
+      .orQuery('wildcard', { 'full_workflow_path.keyword': { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { 'tool_path.keyword': { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { 'workflowVersions.sourceFiles.content': { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { 'tags.sourceFiles.content': { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { description: { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { labels: { value: '*' + searchString + '*', case_insensitive: true } })
+      .orQuery('wildcard', { author: { value: '*' + searchString + '*', case_insensitive: true } });
   }
 
   /**===============================================
