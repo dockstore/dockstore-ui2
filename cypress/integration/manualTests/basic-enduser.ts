b39186034b63075180dec822e2b7efc1321a2883
NatalieEO <nperez9@ucsc.edu>
2021-02-08 10:55:04 -0800

    only test in prod

--- cypress/integration/manualTests/basic-enduser.ts
+++ cypress/integration/manualTests/basic-enduser.ts
@@ -148,30 +148,6 @@ describe('Test workflow page functionality', () => {
       });
   });
 });
-//
-describe('Test existence of Logs', () => {
-  it('Find Logs in Workflows', () => {
-    cy.visit('/workflows/github.com/DataBiosphere/topmed-workflows/UM_variant_caller_wdl:1.32.0?tab=info');
-    cy.get('[data-cy=verificationLogsDialog]').click();
-
-    cy.contains('.mat-card-title', 'Verification Information');
-    cy.get('.mat-table')
-      .first()
-      .within(() => {
-        cy.get('.mat-row').should('have.length.of.at.least', 1);
-        cy.contains('Dockstore CLI');
-      });
-
-    cy.contains('.mat-card-title', 'Logs');
-    cy.get('.mat-table')
-      .eq(1)
-      .within(() => {
-        cy.get('.mat-row').should('have.length.of.at.least', 3);
-        cy.contains('variant-caller/variant-caller-wdl/topmed_freeze3_calling.json');
-        cy.contains('View FULL log');
-      });
-  });
-});
 
 // pairs of [workflow URL without version number, verified version number, another verified version number, workflow.trsUrl]
 const workflowVersionTuples = [
