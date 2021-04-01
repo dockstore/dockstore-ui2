6bff29475308ed4706b64c76c29e1f9b47d0f95c
Wendy Zhang <57231021+wendidy@users.noreply.github.com>
2021-04-01 14:49:27 -0400

    added workflow length display and test cases in monitor.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Funder, FundingComponent } from '../funding/funding.component';
+import { CollectionsQuery } from '../organizations/state/collections.query';
+import { CollectionsService } from '../organizations/state/collections.service';
 import { Dockstore } from '../shared/dockstore.model';
 import { Sponsor } from '../sponsors/sponsor.model';
 
@@ -8,11 +12,14 @@ import { Sponsor } from '../sponsors/sponsor.model';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  providers: [FundingComponent]
+  providers: [FundingComponent],
 })
 export class AboutComponent implements OnInit {
   Dockstore = Dockstore;
-  public safeURL: SafeResourceUrl;
+  private readonly BROAD_ORGANIZATION_ID = 16;
+  private readonly BROAD_COLLECTION_ID = 23;
+  public youtubeSafeURL: SafeResourceUrl;
+  public workflowsLength$: Observable<number>;
   public funders: Funder[];
   public partners: Sponsor[] = [
     new Sponsor('dnastack.png', new URL('https://dnastack.com')),
@@ -34,12 +41,31 @@ export class AboutComponent implements OnInit {
 
   constructor(
     private sanitizer: DomSanitizer,
-    private fundingComponent: FundingComponent
-    ) {
-    this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/shMr_Bd01Ko');
+    private fundingComponent: FundingComponent,
+    private collectionsQuery: CollectionsQuery,
+    private collectionsService: CollectionsService
+  ) {
+    this.youtubeSafeURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/shMr_Bd01Ko');
   }
 
   ngOnInit() {
     this.funders = this.fundingComponent.getFunders();
+
+    this.collectionsService.updateCollections(this.BROAD_ORGANIZATION_ID);
+    this.workflowsLength$ = this.collectionsQuery.collections$.pipe(
+      map((collections) => {
+        for (const index in collections) {
+          if (collections[index].id === this.BROAD_COLLECTION_ID) {
+            return collections[index].workflowsLength;
+          }
+        }
+      })
+    );
+    // this.collectionsService.updateCollectionFromId(this.BROAD_ORGANIZATION_ID, this.BROAD_COLLECTION_ID);
+    // this.workflowsLength$ = this.collectionsQuery.collections$.pipe(
+    //   map((collections) => {
+    //     return collections[this.BROAD_COLLECTION_ID].workflowsLength;
+    //   })
+    // );
   }
 }
