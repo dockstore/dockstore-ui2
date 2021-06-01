b787a27f1804e84342aad0b9c5caad65c53d54ce
Denis Yuen <denis.yuen@oicr.on.ca>
2021-06-01 19:05:43 -0400

    Ugly way of getting news and featured content to look like invision

---
+++ src/app/home-page/widget/featured-content/featured-news.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../../../shared/alert/state/alert.service';
+import { Dockstore } from '../../../shared/dockstore.model';
+
+@Component({
+  selector: 'featured-news',
+  template: ` <div [innerHTML]="myExternalHTML"></div> `,
+})
+export class FeaturedNewsComponent implements OnInit {
+  // TODO should parameterize FeaturedContentComponent or something
+  public myExternalHTML: any = '';
+
+  constructor(private http: HttpClient, private alertService: AlertService) {}
+  ngOnInit() {
+    this.alertService.start('Retrieve featured content');
+    this.http
+      .get(
+        Dockstore.FEATURED_NEWS_URL,
+        // set authorization header to empty, prevents ng2-ui interceptor from adding dockstore bearer token
+        { headers: { Authorization: '' }, responseType: 'text' }
+      )
+      .subscribe(
+        (data) => {
+          this.myExternalHTML = data;
+          this.alertService.simpleSuccess();
+        },
+        () => {
+          this.alertService.simpleError();
+        }
+      );
+  }
+}
