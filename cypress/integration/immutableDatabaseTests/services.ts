221f1cf059f1ffa978e5c9f0b67db03316fd40df
Gary Luu <garylau.work@gmail.com>
2021-07-13 13:02:18 -0400

    Revert tests completely

--- cypress/integration/immutableDatabaseTests/services.ts
+++ cypress/integration/immutableDatabaseTests/services.ts
@@ -136,7 +136,9 @@ describe('Dockstore Home', () => {
     cy.contains('README.md');
     cy.contains('# another-test-serviceaaaa');
 
-    cy.get('.mat-form-field-infix:visible').contains("README.md").click();
+    cy.get('mat-tab-body').within((tabBody) => {
+      cy.get('mat-select').click();
+    });
     cy.get('mat-option').contains('docker-compose.yml').click();
     cy.contains('docker-compose.yml');
 
