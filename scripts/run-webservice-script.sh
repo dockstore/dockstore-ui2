e182971b0a39ea268a985d09fe5114f201eb2319
Denis Yuen <denis.yuen@oicr.on.ca>
2021-08-24 10:31:06 -0400

    flip

--- scripts/run-webservice-script.sh
+++ scripts/run-webservice-script.sh
@@ -4,9 +4,9 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 # Uncomment this to use the actual Dockstore webservice from the package.json
-# JAR_PATH="https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${npm_package_config_webservice_version}/dockstore-webservice-${npm_package_config_webservice_version}.jar"
+JAR_PATH="https://artifacts.oicr.on.ca/artifactory/collab-release/io/dockstore/dockstore-webservice/${npm_package_config_webservice_version}/dockstore-webservice-${npm_package_config_webservice_version}.jar"
 # Uncomment this to use the CircleCI jar
-JAR_PATH="https://13342-33383826-gh.circle-artifacts.com/0/tmp/artifacts/dockstore-webservice-1.11.6.jar"
+# JAR_PATH="https://13342-33383826-gh.circle-artifacts.com/0/tmp/artifacts/dockstore-webservice-1.11.6.jar"
 
 wget -O dockstore-webservice.jar --no-verbose --tries=10 ${JAR_PATH}
 chmod u+x dockstore-webservice.jar
