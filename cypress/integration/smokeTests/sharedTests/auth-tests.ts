63f1842585a06fb93366643e65f2d753bf1122f0
richard-hansen <richie.hansen@gmail.com>
2021-11-02 17:14:39 -0400

    I've tricked myself with a timeout error

--- cypress/integration/smokeTests/sharedTests/auth-tests.ts
+++ cypress/integration/smokeTests/sharedTests/auth-tests.ts
@@ -288,7 +288,7 @@ function testCollection(org: string, collection: string, registry: string, repo:
   });
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   testCollection(collectionTuple[0], collectionTuple[1], toolTuple[0], toolTuple[1], toolTuple[2]);
   testTool(toolTuple[0], toolTuple[1], toolTuple[2]);
   testWorkflow(workflowTuple[0], workflowTuple[1], workflowTuple[2]);
