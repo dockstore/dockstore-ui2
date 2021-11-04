3096ebfbd11cf2e13a4a08ad1d72628de34b2aa5
Stephen Von Worley <svonworl@ucsc.edu>
2021-11-04 08:39:40 -0700

    change db load user

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -51,7 +51,7 @@ export function assertNoTab(tabName: string): any {
 export function resetDB() {
   before(() => {
     cy.exec('java -jar dockstore-webservice.jar db drop-all --confirm-delete-everything travisci/web.yml');
-    cy.exec(psqlInvocation + ' -h localhost webservice_test -U postgres < travisci/db_dump.sql');
+    cy.exec(psqlInvocation + ' -h localhost webservice_test -U dockstore < travisci/db_dump.sql');
     cy.exec(
       'java -jar dockstore-webservice.jar db migrate -i 1.5.0,1.6.0,1.7.0,1.8.0,1.9.0,1.10.0,alter_test_user_1.10.2,1.11.0,1.12.0 travisci/web.yml'
     );
