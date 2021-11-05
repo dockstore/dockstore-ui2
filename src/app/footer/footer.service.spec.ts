56243770e75028ecae81d15410dedbb6e76fb0cd
Gary Luu <garylau.work@gmail.com>
2021-11-04 23:21:14 -0400

    Fix tests

--- src/app/footer/footer.service.spec.ts
+++ src/app/footer/footer.service.spec.ts
@@ -12,12 +12,12 @@ describe('FooterService', () => {
 
   it('should generate markdown', () => {
     // sanity test the method; don't want to look for the exact value as that would just be writing it all over again
-    const markdown = service.versionsToMarkdown('981edd1', '2.6.1-39-g597aeeed', '1.9.0', '3723b1a"');
+    const markdown = service.versionsToMarkdown('981edd1', '2.6.1-39-g597aeeed', '1.9.0', '3723b1a"', null, null, null, null, null);
     expect(markdown).toContain('981edd1');
   });
 
   it('should handle nulls', () => {
-    const markdown = service.versionsToMarkdown(null, null, null, null);
+    const markdown = service.versionsToMarkdown(null, null, null, null, null, null, null, null, null);
     expect(markdown.length).toBeGreaterThan(100);
   });
 });
