21fe3e5247b8c89ee372b8d15e122e2800cf3249
Stephen Von Worley <svonworl@ucsc.edu>
2021-11-05 19:28:17 -0700

    spans -> buttons

--- src/app/category/category-link.service.ts
+++ src/app/category/category-link.service.ts
@@ -1,12 +1,15 @@
 // TODO add copyright
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Category } from '../shared/openapi';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryLinkService {
   constructor(
+    private router: Router,
   ) {}
 
-  goToCategorySearch(categoryName: string, entryType: string) {
-    window.location.href = '/search?categories.name.keyword=' + categoryName + '&searchMode=files&entryType=' + entryType;
+  goToCategoryPage(category: Category, entryType: string) {
+    this.router.navigate(['/search'], {'queryParams': {'categories.name.keyword': category.name, 'entryType': entryType, 'searchMode': 'files'}});
   }
 }
