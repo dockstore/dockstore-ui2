2577b467e5adeb5a131d1ad01ca7d07d6018ab08
NatalieEO <nperez9@ucsc.edu>
2021-11-16 15:56:28 -0800

    echo how many high vulns have been introduced

--- scripts/compare-num-vulns.sh
+++ scripts/compare-num-vulns.sh
@@ -3,10 +3,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-PR_NUM_VULNS=$(head -n 1 compare-num-vulnerabilities.txt)
-DEV_NUM_VULNS=$(sed -n '2p' compare-num-vulnerabilities.txt)
+#PR_NUM_VULNS=$(head -n 1 compare-num-vulnerabilities.txt)
+#DEV_NUM_VULNS=$(sed -n '2p' compare-num-vulnerabilities.txt)
 
 if [ "$PR_NUM_VULNS" -gt "$DEV_NUM_VULNS" ]; then
-  echo "You have introduced more high vulnerabilities."
+  echo "You have introduced $((PR_NUM_VULNS-DEV_NUM_VULNS)) high vulnerabilities."
   exit 1
   fi
