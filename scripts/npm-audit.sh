234e4980047a998aee66a3fa2168288c6dcca23b
NatalieEO <nperez9@ucsc.edu>
2021-11-30 11:50:52 -0800

    make case sensitive, ignore grep exit code

--- scripts/npm-audit.sh
+++ scripts/npm-audit.sh
@@ -3,8 +3,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-HIGH_VULN_DEV="$(grep -o -i High vulnerabilities.txt | wc -l)"
-CRITICAL_VULN_DEV="$(grep -o -i Critical vulnerabilities.txt | wc -l)"
+HIGH_VULN_DEV="$(grep -o High vulnerabilities.txt | wc -l || true)"
+CRITICAL_VULN_DEV="$(grep -o Critical vulnerabilities.txt | wc -l || true)"
 
 echo $HIGH_VULN_DEV >> compare-num-vulnerabilities.txt
 echo $CRITICAL_VULN_DEV >> compare-num-vulnerabilities.txt
