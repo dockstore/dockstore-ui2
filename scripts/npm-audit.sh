932d83d24b0e3172f9cb4847d0078fecbcfec27c
NatalieEO <nperez9@ucsc.edu>
2021-11-30 10:19:42 -0800

    PR feedback

--- scripts/npm-audit.sh
+++ scripts/npm-audit.sh
@@ -4,5 +4,7 @@ set -o pipefail
 set -o nounset
 
 HIGH_VULN_DEV="$(grep -o -i High vulnerabilities.txt | wc -l)"
+CRITICAL_VULN_DEV="$(grep -o -i Critical vulnerabilities.txt | wc -l)"
 
 echo $HIGH_VULN_DEV >> compare-num-vulnerabilities.txt
+echo $CRITICAL_VULN_DEV >> compare-num-vulnerabilities.txt
