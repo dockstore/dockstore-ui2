4ce46bfd9ca825748df73a79598809e9b249a28c
Charles Reid <chmreid@ucsc.edu>
2021-12-02 10:02:57 -0800

    eliminate shared code with function

--- cypress/integration/smokeTests/sharedTests/secheaders.ts
+++ cypress/integration/smokeTests/sharedTests/secheaders.ts
@@ -4,6 +4,17 @@ before(() => {
   cy.visit('');
 });
 
+// Function to check that the value of the specified response header
+// contains the substring "containsValue".
+function checkHeaderValueContains(url: string, headerKey: string, containsValue: string) {
+    cy.request(url).then((resp) => {
+        expect(resp.headers).to.have.property(headerKey);
+        const headerValue = resp.headers[headerKey];
+        expect(headerValue).to.include(containsValue);
+    });
+}
+
+
 describe('Test for security headers', () => {
   it('Check for presence of headers in response', () => {
     cy.request('/').then((resp) => {
@@ -11,17 +22,20 @@ describe('Test for security headers', () => {
     });
   });
 
-  it('Check for x-xss-protection header in response', () => {
-    cy.request('/').then((resp) => {
-        expect(resp.headers).to.have.property('x-xss-protection');
-    });
+  // ----------------
+  // x-xss-protection
+  // stop pages from loading when xss attacks detected
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+  it('Check x-xss-protection headers in response', () => {
+    checkHeaderValueContains('/', 'x-xss-protection', 'mode=block');
   });
 
-  it('Check that x-xss-protection header has correct value', () => {
-    cy.request('/').then((resp) => {
-        const headerval = resp.headers['x-xss-protection'];
-        expect(headerval).to.include('mode=block');
-    });
+  // ---------
+  // expect-ct
+  // reporting and enforcement of certificate transparency requirements
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expect-CT
+  it('Check expect-ct headers in response', () => {
+    checkHeaderValueContains('/', 'expect-ct', 'enforce, max-age=');
   });
 
 });
