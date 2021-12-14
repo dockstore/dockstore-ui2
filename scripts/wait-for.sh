7bcc1e3a367104e421517ed6a89a8bf6ec730eb7
richard-hansen <richie.hansen@gmail.com>
2021-12-14 12:42:04 -0500

    moved wait for ES to this script

--- scripts/wait-for.sh
+++ scripts/wait-for.sh
@@ -5,3 +5,4 @@ set -o nounset
 set -o xtrace
 wget --output-document /dev/null --waitretry=5 --tries=10 --retry-connrefused localhost:4200 || true
 wget --output-document /dev/null --waitretry=5 --tries=10 --retry-connrefused localhost:8080 || true
+wget --output-document /dev/null --waitretry=5 --tries=10 --retry-connrefused localhost:9200 || true
