bd6258e157f37e10cb46badcd44e53b7cc27ffb6
Stephen Von Worley <svonworl@ucsc.edu>
2022-01-14 15:51:48 -0800

    change edit collection dialog save button label

--- src/app/organizations/collections/state/create-collection.query.ts
+++ src/app/organizations/collections/state/create-collection.query.ts
@@ -8,6 +8,7 @@ import { CreateCollectionState, CreateCollectionStore } from './create-collectio
 export class CreateCollectionQuery extends Query<CreateCollectionState> {
   loading$: Observable<boolean> = this.selectLoading();
   title$: Observable<string> = this.select((state) => state.title);
+  saveLabel$: Observable<string> = this.select((state) => state.saveLabel);
   constructor(protected store: CreateCollectionStore) {
     super(store);
   }
