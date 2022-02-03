ce7fcda9ced932fa639283361327abf4046180d4
Stephen Von Worley <svonworl@ucsc.edu>
2022-02-02 19:06:02 -0800

    remove duplicate code, move comments

--- src/app/shared/img-fallback.directive.ts
+++ src/app/shared/img-fallback.directive.ts
@@ -12,6 +12,9 @@ export class ImgFallbackDirective {
     this.img = elementRef.nativeElement;
   }
   @HostListener('error') onError() {
+    // If we're here, the image referenced by img.src was not loadable.
+    // Set img.src to the fallback URL once, and once only, to avoid an infinite load loop if the fallback image is not loadable
+    // IMPORTANT: BEFORE YOU CHANGE THE BELOW CODE, MAKE SURE YOU COMPLETELY UNDERSTAND IT!
     if (!this.changed) {
       this.changed = true;
       this.img.src = this.fallback;
--- src/app/shared/org-logo.service.ts
+++ src/app/shared/org-logo.service.ts
@@ -2,14 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrgLogoService {
+  // default org logo image URL, including enough parent directory references to ensure that '/assets' is always relative to the root
   readonly DEFAULT_URL = '../../../../assets/images/dockstore/default-org-logo.svg';
-  setDefault(img: any) {
-    // Check/set a flag to ensure the default logo is set only once, to avoid an infinite load loop if the default org image is not loadable
-    // IMPORTANT: BEFORE YOU CHANGE THE BELOW CODE, MAKE SURE YOU COMPLETELY UNDERSTAND IT!
-    if (!img.changed) {
-      img.changed = true;
-      // Set the default org image, including enough parent directory references to ensure that '/assets' is always relative to the root
-      img.src = '../../../../assets/images/dockstore/default-org-logo.svg';
-    }
-  }
 }
