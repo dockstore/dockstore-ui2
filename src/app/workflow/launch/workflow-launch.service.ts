dad87d7d594dc1dd4db38fe3f9c3f0b465353e9d
Gary Luu <garylau.work@gmail.com>
2022-02-03 10:56:48 -0500

    Update workflow-launch.service.ts

--- src/app/workflow/launch/workflow-launch.service.ts
+++ src/app/workflow/launch/workflow-launch.service.ts
@@ -43,7 +43,6 @@ export class WorkflowLaunchService extends LaunchService {
   }
 
   getDockstoreSupportedCwlMakeTemplateString(path: string, versionName: string, entryType?: EntryType) {
-    console.log(entryType);
     if (entryType === EntryType.Tool) {
       return super.getDockstoreSupportedCwlMakeTemplateString(path, versionName);
     }
