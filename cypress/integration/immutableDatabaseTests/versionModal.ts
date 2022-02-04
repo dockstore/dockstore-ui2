bf07a1f7628d8d8198e089a8b8cff8c362da31a7
KimberleyChong <97123241+KimberleyChong@users.noreply.github.com>
2022-02-04 15:16:29 -0500

    Global mat-raised-button changes (#1447)
    * Global mat-raised-button style update, small mat raised button skin and structure created, some padding fixes
    
    * Additional button changes, mat-card padding adjustments, and bubble height adjustment
    
    * Padding update and few more mat-raised-buttons
    
    * Updated more mat-raised-buttons

--- cypress/integration/immutableDatabaseTests/versionModal.ts
+++ cypress/integration/immutableDatabaseTests/versionModal.ts
@@ -24,8 +24,7 @@ describe('Public Version Modal', () => {
 
   it('Change tab to versions', () => {
     goToTab('Versions');
-    cy.contains('button', 'Actions').should('be.visible').click();
-    cy.contains('button:visible', 'View').should('be.visible').click();
+    cy.contains('button', 'Info').should('be.visible').click();
     cy.get('form');
     cy.get('#dockerPullCommand').should('be.visible').should('have.value', 'docker pull quay.io/garyluu/dockstore-cgpmap:3.0.0-rc8');
   });
