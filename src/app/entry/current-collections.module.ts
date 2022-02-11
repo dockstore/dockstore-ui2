910dd572928823c507a8b7d29f01c35d38f35c2a
Steve Von Worley <svonworl@ucsc.edu>
2022-02-11 12:44:11 -0800

    add org logos (avatars) to listings in entry collection box
    https://ucsc-cgl.atlassian.net/browse/DOCK-1907
    dockstore/dockstore#4452

--- src/app/entry/current-collections.module.ts
+++ src/app/entry/current-collections.module.ts
@@ -4,9 +4,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { CustomMaterialModule } from '../shared/modules/material.module';
 import { CurrentCollectionsComponent } from './current-collections/current-collections.component';
+import { ImgFallbackModule } from '../shared/modules/img-fallback.module';
 
 @NgModule({
-  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule],
+  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule, ImgFallbackModule],
   declarations: [CurrentCollectionsComponent],
   exports: [CurrentCollectionsComponent],
 })
