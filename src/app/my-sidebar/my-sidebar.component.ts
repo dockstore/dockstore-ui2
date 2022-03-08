f8f31f265374cb81ef16c2e7958ba80fe897fc32
Kimberley Chong <kim.barleyyy@gmail.com>
2022-03-08 15:21:03 -0500

    add alt tag and remove unused imports

--- src/app/my-sidebar/my-sidebar.component.ts
+++ src/app/my-sidebar/my-sidebar.component.ts
@@ -1,15 +1,10 @@
-import { I } from '@angular/cdk/keycodes';
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-sidebar',
   templateUrl: './my-sidebar.component.html',
   styleUrls: ['./my-sidebar.component.scss'],
 })
-export class MySidebarComponent implements OnInit {
+export class MySidebarComponent {
   constructor() {}
-
-  ngOnInit(): void {
-    let init: boolean = true;
-  }
 }
