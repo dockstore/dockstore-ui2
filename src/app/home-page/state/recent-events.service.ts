5f6f0e34bdb0c0b929650c6cab13504883553d78
Eileen Zhang <eileen.zhang@live.ca>
2022-06-07 11:04:50 -0400

    get rid of unused inputs

--- src/app/home-page/state/recent-events.service.ts
+++ src/app/home-page/state/recent-events.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { Event, EventsService, User } from 'app/shared/openapi';
-import { UsersService } from 'app/shared/swagger';
-import { UserQuery } from 'app/shared/user/user.query';
 import { finalize, tap } from 'rxjs/operators';
 import { RecentEventsStore } from './recent-events.store';
 
 @Injectable({ providedIn: 'root' })
 export class RecentEventsService {
-  constructor(
-    private recentEventsStore: RecentEventsStore,
-    private eventsService: EventsService,
-    protected userQuery: UserQuery,
-    protected usersService: UsersService
-  ) {}
+  constructor(private recentEventsStore: RecentEventsStore, private eventsService: EventsService) {}
 
   get(user: User) {
     this.recentEventsStore.setLoading(true);
