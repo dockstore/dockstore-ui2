cae73d3f7df1a7e0c4dbac39c5ee69950652c08a
Eileen Zhang <eileen.zhang@live.ca>
2022-06-15 14:59:33 -0400

    update tests

--- cypress/integration/group2/myworkflows.ts
+++ cypress/integration/group2/myworkflows.ts
@@ -25,7 +25,7 @@ describe('Dockstore my workflows', () => {
   const wdlDescriptorType = 'WDL';
   const nextflowDescriptorType = 'Nextflow';
   it('have entries shown on the homepage', () => {
-    cy.visit('/');
+    cy.visit('/dashboard');
     cy.contains(/^l$/);
     cy.contains('Find entries');
     cy.get('#mat-input-0').type('hosted');
