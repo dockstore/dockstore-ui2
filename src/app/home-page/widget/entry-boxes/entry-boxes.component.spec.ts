35b3aa6b7384c5d47aedaba152af1cc0ecfb4521
Eileen Zhang <eileen.zhang@live.ca>
2022-07-25 14:38:29 -0400

    change provider

--- src/app/home-page/widget/entry-boxes/entry-boxes.component.spec.ts
+++ src/app/home-page/widget/entry-boxes/entry-boxes.component.spec.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MytoolsService } from 'app/mytools/mytools.service';
+import { RegisterToolService } from 'app/container/register-tool/register-tool.service';
 import { EntryBoxesComponent } from './entry-boxes.component';
 
 describe('EntryBoxesComponent', () => {
@@ -17,7 +17,7 @@ describe('EntryBoxesComponent', () => {
         declarations: [EntryBoxesComponent],
         schemas: [NO_ERRORS_SCHEMA],
         imports: [RouterTestingModule, MatButtonModule, MatIconModule, MatDialogModule],
-        providers: [MytoolsService],
+        providers: [RegisterToolService],
       }).compileComponents();
     })
   );
