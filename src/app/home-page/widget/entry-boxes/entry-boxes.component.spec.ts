18a0eb544bfe983b7c4dfa40fef8d93c9d8d25b6
Eileen Zhang <eileen.zhang@live.ca>
2022-07-25 14:20:45 -0400

    fix tests

---
+++ src/app/home-page/widget/entry-boxes/entry-boxes.component.spec.ts
@@ -0,0 +1,34 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MytoolsService } from 'app/mytools/mytools.service';
+import { EntryBoxesComponent } from './entry-boxes.component';
+
+describe('EntryBoxesComponent', () => {
+  let component: EntryBoxesComponent;
+  let fixture: ComponentFixture<EntryBoxesComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [EntryBoxesComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+        imports: [RouterTestingModule, MatButtonModule, MatIconModule, MatDialogModule],
+        providers: [MytoolsService],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EntryBoxesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
