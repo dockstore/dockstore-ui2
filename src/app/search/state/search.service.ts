08295ba43211e753a7ab5cdc04fbbc67ad899e8b
Stephen Von Worley <svonworl@ucsc.edu>
2022-07-27 22:02:04 -0700

    fix a search self-dos

--- src/app/search/state/search.service.ts
+++ src/app/search/state/search.service.ts
@@ -360,7 +360,7 @@ export class SearchService {
   }
 
   handleLink(linkArray: Array<string>) {
-    this.router.navigateByUrl('search?' + linkArray[1]);
+    this.router.navigateByUrl('search?' + linkArray[1], { replaceUrl: true });
     this.setShortUrl(linkArray[0] + '?' + linkArray[1]);
   }
 
