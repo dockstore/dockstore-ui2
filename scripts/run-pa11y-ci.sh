3df74a1d5c2764a09cb15d5f475e2be8300ad5b7
Flynn Hembroff <85577755+fhembroff@users.noreply.github.com>
2022-08-17 15:17:51 -0400

    Feature/seab 1498/pa11y ci implementation (#1593)
    * Attempt to run pa11y-ci
    
    * Changed timeout
    
    * Added .pa11yci file
    
    * Removed bash -i -c "
    
    * Changed pa11y-ci command
    
    * Running accessibility test
    
    * Testing change
    
    * Corrected syntax
    
    * Changed resource class
    
    * Updated pa11y script
    
    * Updated pa11y script
    
    * Added base-branch
    
    * Added base-branch
    
    * Addded paraamter to prepare_accessibility_test
    
    * Modified accessibility test
    
    * Corrected syntax error
    
    * Corrected syntax error
    
    * Corrected syntax error
    
    * Moved install_container_dependencies command
    
    * Corrected syntax
    
    * Corrected the persisting eror
    
    * Corrected the persisting error (hopefully)
    
    * Changed base branch
    
    * Removed checkout command
    
    * Prevented non-existent file from being "cat"ed
    
    * Made correction about checkout statement
    
    * Corrected syntax
    
    * Added dependencies to config.yml
    
    * Added checkout command
    
    * Changed order of checking out code
    
    * Removed check that files are the same
    
    * Changed base branch for testing
    
    * Changed base_branch back to develop
    
    * Changed base branch for test
    
    * Changed base branch
    
    * Changed base branch
    
    * Changed base branch
    
    * Changed base branch for testing
    
    * Added functionality to store artifacts
    
    * Stored artifacts in compare_accessibility_results job as well
    
    * Added all tests back into config.yml
    
    * Added a statement telling users that run-pa11y-ci.sh isn't very helpful when running locally
    
    Fixed a few typos in run-pa11y-ci.sh
    
    * Changed #!/bin/bash to #!/usr/bin/env bash
    
    * Corrected indentation error in config.yml
    
    * Corrected unintentional indentation errors
    
    * Removed unneeded comment
    
    * Removed unneeded newlines
    
    * Chnaged base branch back to develop
    
    * Removed extra set -o nounset in run-pa11y-ci.sh
    
    * Added newlines to run-pa11y-ci.sh to increase readability
    
    * Removed unneeded whitespace
    
    * Added some more tests to .pa11yci
    
    * Changed .pa11yci
    
    * Adding pa11y-ci to package.json
    
    * Adding pa11y-ci to package.json
    
    * Switched out the executor
    
    * Updated license
    
    * Added accessibility_test_exec
    
    * Added tests for workflow and tool pages to .pa11yci
    
    * Updated .pa11yci to include more URLs to test
    
    * Moved pa11y-ci to devDependencies
    
    * Added http://localhost:4200/login and  http://localhost:4200/register to .pa11yci

---
+++ scripts/run-pa11y-ci.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+# Run pa11y-ci shows any accessibility issues related to the UI.
+# NOTE: This script will not be useful if you are running it locally,
+# it is recommended that you run `pa11y-ci` in your command line instead.
+
+RESULT_DIRECTORY="accessibility-results"
+CURRENT_BRANCH_RESULT_FILE_NAME="current-branch"
+BASE_BRANCH_RESULT_FILE_NAME="base-branch"
+RESULT_FILE="$CURRENT_BRANCH_RESULT_FILE_NAME"
+
+
+usage() {
+  echo "Usage: $0 [OPTION...]"
+  echo ""
+  echo "-H, Display help command"
+  echo "-B, Do all commands on base branch (if this option is not given all commands are done on current branch)"
+  echo "-C, Checkouts the code from the base branch (note: the option -B is not required to do this)"
+  echo "-R, Runs pa11y-ci on branch (requires webservice to be running) and outputs results in a form that can be analysed"
+  echo "-A, Determines if current branch has more accessibility issues then base branch, requires the results from option -R to be available"
+}
+
+# Variables for determining which command to run
+RUN_ACCESSIBILITY_TEST="false"
+COMPARE_RESULTS="false"
+
+no_args="true"
+while getopts 'HBCRA' OPTION; do
+  case "$OPTION" in
+    H)
+      usage
+      exit 0
+      ;;
+    B)
+      RESULT_FILE="$BASE_BRANCH_RESULT_FILE_NAME"
+      ;;
+    C)
+      git checkout "$npm_package_config_base_branch"
+      echo "The base branch has been checked out, all other flags (if any) have been ignored"
+      exit 0
+      ;;
+    R)
+      RUN_ACCESSIBILITY_TEST="true"
+      if [ "$COMPARE_RESULTS" == "true" ]
+      then
+        echo "ERROR: You cannot use the R and A flags at the same time"
+        usage
+        exit 1
+      fi
+      ;;
+    A)
+      COMPARE_RESULTS="true"
+      if [ "$RUN_ACCESSIBILITY_TEST" == "true" ]
+      then
+        echo "ERROR: You cannot use the R and A flags at the same time"
+        usage
+        exit 1
+      fi
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+  no_args="false"
+done
+
+# Display help message if no arguments were given
+if [ "$no_args" == "true" ]
+then
+  usage
+  exit 1
+fi
+
+if [ "$RUN_ACCESSIBILITY_TEST" == "true" ]
+then
+  mkdir -p "$RESULT_DIRECTORY"
+  OUTPUT_FILE_PATH="${RESULT_DIRECTORY}/${RESULT_FILE}"
+
+  npx pa11y-ci --json > "$OUTPUT_FILE_PATH".json || true
+
+  if [ "$RESULT_FILE" == "$CURRENT_BRANCH_RESULT_FILE_NAME" ]
+  then
+    npx pa11y-ci 2> "$OUTPUT_FILE_PATH".txt || true # Get nicely formatted version of results
+  fi
+
+  echo "Successfully ran accessibility test"
+  exit 0
+fi
+
+
+
+if [ "$COMPARE_RESULTS" == "true" ]
+then
+  ACCESSIBILITY_ERRORS_CURRENT_BRANCH=$(jq .errors "${RESULT_DIRECTORY}/${CURRENT_BRANCH_RESULT_FILE_NAME}".json)
+  ACCESSIBILITY_ERRORS_BASE_BRANCH=$(jq .errors "${RESULT_DIRECTORY}/${BASE_BRANCH_RESULT_FILE_NAME}".json)
+
+  echo "Accessibility errors on current branch:"
+  cat "${RESULT_DIRECTORY}/${CURRENT_BRANCH_RESULT_FILE_NAME}".txt
+
+  if [ "$ACCESSIBILITY_ERRORS_CURRENT_BRANCH" -gt "$ACCESSIBILITY_ERRORS_BASE_BRANCH" ]; then
+    echo "You have introduced $((ACCESSIBILITY_ERRORS_CURRENT_BRANCH-ACCESSIBILITY_ERRORS_BASE_BRANCH)) accessibility errors."
+    exit 1
+  fi
+
+  # If the number of accessibility errors is equal, then check that if they are the same set of errors.
+  if [ "$ACCESSIBILITY_ERRORS_CURRENT_BRANCH" -eq "$ACCESSIBILITY_ERRORS_BASE_BRANCH" ]; then
+    echo "The number of accessibility erorrs in this PR and the base branch are the same"
+    exit 0
+  fi
+
+  echo "You have reduced the number of accessibility issues with this PR"
+  exit 0
+fi
