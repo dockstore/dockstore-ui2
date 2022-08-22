61e726845a88ae70b5efcc3eb877e1f7f33c7e11
Denis Yuen <denis.yuen@oicr.on.ca>
2022-08-22 18:32:29 -0400

    move around styles

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -4,7 +4,6 @@ import { Dockstore } from '../shared/dockstore.model';
 @Component({
   selector: 'app-pagenotfound',
   templateUrl: './pagenotfound.component.html',
-  styleUrls: ['./pagenotfound.component.scss'],
 })
 export class PageNotFoundComponent {
   Dockstore = Dockstore;
