0ee6532eff515bf66ffb5be0f60530c9a6dd0955
Charles Overbeck <coverbec@ucsc.edu>
2022-08-31 14:44:48 -0700

    Revert unintended change

--- cypress/integration/smokeTests/sharedTests/auth-tests.ts
+++ cypress/integration/smokeTests/sharedTests/auth-tests.ts
@@ -325,7 +325,7 @@ function testCollection(org: string, collection: string, registry: string, repo:
     deleteTool();
   });
 }
-//
+
 testCollection(collectionTuple[0], collectionTuple[1], toolTuple[0], toolTuple[1], toolTuple[2]);
 testTool(toolTuple[0], toolTuple[1], toolTuple[2]);
 testWorkflow(workflowTuple[0], workflowTuple[1], workflowTuple[2]);
