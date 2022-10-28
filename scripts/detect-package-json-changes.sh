148fe23b115efccc2c3521aea79a9a1e3059cea0
Charles Overbeck <coverbec@ucsc.edu>
2022-10-27 17:29:59 -0700

    Debug what is happening

--- scripts/detect-package-json-changes.sh
+++ scripts/detect-package-json-changes.sh
@@ -5,5 +5,6 @@ licenseFileStatus="$(diff CIRCLE-THIRD-PARTY-LICENSES.csv THIRD-PARTY-LICENSES.c
 if [ -n "$licenseFileStatus" ]
     then
         echo "The THIRD-PARTY-LICENSES.csv file has not been updated. Update the file by running 'npm run license'."
+        diff CIRCLE-THIRD-PARTY-LICENSES.csv THIRD-PARTY-LICENSES.csv
         exit 1
     fi
