32fa001325044c49d76ed135f1354320aa8bfbe6
Nayeon Hyun <61166764+hyunnaye@users.noreply.github.com>
2022-11-07 10:23:22 -0500

    DOCK-2240 Changed colors of each entry widget bubbles on new dashboard (#1638)
    * changed colors of each entry widget bubbles
    
    * changes regarding PR review
    
    * added/updated copyright message

--- src/app/home-page/widget/entry-box/entry-box.component.ts
+++ src/app/home-page/widget/entry-box/entry-box.component.ts
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2022 OICR, UCSC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License")
+ *    you may not use this file except in compliance with the License
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 import { Component, Input, OnInit } from '@angular/core';
 import { EntryType } from 'app/shared/enum/entry-type';
 import { EntryUpdateTime, UsersService } from 'app/shared/openapi';
@@ -22,6 +38,7 @@ export class EntryBoxComponent extends Base implements OnInit {
     | typeof EntryUpdateTime.EntryTypeEnum.SERVICE
     | typeof EntryUpdateTime.EntryTypeEnum.WORKFLOW;
   entryTypeLowerCase: string;
+  public entryTypeEnum = EntryUpdateTime.EntryTypeEnum;
   filterText: string;
   listOfEntries: Array<EntryUpdateTime> = [];
   helpLink: string;
