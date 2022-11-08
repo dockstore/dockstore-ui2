8fee71f2ef4d3d0693b1586446d3d4521b5134aa
hyunnaye <nayeonhyun802@gmail.com>
2022-11-08 10:51:06 -0500

    changes made regarding PR review

--- scripts/reset-dockstoretestuser4-resources.sh
+++ scripts/reset-dockstoretestuser4-resources.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
 
 STACK=$1
 
@@ -17,8 +21,9 @@ fi
 
 #The function getUser takes username as argument to get the user JSON object and user id.
 function getUser {
+  USERNAME=S1
   USER=$(curl -X 'GET' \
-  "${URL}/api/users/username/$1" \
+  "${URL}/api/users/username/${USERNAME}" \
   -H 'accept: application/json')
 
   USER_ID=$(echo "$USER" | jq -r ".id")
@@ -26,29 +31,23 @@ function getUser {
 
 
 function getUserPublishedWorkflows {
-  while IFS=$'\n' read -r line
-  do
-    PUBLISHED_WORKFLOWS_IDS+=("$line")
-  done < <(curl -X 'GET' \
+  PUBLISHED_WORKFLOWS_IDS=$(curl -X 'GET' \
     "${URL}/api/users/${USER_ID}/workflows/published" \
     -H 'accept: application/json' \
     -H "Authorization: Bearer ${TOKEN}" | jq .[].id)
 }
 
 function getUserRegisteredTools {
-  while IFS=$'\n' read -r line
-  do
-    REGISTERED_TOOLS_IDS+=("$line")
-  done < <(curl -X 'GET' \
+  REGISTERED_TOOLS_IDS=$(curl -X 'GET' \
   "${URL}/api/users/${USER_ID}/containers" \
     -H "Authorization: Bearer ${TOKEN}" \
     -H 'accept: application/json' | jq .[].id
   )
 }
 
-#This function will unpublish all of the user's workflows and delete its registered tools
+#This function will first unpublish all of the user's workflows and tools then delete its registered tools.
 function resetUserResources {
-  for WORKFLOW_ID in "${PUBLISHED_WORKFLOWS_IDS[@]}"
+  for WORKFLOW_ID in ${PUBLISHED_WORKFLOWS_IDS}
   do
     curl -X 'POST' \
       "${URL}/api/workflows/${WORKFLOW_ID}/publish" \
@@ -57,11 +56,19 @@ function resetUserResources {
       -H "Authorization: Bearer ${TOKEN}" \
       -d '{
       "publish": false
-    }'
+      }'
   done
 
-  for TOOL_ID in "${REGISTERED_TOOLS_IDS[@]}"
+  for TOOL_ID in ${REGISTERED_TOOLS_IDS}
   do
+    curl -X 'POST' \
+      "${URL}/api/containers/${TOOL_ID}/publish" \
+      -H 'accept: application/json' \
+      -H 'Content-Type: application/json' \
+      -H "Authorization: Bearer ${TOKEN}" \
+      -d '{
+      "publish": false
+      }'
     curl -X 'DELETE' \
       "${URL}/api/containers/${TOOL_ID}" \
       -H 'accept: application/json' \
