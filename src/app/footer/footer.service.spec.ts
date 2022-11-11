01ab9f1e7a77b90265e2eb298bcbeffc469eaa75
hyunnaye <nayeonhyun802@gmail.com>
2022-11-11 10:44:05 -0500

    fixed unit test

--- src/app/footer/footer.service.spec.ts
+++ src/app/footer/footer.service.spec.ts
@@ -12,14 +12,26 @@ describe('FooterService', () => {
 
   it('should generate markdown', () => {
     // sanity test the method; don't want to look for the exact value as that would just be writing it all over again
-    const markdown = service.versionsToMarkdown('981edd1', '2.6.1-39-g597aeeed', '1.9.0', '3723b1a"', null, null, null, null, 'potato');
+    const markdown = service.versionsToMarkdown(
+      window.location.href,
+      '981edd1',
+      '2.6.1-39-g597aeeed',
+      '1.9.0',
+      '3723b1a"',
+      null,
+      null,
+      null,
+      null,
+      'potato'
+    );
+    expect(markdown).toContain(window.location.href);
     expect(markdown).toContain('981edd1');
     expect(markdown).toContain('potato');
     expect(markdown).toContain('checkUrlLambdaVersion');
   });
 
   it('should handle nulls', () => {
-    const markdown = service.versionsToMarkdown(null, null, null, null, null, null, null, null, null);
+    const markdown = service.versionsToMarkdown(null, null, null, null, null, null, null, null, null, null);
     expect(markdown.length).toBeGreaterThan(100);
     expect(markdown).not.toContain('compose_setup');
     expect(markdown).not.toContain('dockstore-deploy');
