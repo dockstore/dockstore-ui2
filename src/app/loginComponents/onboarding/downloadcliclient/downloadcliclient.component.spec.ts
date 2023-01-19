81ffd5eedd672f7f69d52ecfcad157473feb4a7b
KimberleyChong <kim.barleyyy@gmail.com>
2023-01-19 10:49:47 -0500

    instead of calling api directly, use service-info.service

--- src/app/loginComponents/onboarding/downloadcliclient/downloadcliclient.component.spec.ts
+++ src/app/loginComponents/onboarding/downloadcliclient/downloadcliclient.component.spec.ts
@@ -10,6 +10,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MarkdownModule } from 'ngx-markdown';
 import { MetadataService } from '../../../shared/swagger';
 import { GA4GHV20Service } from './../../../shared/openapi';
+import { ServiceInfoService } from '../../../service-info/service-info.service';
 import { RouterLinkStubDirective, RouterOutletStubComponent } from './../../../test/router-stubs';
 import { AuthStubService, GA4GHV20StubService } from './../../../test/service-stubs';
 import { DownloadCLIClientComponent } from './downloadcliclient.component';
@@ -37,6 +38,7 @@ describe('DownloadCLIClientComponent', () => {
           { provide: AuthService, useClass: AuthStubService },
           { provide: GA4GHV20Service, useClass: GA4GHV20StubService },
           MetadataService,
+          ServiceInfoService,
         ],
       }).compileComponents();
     })
