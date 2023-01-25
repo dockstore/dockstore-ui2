ab845bc980803aa4cb997b29187c4da1d3d74d39
Kimberley Chong <97123241+KimberleyChong@users.noreply.github.com>
2023-01-25 13:49:44 -0500

    Updated UCSCGI and BDC Logos - DOCK-2204/2291 (#1687)
    * new ucsc logos
    
    * Updated BDC text logos
    
    * convert px to rem
    
    * Updated BDC launch with button and language use

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -44,7 +44,7 @@ export class FooterComponent extends Base implements OnInit {
     new Sponsor('oicr.svg', new URL('https://oicr.on.ca/')),
     new Sponsor('broad1.svg', new URL('https://www.broadinstitute.org/')),
     new Sponsor('ga.svg', new URL('https://genomicsandhealth.org/')),
-    new Sponsor('ucsc.png', new URL('https://ucscgenomics.soe.ucsc.edu/')),
+    new Sponsor('ucsc.svg', new URL('https://ucscgenomics.soe.ucsc.edu/')),
   ];
 
   /**
