4cd93541736c5aa81392612dc1364d734c31c2f8
Charles Overbeck <coverbec@ucsc.edu>
2023-02-01 09:37:18 -0800

    PR feedback, same casing as other facets

--- src/app/search/map-friendly-values.pipe.ts
+++ src/app/search/map-friendly-values.pipe.ts
@@ -68,6 +68,13 @@ export class MapFriendlyValuesPipe implements PipeTransform {
       ]),
     ],
     [
+      'openData',
+      new Map([
+        ['1', 'has public test data'],
+        ['0', 'no public test data'],
+      ]),
+    ],
+    [
       'registry',
       new Map([
         ['QUAY_IO', 'Quay.io'],
@@ -110,13 +117,6 @@ export class MapFriendlyValuesPipe implements PipeTransform {
         ['PUSH', 'Push'],
         ['DELETE', 'Delete'],
         ['INSTALL', 'Install'],
-      ]),
-    ],
-    [
-      'openData',
-      new Map([
-        ['0', 'No public test data'],
-        ['1', 'Has public test data'],
       ]),
     ],
   ]);
