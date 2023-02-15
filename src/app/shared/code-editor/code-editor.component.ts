3535c47ff58148dcdafcf164c1d098b9a35bd601
Charles Overbeck <coverbec@ucsc.edu>
2023-02-15 14:36:46 -0800

    Update smoke tests and Ace editor (#1710)
    https://ucsc-cgl.atlassian.net/browse/SEAB-5240

--- src/app/shared/code-editor/code-editor.component.ts
+++ src/app/shared/code-editor/code-editor.component.ts
@@ -49,7 +49,7 @@ export class CodeEditorComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     const aceMode = 'ace/mode/' + this.mode;
-    ace.config.set('workerPath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.4/');
+    ace.config.set('workerPath', '/assets/ace');
     this.editor = ace.edit(this.aceId, {
       mode: aceMode,
       readOnly: this.readOnly,
