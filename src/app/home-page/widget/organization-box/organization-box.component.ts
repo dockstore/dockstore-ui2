62bcc95b663584e0552e81e870ec95c47d702fe3
KimberleyChong <kim.barleyyy@gmail.com>
2023-02-16 16:09:26 -0500

    organization box fixes

--- src/app/home-page/widget/organization-box/organization-box.component.ts
+++ src/app/home-page/widget/organization-box/organization-box.component.ts
@@ -5,11 +5,12 @@ import { RequireAccountsModalComponent } from 'app/organizations/registerOrganiz
 import { Base } from 'app/shared/base';
 import { Dockstore } from 'app/shared/dockstore.model';
 import { Event, OrganizationUser, OrganizationUpdateTime } from 'app/shared/openapi';
-import { UsersService } from 'app/shared/swagger';
+import { UsersService } from '../../../shared/openapi';
 import { finalize, takeUntil } from 'rxjs/operators';
 import { bootstrap4mediumModalSize } from 'app/shared/constants';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AlertService } from 'app/shared/alert/state/alert.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-organization-box',
@@ -19,10 +20,10 @@ import { AlertService } from 'app/shared/alert/state/alert.service';
 export class OrganizationBoxComponent extends Base implements OnInit {
   Dockstore = Dockstore;
   listOfOrganizations: Array<OrganizationUpdateTime> = [];
-  firstCall = true;
+  pendingRequests$: Observable<Array<OrganizationUser>>;
+  pendingInvites$: Observable<Array<OrganizationUser>>;
+  filterText: string;
   totalOrgs: number = 0;
-  pendingRequests: Array<OrganizationUser> = [];
-  pendingInvites: Array<OrganizationUser> = [];
   EventType = Event.TypeEnum;
 
   public isLoading = true;
@@ -38,51 +39,33 @@ export class OrganizationBoxComponent extends Base implements OnInit {
 
   ngOnInit(): void {
     this.getMyOrganizations();
-    this.requestsQuery.myPendingOrganizationRequests$.subscribe(
-      (requests) => {
-        if (requests) {
-          this.pendingRequests = requests;
-        }
-      },
-      (error: HttpErrorResponse) => {
-        this.alertService.detailedError(error);
-      }
-    );
-    this.requestsQuery.myOrganizationInvites$.subscribe(
-      (invites) => {
-        if (invites) {
-          this.pendingRequests = invites;
-        }
-      },
-      (error: HttpErrorResponse) => {
-        this.alertService.detailedError(error);
-      }
-    );
+    this.pendingRequests$ = this.requestsQuery.myPendingOrganizationRequests$;
+    this.pendingInvites$ = this.requestsQuery.myOrganizationInvites$;
   }
 
   private getMyOrganizations() {
     this.usersService
-      .getUserDockstoreOrganizations()
+      .getUserDockstoreOrganizations(null, this.filterText)
       .pipe(
         finalize(() => (this.isLoading = false)),
         takeUntil(this.ngUnsubscribe)
       )
       .subscribe(
         (myOrgs: Array<OrganizationUpdateTime>) => {
-          this.listOfOrganizations = [];
-          myOrgs.forEach((org: OrganizationUpdateTime) => {
-            if (this.listOfOrganizations.length < 5) {
-              this.listOfOrganizations.push(org);
-            }
-            if (this.firstCall) {
-              this.totalOrgs += 1;
-            }
+          myOrgs.forEach(() => {
+            this.listOfOrganizations = myOrgs.slice(0, 7);
+            this.totalOrgs = myOrgs.length;
           });
         },
         (error: HttpErrorResponse) => {
           this.alertService.detailedError(error);
         }
       );
+  }
+
+  onTextChange(event: any) {
+    this.isLoading = true;
+    this.getMyOrganizations();
   }
 
   /**
