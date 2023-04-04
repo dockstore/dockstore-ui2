274e8deece1f8fdfbdc3f20b7ea5cb0f8a80aaea
Kimberley Chong <97123241+KimberleyChong@users.noreply.github.com>
2023-04-04 15:18:10 -0400

    Update Featured Content Styles - SEAB-4628 (#1744)
    * updated featured content styles
    
    * remove text-decoration and color
    
    * match changes to feat-content.html and count news entries

--- src/app/home-page/widget/featured-content/news-and-updates.component.ts
+++ src/app/home-page/widget/featured-content/news-and-updates.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AlertService } from '../../../shared/alert/state/alert.service';
 import { Dockstore } from '../../../shared/dockstore.model';
 
@@ -8,12 +8,12 @@ import { Dockstore } from '../../../shared/dockstore.model';
   template: ` <span [innerHTML]="myExternalHTML"></span> `,
   styleUrls: ['../../../shared/styles/dashboard-boxes.scss'],
 })
-export class NewsAndUpdatesComponent implements OnInit {
+export class NewsAndUpdatesComponent {
   // TODO should parameterize FeaturedContentComponent or something
   public myExternalHTML = '';
+  public newsEntriesCount: number = 0;
 
-  constructor(private http: HttpClient, private alertService: AlertService) {}
-  ngOnInit() {
+  constructor(private http: HttpClient, private alertService: AlertService) {
     this.alertService.start('Retrieving news and updates');
     this.http
       .get(
@@ -24,6 +24,8 @@ export class NewsAndUpdatesComponent implements OnInit {
       .subscribe(
         (data) => {
           this.myExternalHTML = data;
+          // Count the number of "news-entry" classes to indicate the number of news entries
+          this.newsEntriesCount = (this.myExternalHTML.match(/news-entry/g) || []).length;
           this.alertService.simpleSuccess();
         },
         () => {
