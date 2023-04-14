3e8a02ff894555aee75df6a6ac25e0cfb6b2a27a
hyunnaye <nayeonhyun802@gmail.com>
2023-04-14 13:59:20 -0400

    changed parameter to optional

--- src/app/shared/entry/base-url.pipe.ts
+++ src/app/shared/entry/base-url.pipe.ts
@@ -4,31 +4,31 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'baseUrl',
 })
 export class BaseUrlPipe implements PipeTransform {
-  private readMeBasePath: string;
+  private markdownBasePath: string;
   /**
    * Constructs a base url for relative links in markdown README files.
    * @param providerUrl
    * @param versionName
-   * @param readMePath path for desired base url is different from the root of the repo. Ex. readMePath defined in .dockstore.yml
+   * @param customPath relative path of markdown file if its parent directory is not the root of the repo. Set to null if the file is in the root of the repo Ex. custom readMePath defined in .dockstore.yml
    * @returns {string} base url to be used by ngx-markdown
    */
-  transform(providerUrl: string, versionName: string, readMePath: string): string {
+  transform(providerUrl: string, versionName: string, customPath?: string): string {
     if (!providerUrl || !versionName) {
       console.error('providerUrl or versionName is not truthy');
       return null;
     }
 
-    if (readMePath) {
-      const slashIndex = readMePath.lastIndexOf('/'); //index of the slash before the readme filename. This allows us to get the path to its parent directory.
-      this.readMeBasePath = readMePath.substring(0, slashIndex) + '/'; // the last slash is needed by ngx-markdown
+    if (customPath) {
+      const slashIndex = customPath.lastIndexOf('/'); //index of the slash before the readme filename. This allows us to get the path to its parent directory.
+      this.markdownBasePath = customPath.substring(0, slashIndex) + '/'; // the last slash is needed by ngx-markdown
     } else {
-      this.readMeBasePath = '';
+      this.markdownBasePath = '';
     }
 
     if (providerUrl.startsWith('https://github.com/') || providerUrl.startsWith('https://gitlab.com/')) {
-      return providerUrl + '/blob/' + versionName + '/' + this.readMeBasePath; // the last slash needed by ngx-markdown is provided by this.readMeBasePath.
+      return providerUrl + '/blob/' + versionName + '/' + this.markdownBasePath; // the last slash needed by ngx-markdown is provided by this.markdownBasePath.
     } else if (providerUrl.startsWith('https://bitbucket.org/')) {
-      return providerUrl + '/src/' + versionName + '/' + this.readMeBasePath; //  the last slash needed by ngx-markdown is provided by this.readMeBasePath.
+      return providerUrl + '/src/' + versionName + '/' + this.markdownBasePath; //  the last slash needed by ngx-markdown is provided by this.rmarkdownBasePath.
     } else {
       return null;
     }
