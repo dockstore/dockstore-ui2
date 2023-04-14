1a55e6c97de54de1646236cf6ca53756bd19380f
hyunnaye <nayeonhyun802@gmail.com>
2023-04-14 15:27:26 -0400

    fixed base-url pipe

--- src/app/shared/entry/base-url.pipe.spec.ts
+++ src/app/shared/entry/base-url.pipe.spec.ts
@@ -5,7 +5,10 @@ describe('Pipe: BaseUrl', () => {
     const pipe = new BaseUrlPipe();
     expect(pipe).toBeTruthy();
     expect(pipe.transform('https://github.com/foobar/hello_world', 'master')).toBe('https://github.com/foobar/hello_world/blob/master/');
-    expect(pipe.transform('https://gitlab.com/foobar/hello_world', 'develop', 'dir/readme.md')).toBe(
+    expect(pipe.transform('https://github.com/foobar/hello_world', 'master', null)).toBe(
+      'https://github.com/foobar/hello_world/blob/master/'
+    );
+    expect(pipe.transform('https://gitlab.com/foobar/hello_world', 'develop', '/dir/readme.md')).toBe(
       'https://gitlab.com/foobar/hello_world/blob/develop/dir/'
     );
     expect(pipe.transform('https://bitbucket.org/foobar/hello_world', '1.0')).toBe('https://bitbucket.org/foobar/hello_world/src/1.0/');
--- src/app/shared/entry/base-url.pipe.ts
+++ src/app/shared/entry/base-url.pipe.ts
@@ -4,7 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'baseUrl',
 })
 export class BaseUrlPipe implements PipeTransform {
-  private markdownBasePath: string;
   /**
    * Constructs a base url for relative links in markdown README files.
    * @param providerUrl
@@ -12,7 +11,8 @@ export class BaseUrlPipe implements PipeTransform {
    * @param customPath If applicable, the relative path of markdown file if its parent directory is not the root of the repo. Ex. custom readMePath defined in .dockstore.yml
    * @returns {string} base url to be used by ngx-markdown
    */
-  transform(providerUrl: string, versionName: string, customPath?: string): string {
+  transform(providerUrl: string, versionName: string, customPath?: string | null): string {
+    let markdownBasePath: string;
     if (!providerUrl || !versionName) {
       console.error('providerUrl or versionName is not truthy');
       return null;
@@ -20,15 +20,15 @@ export class BaseUrlPipe implements PipeTransform {
 
     if (customPath) {
       const slashIndex = customPath.lastIndexOf('/'); //index of the slash before the readme filename. This allows us to get the path to its parent directory.
-      this.markdownBasePath = customPath.substring(0, slashIndex) + '/'; // the last slash is needed by ngx-markdown
+      markdownBasePath = customPath.substring(0, slashIndex) + '/'; // the last slash is needed by ngx-markdown
     } else {
-      this.markdownBasePath = '';
+      markdownBasePath = '/';
     }
 
     if (providerUrl.startsWith('https://github.com/') || providerUrl.startsWith('https://gitlab.com/')) {
-      return providerUrl + '/blob/' + versionName + '/' + this.markdownBasePath; // the last slash needed by ngx-markdown is provided by this.markdownBasePath.
+      return providerUrl + '/blob/' + versionName + markdownBasePath; // the last slash needed by ngx-markdown is provided by markdownBasePath.
     } else if (providerUrl.startsWith('https://bitbucket.org/')) {
-      return providerUrl + '/src/' + versionName + '/' + this.markdownBasePath; //  the last slash needed by ngx-markdown is provided by this.rmarkdownBasePath.
+      return providerUrl + '/src/' + versionName + markdownBasePath; //  the last slash needed by ngx-markdown is provided by markdownBasePath.
     } else {
       return null;
     }
