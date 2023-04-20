ae967bbd2b2bb0e888f89d48e2cbaf51ca09959e
hyunnaye <nayeonhyun802@gmail.com>
2023-04-20 14:05:06 -0400

    codacy check fix

--- src/app/workflow/executions/executions-tab.component.ts
+++ src/app/workflow/executions/executions-tab.component.ts
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { EntryTab } from '../../shared/entry/entry-tab';
 import {
   CloudInstance,
