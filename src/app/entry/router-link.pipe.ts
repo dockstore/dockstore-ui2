7eb4be32b4db73141ca557feecda0020929f4d44
KimberleyChong <kim.barleyyy@gmail.com>
2023-04-21 11:55:12 -0400

    use entryTypeMetadata for routerlink pipe

--- src/app/entry/router-link.pipe.ts
+++ src/app/entry/router-link.pipe.ts
@@ -1,68 +1,41 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { EntryType } from '../shared/enum/entry-type';
-import { CollectionEntry, Workflow, DockstoreTool } from '../shared/openapi';
+import { CollectionEntry, Workflow, DockstoreTool, EntryType } from '../shared/openapi';
+import { EntryTypeMetadataService } from './type-metadata/entry-type-metadata.service';
 
 @Pipe({
   name: 'routerLink',
 })
 export class RouterLinkPipe implements PipeTransform {
+  constructor(private entryTypeMetadataService: EntryTypeMetadataService) {}
+
   /**
    * Returns the complete path for a workflow, tool, or collection entry
    * To be used for routerLink navigation
    *
-   * @param entryType
    * @param entry
    * @returns string | null
    */
-  transform(entryType: EntryType, entry: DockstoreTool | Workflow | CollectionEntry): string | null {
-    if (!entry || !entryType) {
+  transform(entry: DockstoreTool | Workflow | CollectionEntry): string | null {
+    if (!entry) {
       return null;
     }
 
     if ((entry as Workflow).full_workflow_path) {
       const workflow = entry as Workflow;
-      switch (entryType) {
-        case EntryType.BioWorkflow:
-          return '/workflows/' + workflow.full_workflow_path;
-        case EntryType.Service:
-          return '/services/' + workflow.full_workflow_path;
-        case EntryType.AppTool:
-          return '/containers/' + workflow.full_workflow_path;
-        case EntryType.Notebook:
-          return '/notebooks/' + workflow.full_workflow_path;
-      }
+      return '/' + workflow.entryTypeMetadata.sitePath + '/' + workflow.full_workflow_path;
     }
     if ((entry as DockstoreTool).tool_path) {
-      return '/containers/' + (entry as DockstoreTool).tool_path;
+      const tool = entry as DockstoreTool;
+      return '/' + tool.entryTypeMetadata.sitePath + '/' + tool.tool_path;
     }
     if ((entry as CollectionEntry).entryPath) {
       const collectionEntry = entry as CollectionEntry;
+      const entryTypeMetadata = this.entryTypeMetadataService.get(collectionEntry.entryType.toUpperCase() as EntryType);
+
       if (collectionEntry.versionName) {
-        switch (collectionEntry.entryType) {
-          case EntryType.BioWorkflow:
-            return '/workflows/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
-          case EntryType.Service:
-            return '/services/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
-          case EntryType.AppTool:
-            return '/containers/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
-          case EntryType.Notebook:
-            return '/notebooks/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
-          case EntryType.Tool:
-            return '/containers/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
-        }
+        return '/' + entryTypeMetadata.sitePath + '/' + collectionEntry.entryPath + ':' + collectionEntry.versionName;
       } else {
-        switch (collectionEntry.entryType) {
-          case EntryType.BioWorkflow:
-            return '/workflows/' + collectionEntry.entryPath;
-          case EntryType.Service:
-            return '/services/' + collectionEntry.entryPath;
-          case EntryType.AppTool:
-            return '/containers/' + collectionEntry.entryPath;
-          case EntryType.Notebook:
-            return '/notebooks/' + collectionEntry.entryPath;
-          case EntryType.Tool:
-            return '/containers/' + collectionEntry.entryPath;
-        }
+        return '/' + entryTypeMetadata.sitePath + '/' + collectionEntry.entryPath;
       }
     }
 
