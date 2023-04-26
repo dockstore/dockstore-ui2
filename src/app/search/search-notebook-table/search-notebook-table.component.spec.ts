589b2ff5bb97ab1233325d03b615762e4726e4cd
Stephen Von Worley <svonworl@ucsc.edu>
2023-04-25 19:54:09 -0700

    add new notebook files

---
+++ src/app/search/search-notebook-table/search-notebook-table.component.spec.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DateService } from '../../shared/date.service';
+import { DockstoreService } from '../../shared/dockstore.service';
+import { CustomMaterialModule } from '../../shared/modules/material.module';
+import { DockstoreStubService, SearchStubService } from '../../test/service-stubs';
+import { SearchService } from '../state/search.service';
+import { SearchNotebookTableComponent } from './search-notebook-table.component';
+
+describe('SearchNotebookTableComponent', () => {
+  let component: SearchNotebookTableComponent;
+  let fixture: ComponentFixture<SearchNotebookTableComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SearchNotebookTableComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+        imports: [CustomMaterialModule, BrowserAnimationsModule, RouterTestingModule],
+        providers: [
+          { provide: DockstoreService, useClass: DockstoreStubService },
+          DateService,
+          { provide: SearchService, useClass: SearchStubService },
+        ],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchNotebookTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
