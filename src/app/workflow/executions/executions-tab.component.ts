a73f847a101423fa56cafe3cff43babc71d77a56
hyunnaye <nayeonhyun802@gmail.com>
2023-04-26 15:55:57 -0400

    minor fixes

--- src/app/workflow/executions/executions-tab.component.ts
+++ src/app/workflow/executions/executions-tab.component.ts
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2023 OICR & UCSC
+ *    Copyright 2023 OICR, UCSC
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
