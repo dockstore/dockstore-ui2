19b28ed23e173f2f56c5d0accba03c1cc386be31
Stephen Von Worley <svonworl@ucsc.edu>
2023-05-01 12:20:34 -0700

    remove unused assignment

--- src/app/search/state/search.service.spec.ts
+++ src/app/search/state/search.service.spec.ts
@@ -112,7 +112,6 @@ describe('SearchService', () => {
       const filtered: [Array<Hit>, Array<Hit>, Array<Hit>] = service.filterEntry(elasticSearchResponse, 201);
       const tools = filtered[0];
       const workflows = filtered[1];
-      const notebooks = filtered[2];
       const toolsSource = tools[0]._source;
       const workflowSource = workflows[0]._source;
       expect(toolsSource.imgProvider).toBe('Docker Hub');
