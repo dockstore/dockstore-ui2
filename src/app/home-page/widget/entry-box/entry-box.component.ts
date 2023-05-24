79fffb31e06ebe618033e6d245c1e5c93879831d
Nayeon Hyun <61166764+hyunnaye@users.noreply.github.com>
2023-05-24 10:11:07 -0400

    SEAB-5437: show notebooks in dashboard (#1783)
    * show notebooks in dashboard
    
    * merge
    
    * integration test fix
    
    * fixed typos
    
    * pr review changes
    
    * data-cy error fix
    
    * data-cy error fix
    
    * removed code

--- src/app/home-page/widget/entry-box/entry-box.component.ts
+++ src/app/home-page/widget/entry-box/entry-box.component.ts
@@ -83,14 +83,9 @@ export class EntryBoxComponent extends Base implements OnInit {
       this.helpLink = Dockstore.DOCUMENTATION_URL + '/getting-started/notebooks/notebooks.html';
       this.allEntriesLink = '/my-notebooks/';
       this.entryTypeParam = 'NOTEBOOKS';
-      // Not loading entries for Notebooks - remove when supported
-      this.isLoading = false;
     }
 
-    // Do not get Notebook entries, unsupported - remove when suported
-    if (this.entryType !== NewEntryType.NOTEBOOK) {
-      this.getMyEntries();
-    }
+    this.getMyEntries();
   }
 
   getMyEntries() {
