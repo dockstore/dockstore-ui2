4f7049ef7848919cc61c289af2cae62ed9af667e
Kathy Tran <kathylevtran@gmail.com>
2023-05-26 11:15:58 -0400

    Clean up base branch script

--- scripts/get-base-branch.sh
+++ scripts/get-base-branch.sh
@@ -4,9 +4,17 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-# This script gets the base branch of the PR
+# This script gets the base branch of the PR and is meant to be used by CircleCI jobs
 
+# CIRCLE_PULL_REQUEST is an environment variable set by CircleCI if a pull request exists
+# https://circleci.com/docs/variables/#built-in-environment-variables
 PR_NUMBER="${CIRCLE_PULL_REQUEST##*/}"
+
+if [[ -z "${PR_NUMBER}" ]]; then
+    echo "A PR number is required to get the base branch"
+    exit 1
+fi
+
 BASE_BRANCH=$(curl "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${PR_NUMBER}" | jq -r '.base.ref' )
 
 echo "${BASE_BRANCH}"
