b42a64e69b43dc5941d2096992b7c838c9149f38
hyunnaye <nayeonhyun802@gmail.com>
2023-06-12 13:06:15 -0400

    removed tests

--- cypress/e2e/group1/dashboard.ts
+++ cypress/e2e/group1/dashboard.ts
@@ -33,17 +33,6 @@ describe('Dockstore dashboard', () => {
     cy.get('[data-cy=all-entries-btn]').contains('All Tools').should('have.attr', 'href').and('include', '/my-tools');
   });
 
-  it('no services display correctly', () => {
-    cy.visit('/dashboard');
-    cy.contains('Services');
-    cy.contains('You have not registered any services.');
-    cy.get('[data-cy=no-entry-register-modal]').contains('service');
-    cy.get('[data-cy=help-link]')
-      .contains('Learn more about services')
-      .should('have.attr', 'href')
-      .and('include', 'getting-started-with-services');
-  });
-
   it('no notebooks display correctly', () => {
     cy.visit('/dashboard');
     cy.contains('Notebooks');
