ee600f9958151c775c3ecd87b0b1609e18fce342
y-ng <68610185+y-ng@users.noreply.github.com>
2023-08-22 16:34:18 -0400

    add link to organization events

--- src/app/organizations/events.module.ts
+++ src/app/organizations/events.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 import { RefreshAlertModule } from '../shared/alert/alert.module';
 import { CustomMaterialModule } from './../shared/modules/material.module';
 import { EventsComponent } from './events/events.component';
 
 @NgModule({
-  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RefreshAlertModule],
+  imports: [CommonModule, CustomMaterialModule, FlexLayoutModule, RouterModule, RefreshAlertModule],
   declarations: [EventsComponent],
   exports: [EventsComponent],
 })
