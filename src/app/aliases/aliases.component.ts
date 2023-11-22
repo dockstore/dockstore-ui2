25f81b08d861822a9c474bae44c95aae5caf30c8
Stephen Von Worley <svonworl@ucsc.edu>
2023-11-21 19:40:59 -0800

    refactor moar

--- src/app/aliases/aliases.component.ts
+++ src/app/aliases/aliases.component.ts
@@ -14,10 +14,6 @@ import { AliasesService } from './state/aliases.service';
 })
 export class AliasesComponent extends Base implements OnInit {
   loading$: Observable<boolean>;
-  organization$: Observable<Organization | null>;
-  collection$: Observable<Collection | null>;
-  entry$: Observable<Entry | null>;
-  workflowVersionPathInfo$: Observable<WorkflowVersionPathInfo | null>;
   public type: string | null;
   public alias: string | null;
   public validType: boolean;
@@ -33,56 +29,72 @@ export class AliasesComponent extends Base implements OnInit {
 
   ngOnInit() {
     this.loading$ = this.aliasesQuery.loading$;
-    this.type = this.route.snapshot.paramMap.get('type');
+    this.type = this.normalizeType(this.route.snapshot.paramMap.get('type'));
     this.alias = this.route.snapshot.paramMap.get('alias');
-    this.validType = false;
+
     if (this.type === 'organizations') {
+      this.validType = true;
       this.aliasesService.updateOrganizationFromAlias(this.alias);
-      this.organization$ = this.aliasesQuery.organization$;
-      this.navigateTo(this.organization$, (organization: Organization) => ['/organizations', organization.name]);
-    }
-    if (this.type === 'collections') {
+      this.navigateTo(this.aliasesQuery.organization$, (organization: Organization) => ['/organizations', organization.name]);
+    } else if (this.type === 'collections') {
+      this.validType = true;
       this.aliasesService.updateCollectionFromAlias(this.alias);
-      this.collection$ = this.aliasesQuery.collection$;
-      this.navigateTo(this.collection$, (collection: Collection) => [
+      this.navigateTo(this.aliasesQuery.collection$, (collection: Collection) => [
         '/organizations',
         collection.organizationName,
         'collections',
         collection.name,
       ]);
-    }
-    if (
+    } else if (
       this.type === 'workflow-versions' ||
       this.type === 'tool-versions' ||
       this.type === 'service-versions' ||
       this.type === 'notebook-versions'
     ) {
+      this.validType = true;
       this.aliasesService.updateWorkflowVersionPathInfoFromAlias(this.alias);
-      this.workflowVersionPathInfo$ = this.aliasesQuery.workflowVersionPathInfo$;
-      this.navigateTo(this.workflowVersionPathInfo$, (info: WorkflowVersionPathInfo) => [
-        info.entryTypeMetadata.sitePath,
-        info.fullWorkflowPath + ':' + info.tagName,
-      ]);
-    }
-    if (this.type === 'workflows' || this.type === 'tools' || this.type === 'services' || this.type === 'notebooks') {
+      this.navigateTo(
+        this.aliasesQuery.workflowVersionPathInfo$,
+        (info: WorkflowVersionPathInfo) => [info.entryTypeMetadata.sitePath, info.fullWorkflowPath + ':' + info.tagName],
+        (info: WorkflowVersionPathInfo) => info.entryTypeMetadata.term === this.type.split('-')[0]
+      );
+    } else if (this.type === 'workflows' || this.type === 'tools' || this.type === 'services' || this.type === 'notebooks') {
+      this.validType = true;
       this.aliasesService.updateEntryFromAlias(this.alias);
-      this.entry$ = this.aliasesQuery.entry$;
-      this.navigateTo(this.entry$, (entry: Entry) => [entry.entryTypeMetadata.sitePath, this.getPath(entry)]);
+      this.navigateTo(
+        this.aliasesQuery.entry$,
+        (entry: Entry) => [entry.entryTypeMetadata.sitePath, this.getPath(entry)],
+        (entry: Entry) => entry.entryTypeMetadata.termPlural === this.type
+      );
+    } else {
+      this.validType = false;
     }
+  }
+
+  private normalizeType(type: string): string {
+    if (type === 'containers') {
+      return 'tools';
+    }
+    return type;
   }
 
   private getPath(entry: Entry): string {
     return (entry as Workflow).full_workflow_path ?? (entry as DockstoreTool).tool_path;
   }
 
-  private navigateTo<EntityType>(entity$: Observable<EntityType>, entityToPath: (entity: EntityType) => string[]): void {
-    this.validType = true;
+  private navigateTo<EntityType>(
+    entity$: Observable<EntityType>,
+    entityToPath: (entity: EntityType) => string[],
+    entityFilter: (entity: EntityType) => boolean = () => true
+  ): void {
     entity$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((entity: EntityType | null) => {
-      if (entity) {
+      if (entity && entityFilter(entity)) {
         const path = entityToPath(entity);
         this.router.navigate(path);
+        this.aliasNotFound = false;
+      } else {
+        this.aliasNotFound = true;
       }
-      this.aliasNotFound = !entity;
     });
   }
 }
