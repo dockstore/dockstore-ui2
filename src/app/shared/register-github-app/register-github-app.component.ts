937a3b1e5cf1cf3e736bb23d7c00132308d8a143
hyunnaye <nayeonhyun802@gmail.com>
2023-11-23 12:34:28 -0500

    open github apps auth in new tab

--- src/app/shared/register-github-app/register-github-app.component.ts
+++ src/app/shared/register-github-app/register-github-app.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Dockstore } from '../dockstore.model';
 import { SessionQuery } from '../session/session.query';
 import { UserQuery } from '../user/user.query';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material/dialog';
+import { RegisterGithubAppModalComponent } from '../../workflow/register-workflow-modal/register-github-app-modal/register-github-app-modal.component';
 
 @Component({
   selector: 'app-register-github-app',
@@ -13,5 +16,23 @@ export class RegisterGithubAppComponent {
   public isUsernameChangeRequired$ = this.userQuery.isUsernameChangeRequired$;
   @Input() public entryType: string;
 
-  constructor(private sessionQuery: SessionQuery, private userQuery: UserQuery) {}
+  constructor(
+    private sessionQuery: SessionQuery,
+    private userQuery: UserQuery,
+    private router: Router,
+    public parentDialogRef: MatDialogRef<RegisterGithubAppModalComponent>
+  ) {}
+
+  public registerGitHubApp(observable) {
+    observable.subscribe(
+      (response) => {
+        window.open(response, '_blank');
+      },
+      (error) => {
+        console.log('GitHub Apps registration error: ' + error);
+      }
+    );
+    this.parentDialogRef.close();
+    this.router.navigate(['/my-workflows']);
+  }
 }
