d637ce2ff1a80667988984dbd6420ec0045e42c4
Kathy Tran <kathylevtran@gmail.com>
2024-01-11 18:09:17 -0500

    Remove metrics feature flag

--- src/app/shared/feature.service.ts
+++ src/app/shared/feature.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Dockstore } from './dockstore.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeatureService {
   updateFeatureFlags(queryParams: string) {
-    const urlSearchParams = new URLSearchParams(queryParams);
-    const gitHubAppCallBackToMetrics = urlSearchParams.has('state') && urlSearchParams.get('state').includes('metrics');
-    const metrics = urlSearchParams.has('metrics') || gitHubAppCallBackToMetrics;
-    Dockstore.FEATURES.enableMetrics = metrics;
+    // Currently, there are no feature flags being used.
   }
 }
