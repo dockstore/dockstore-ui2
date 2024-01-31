5c5b0b7c42634d873c7b21c58049a55049bcab8b
hyunnaye <nayeonhyun802@gmail.com>
2024-01-31 14:43:41 -0500

    hide blank metrics rows

--- src/app/workflow/executions/executions-tab.component.ts
+++ src/app/workflow/executions/executions-tab.component.ts
@@ -169,11 +169,17 @@ export class ExecutionsTabComponent extends EntryTab implements OnChanges {
    */
   private createExecutionsTable(metrics: Metrics | null): ExecutionMetricsTableObject[] {
     let executionsTable: ExecutionMetricsTableObject[] = [];
-    // Only create the table if one of the execution metrics exist
-    if (metrics && (metrics.cpu || metrics.memory || metrics.executionTime || metrics.cost)) {
+    // Only add the rows if there are data for that type
+    if (metrics && metrics.cpu) {
       executionsTable.push({ metric: 'CPU', ...metrics?.cpu });
+    }
+    if (metrics && metrics.memory) {
       executionsTable.push({ metric: 'Memory', ...metrics?.memory });
+    }
+    if (metrics && metrics.executionTime) {
       executionsTable.push({ metric: 'Run Time', ...metrics?.executionTime });
+    }
+    if (metrics && metrics.cost) {
       executionsTable.push({ metric: 'Cost', ...metrics?.cost });
     }
     return executionsTable;
