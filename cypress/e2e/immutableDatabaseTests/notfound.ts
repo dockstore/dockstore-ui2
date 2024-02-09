b41566fe03a8fde53b4adb74cedd1b24bb2a1521
Lucia Zhang <122565245+ll5zh@users.noreply.github.com>
2024-02-09 16:29:21 -0500

    test page not found from different routes

--- cypress/e2e/immutableDatabaseTests/notfound.ts
+++ cypress/e2e/immutableDatabaseTests/notfound.ts
@@ -8,6 +8,14 @@
  *         http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
+    cy.visit('/containers/thistooldoesnotexist');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/containers/thistooldoesnotexist');
+
+    cy.visit('/tools/lmnop');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/tools/lmnop');
+
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
@@ -22,5 +30,29 @@ describe('Dockstore not found page', () => {
     cy.visit('/bewareoftheleopard');
     cy.contains("Don't panic.");
     cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/bewareoftheleopard');
+
+    cy.visit('/workflows/asdf');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/workflows/asdf');
+
+    cy.visit('/organizations/abcdefg');
+    cy.url().should('eq', Cypress.config().baseUrl + '/organizations/abcdefg');
+
+    cy.visit('/users/notarealuser');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/users/notarealuser');
+
+    cy.visit('/notebooks/github.con/entrywithtypo');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/notebooks/github.con/entrywithtypo');
+
+    cy.visit('/services/github.com/invalidentrypath');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/services/github.com/invalidentrypath');
+
+    cy.visit('/apptools/ghjkl');
+    cy.contains('Page Not Found');
+    cy.url().should('eq', Cypress.config().baseUrl + '/apptools/ghjkl');
   });
 });
