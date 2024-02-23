c2f3a5b92fb4289cc0a06db5cd8570ce658c4a8f
Kathy Tran <kathylevtran@gmail.com>
2024-02-23 10:22:02 -0500

    Fix test

--- src/app/search/state/search.service.spec.ts
+++ src/app/search/state/search.service.spec.ts
@@ -135,20 +135,23 @@ describe('SearchService', () => {
       descriptorTypeSubclass: Workflow.DescriptorTypeSubclassEnum.NA,
       full_workflow_path: 'abc',
     };
+    a['all_authors'] = a['authors'];
 
-    const b: Workflow = {
+    const b: Object = {
       ...a,
       authors: [{ name: 'B' }],
       full_workflow_path: 'Bcd',
       starredUsers: [{ isAdmin: false, curator: false, platformPartner: null, setupComplete: true }],
     };
+    b['all_authors'] = b['authors'];
 
     const c: Workflow = { ...a, authors: [], full_workflow_path: null, descriptorType: Workflow.DescriptorTypeEnum.WDL };
+    c['all_authors'] = c['authors'];
 
-    expect(searchService.compareAttributes(a, b, 'author', 'asc', 'workflow')).toEqual(-1);
-    expect(searchService.compareAttributes(a, b, 'author', 'desc', 'workflow')).toEqual(1);
-    expect(searchService.compareAttributes(b, c, 'author', 'asc', 'workflow')).toEqual(-1);
-    expect(searchService.compareAttributes(b, c, 'author', 'desc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(a, b, 'all_authors', 'asc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(a, b, 'all_authors', 'desc', 'workflow')).toEqual(1);
+    expect(searchService.compareAttributes(b, c, 'all_authors', 'asc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(b, c, 'all_authors', 'desc', 'workflow')).toEqual(1);
     expect(searchService.compareAttributes(a, c, 'descriptorType', 'asc', 'workflow')).toEqual(-1);
     expect(searchService.compareAttributes(a, b, 'descriptorType', 'desc', 'workflow')).toEqual(-0);
     expect(searchService.compareAttributes(a, b, 'starredUsers', 'asc', 'workflow')).toEqual(-1);
