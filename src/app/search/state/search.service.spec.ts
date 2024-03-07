fe16fe48371b0976d29bd4144d52661e632da901
Kathy Tran <kathylevtran@gmail.com>
2024-03-07 13:37:06 -0500

    Display skipped executions warning for metrics (#1938)
    https://ucsc-cgl.atlassian.net/browse/SEAB-6046
    * Run 1.16.0 migrations
    * Only display skipped executions info if user is admin/curator/platformPartner

--- src/app/search/state/search.service.spec.ts
+++ src/app/search/state/search.service.spec.ts
@@ -123,7 +123,7 @@ describe('SearchService', () => {
   it('should sort workflows correctly', inject([SearchService], (service: SearchService) => {
     const a: Workflow = {
       type: '',
-      author: 'a',
+      authors: [{ name: 'a' }],
       gitUrl: 'https://giturl',
       mode: Workflow.ModeEnum.FULL,
       organization: '',
@@ -135,20 +135,23 @@ describe('SearchService', () => {
       descriptorTypeSubclass: Workflow.DescriptorTypeSubclassEnum.NA,
       full_workflow_path: 'abc',
     };
+    a['all_authors'] = a['authors'];
 
-    const b: Workflow = {
+    const b: Object = {
       ...a,
-      author: 'B',
+      authors: [{ name: 'B' }],
       full_workflow_path: 'Bcd',
       starredUsers: [{ isAdmin: false, curator: false, platformPartner: null, setupComplete: true }],
     };
+    b['all_authors'] = b['authors'];
 
-    const c: Workflow = { ...a, author: null, full_workflow_path: null, descriptorType: Workflow.DescriptorTypeEnum.WDL };
+    const c: Workflow = { ...a, authors: [], full_workflow_path: null, descriptorType: Workflow.DescriptorTypeEnum.WDL };
+    c['all_authors'] = c['authors'];
 
-    expect(searchService.compareAttributes(a, b, 'author', 'asc', 'workflow')).toEqual(-1);
-    expect(searchService.compareAttributes(a, b, 'author', 'desc', 'workflow')).toEqual(1);
-    expect(searchService.compareAttributes(b, c, 'author', 'asc', 'workflow')).toEqual(-1);
-    expect(searchService.compareAttributes(b, c, 'author', 'desc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(a, b, 'all_authors', 'asc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(a, b, 'all_authors', 'desc', 'workflow')).toEqual(1);
+    expect(searchService.compareAttributes(b, c, 'all_authors', 'asc', 'workflow')).toEqual(-1);
+    expect(searchService.compareAttributes(b, c, 'all_authors', 'desc', 'workflow')).toEqual(1);
     expect(searchService.compareAttributes(a, c, 'descriptorType', 'asc', 'workflow')).toEqual(-1);
     expect(searchService.compareAttributes(a, b, 'descriptorType', 'desc', 'workflow')).toEqual(-0);
     expect(searchService.compareAttributes(a, b, 'starredUsers', 'asc', 'workflow')).toEqual(-1);
