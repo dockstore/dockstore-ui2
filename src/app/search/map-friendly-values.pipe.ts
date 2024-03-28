5f298a0d4bc6b53495b4f557fc11fcc3e9ed4ead
Steve Von Worley <svonworl@ucsc.edu>
2024-03-28 09:05:32 -0700

    add UNINSTALL->Uninstall mapping to MapFriendlyValuesPipe
    https://ucsc-cgl.atlassian.net/browse/SEAB-6000

--- src/app/search/map-friendly-values.pipe.ts
+++ src/app/search/map-friendly-values.pipe.ts
@@ -118,6 +118,7 @@ export class MapFriendlyValuesPipe implements PipeTransform {
         ['PUSH', 'Push'],
         ['DELETE', 'Delete'],
         ['INSTALL', 'Install'],
+        ['UNINSTALL', 'Uninstall'],
         ['PUBLISH', 'Publish'],
       ]),
     ],
