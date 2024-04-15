47345c02644084ee792d7a94e0a110838c74b1be
Lucia Zhang <122565245+ll5zh@users.noreply.github.com>
2024-04-15 16:01:23 -0400

    try fixing services flake - encode url assertions

--- cypress/e2e/immutableDatabaseTests/services.ts
+++ cypress/e2e/immutableDatabaseTests/services.ts
@@ -29,19 +29,19 @@ describe('Dockstore Home', () => {
     setTokenUserViewPort();
     it('Redirects to my-tools', () => {
       cy.visit('/githubCallback?state=/my-tools/quay.io/A2/b1');
-      cy.url().should('contain', '/my-tools/quay.io/A2/b1');
+      cy.url().should('contain', '%2Fmy-tools%2Fquay.io%2FA2%2Fb1');
     });
     it('Redirects to my-workflows', () => {
       cy.visit('/githubCallback?state=/my-workflows/github.com/A/l');
-      cy.url().should('contain', '/my-workflows/github.com/A/l');
+      cy.url().should('contain', '%2Fmy-workflows%2Fgithub.com%2FA%2Fl');
     });
     it('Redirects to my-services', () => {
       cy.visit('/githubCallback?state=/services/github.com/garyluu/another-test-service');
-      cy.url().should('contain', '/services/github.com/garyluu/another-test-service');
+      cy.url().should('contain', '%2Fservices%2Fgithub.com%2Fgaryluu%2Fanother-test-service');
     });
     it('Redirects to dashboard', () => {
       cy.visit('githubCallback?state=/dashboard');
-      cy.url().should('contain', '/dashboard');
+      cy.url().should('contain', '%2Fdashboard');
     });
   });
 
