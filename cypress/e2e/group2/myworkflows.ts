935051db804dad234f160b05cc84b89a07beda5b
Stephen Von Worley <svonworl@ucsc.edu>
2024-06-25 13:20:46 -0700

    test fix

--- cypress/e2e/group2/myworkflows.ts
+++ cypress/e2e/group2/myworkflows.ts
@@ -542,6 +542,7 @@ describe('Dockstore my workflows part 3', () => {
       cy.wait(1000);
       cy.get('#2-register-workflow-option').click();
       cy.contains('button', 'Next').click();
+      cy.get('form[name="registerWorkflowForm"]').should('be.visible');
       // Untouched form should not have errors but is disabled
       cy.get('#submitButton').should('be.disabled');
       notHaveAlert();
