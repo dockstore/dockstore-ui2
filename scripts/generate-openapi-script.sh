9fd879b7cae941a138f193940cf6d7ff0cab7865
hyunnaye <nayeonhyun802@gmail.com>
2024-07-19 13:19:02 -0400

    delete test

--- scripts/generate-openapi-script.sh
+++ scripts/generate-openapi-script.sh
@@ -26,8 +26,6 @@ fi
 
 java -jar openapi-generator-cli.jar generate -i "${OPENAPI_PATH}" -g typescript-angular -o src/app/shared/openapi -c swagger-config.json --skip-validate-spec
 
-echo $(pwd)
-
 for file in src/app/shared/openapi/*.ts src/app/shared/openapi/**/*.ts; do
  echo "//@ts-nocheck" | cat - $file >> tmpFile && mv tmpFile $file
 done
