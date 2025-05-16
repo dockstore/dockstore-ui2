021170517d1dfcd3d194a94d2c2ea1272a0af2d1
Denis Yuen <denis.yuen@oicr.on.ca>
2025-05-16 15:54:01 -0400

    wait longer

--- scripts/wait-for.sh
+++ scripts/wait-for.sh
@@ -3,6 +3,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 set -o xtrace
-wget --output-document /dev/null --waitretry=10 --tries=20 --retry-connrefused localhost:4200 || true
-wget --output-document /dev/null --waitretry=10 --tries=20 --retry-connrefused localhost:8080 || true
-wget --output-document /dev/null --waitretry=10 --tries=20 --retry-connrefused localhost:9200 || true
+wget --output-document /dev/null --waitretry=100 --tries=200 --retry-connrefused localhost:4200 || true
+wget --output-document /dev/null --waitretry=100 --tries=200 --retry-connrefused localhost:8080 || true
+wget --output-document /dev/null --waitretry=100 --tries=200 --retry-connrefused localhost:9200 || true
+
