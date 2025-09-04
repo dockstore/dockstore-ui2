9500107e79dfc3ab8313c054875639005050f16f
Stephen Von Worley <svonworl@ucsc.edu>
2025-09-04 15:43:35 -0700

    point at snapshot webservice, disable mouseovers on thumbnail graphs

--- src/app/shared/graphs/thumbnail-time-series-graph.component.ts
+++ src/app/shared/graphs/thumbnail-time-series-graph.component.ts
@@ -54,12 +54,12 @@ export class ThumbnailTimeSeriesGraphComponent implements OnChanges {
             },
           },
         },
-        tooltips: {
-          enabled: false,
-        },
         plugins: {
           legend: {
             display: false,
+          },
+          tooltip: {
+            enabled: false,
           },
         },
         responsive: false,
