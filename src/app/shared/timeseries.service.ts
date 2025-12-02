ead049e42acea19400a0b2c7297f544dbe8e9d41
Stephen Von Worley <svonworl@ucsc.edu>
2025-12-01 21:31:09 -0800

    adjust for new time series begins/ends representation

--- src/app/shared/timeseries.service.ts
+++ src/app/shared/timeseries.service.ts
@@ -19,58 +19,62 @@ import { TimeSeriesMetric } from './openapi';
 @Injectable()
 export class TimeSeriesService {
   adjustTimeSeries(timeSeriesMetric: TimeSeriesMetric, now: Date, binCount: number): TimeSeriesMetric {
-    const adjusted: TimeSeriesMetric = {
-      begins: timeSeriesMetric.begins,
-      interval: timeSeriesMetric.interval,
-      values: timeSeriesMetric.values.slice(),
-    };
-
-    // TODO make this work for daily intervals, also.
-    const day = 24 * 60 * 60 * 1000; // milliseconds in one day (hours * minutes * seconds * milliseconds)
+    // TODO make this work for monthly intervals, also.
+    const day = 24 * 60 * 60 * 1000; // Milliseconds in one day (hours * minutes * seconds * milliseconds).
     const week = 7 * day;
-    let begins = Number(adjusted.begins); // Midpoint of the oldest bin
-    const ends = begins + (adjusted.values.length - 1) * week + week / 2; // Exact time that the youngest bin ends
 
-    // Expand the newer end of the time series to overlap the current date
+    // Store information from the original time series in variables that we will adjust and then reassemble into a new time series at the end of this method.
+    let begins = Number(timeSeriesMetric.begins);
+    let ends = Number(timeSeriesMetric.ends);
+    let values = timeSeriesMetric.values.slice();
+
+    // Append bins to the newer end of the time series to make it overlap the specified date.
     const binsToAppend = Math.ceil((now.getTime() - ends) / week);
     if (binsToAppend > 0) {
-      adjusted.values = [...adjusted.values, ...this.zeros(binsToAppend)];
+      values = [...values, ...this.zeros(binsToAppend)];
+      ends = ends + binsToAppend * week;
     }
 
-    // Expand the older end of the time series to match the desired bin count
-    const binsToPrepend = binCount - adjusted.values.length;
+    // Prepend bins to the older end of the time series to make its length match the desired bin count.
+    const binsToPrepend = binCount - values.length;
     if (binsToPrepend > 0) {
-      adjusted.values = [...this.zeros(binsToPrepend), ...adjusted.values];
+      values = [...this.zeros(binsToPrepend), ...values];
       begins = begins - binsToPrepend * week;
     }
 
-    // Trim the older end of the time series to match the desired bin count
-    const binsToTrim = adjusted.values.length - binCount;
+    // Trim bins from the older end of the time series to make its length match the desired bin count.
+    const binsToTrim = values.length - binCount;
     if (binsToTrim > 0) {
-      adjusted.values = adjusted.values.slice(binsToTrim);
+      values = values.slice(binsToTrim);
       begins = begins + binsToTrim * week;
     }
 
-    adjusted.begins = String(begins);
-    return adjusted;
+    return {
+      begins: String(begins),
+      ends: String(ends),
+      values: values,
+      interval: timeSeriesMetric.interval,
+    };
   }
 
   emptyTimeSeries(timeSeriesMetric: TimeSeriesMetric) {
     return {
       begins: timeSeriesMetric.begins,
+      ends: timeSeriesMetric.ends,
       interval: timeSeriesMetric.interval,
       values: this.zeros(timeSeriesMetric.values.length),
     };
   }
 
   labelsFromTimeSeries(timeSeriesMetric: TimeSeriesMetric): string[] {
-    // TODO make this work for daily time series, also.
-    const day = 24 * 60 * 60 * 1000; // milliseconds in one day (hours * minutes * seconds * milliseconds)
+    // TODO make this work for monthly time series, also.
+    const day = 24 * 60 * 60 * 1000; // Milliseconds in one day (hours * minutes * seconds * milliseconds).
     const week = 7 * day;
+    const halfWeek = week / 2;
     let begins = Number(timeSeriesMetric.begins);
     const labels: string[] = [];
     for (let i = 0; i < timeSeriesMetric.values.length; i++) {
-      const middle = begins + i * week;
+      const middle = begins + i * week + halfWeek;
       const firstDay: Date = new Date(middle - 3 * day);
       const lastDay: Date = new Date(middle + 3 * day);
       const label = this.formatShortDate(firstDay) + ' to ' + this.formatShortDate(lastDay);
