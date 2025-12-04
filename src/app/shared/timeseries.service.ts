5a7409c5b49b71eb746078dc4b67fcc4fe1aa6fe
Stephen Von Worley <svonworl@ucsc.edu>
2025-12-03 18:34:17 -0800

    continue refactor

--- src/app/shared/timeseries.service.ts
+++ src/app/shared/timeseries.service.ts
@@ -19,9 +19,7 @@ import { TimeSeriesMetric } from './openapi';
 @Injectable()
 export class TimeSeriesService {
   adjustTimeSeries(timeSeriesMetric: TimeSeriesMetric, now: Date, binCount: number): TimeSeriesMetric {
-    // TODO make this work for monthly time series, also.
-    const day = 24 * 60 * 60 * 1000; // Milliseconds in one day (hours * minutes * seconds * milliseconds).
-    const week = 7 * day;
+    const ops = this.createIntervalOps(timeSeriesMetric);
 
     // Store information from the original time series in variables that we will adjust and then reassemble into a new time series at the end of this method.
     let begins = Number(timeSeriesMetric.begins);
@@ -29,24 +27,24 @@ export class TimeSeriesService {
     let values = timeSeriesMetric.values.slice();
 
     // Append bins to the newer end of the time series to make it overlap the specified date.
-    const binsToAppend = Math.ceil((now.getTime() - ends) / week);
+    const binsToAppend = ops.countIntervals(ends, now.getTime());
     if (binsToAppend > 0) {
       values = [...values, ...this.zeros(binsToAppend)];
-      ends = ends + binsToAppend * week;
+      ends = ops.addIntervals(ends, binsToAppend);
     }
 
     // Prepend bins to the beginning (older end) of a "short" time series to make its length match the desired bin count.
     const binsToPrepend = binCount - values.length;
     if (binsToPrepend > 0) {
       values = [...this.zeros(binsToPrepend), ...values];
-      begins = begins - binsToPrepend * week;
+      begins = ops.subtractIntervals(begins, binsToPrepend);
     }
 
     // Trim bins from the beginning (older end) of a "long" time series to make its length match the desired bin count.
     const binsToTrim = values.length - binCount;
     if (binsToTrim > 0) {
       values = values.slice(binsToTrim);
-      begins = begins + binsToTrim * week;
+      begins = ops.addIntervals(begins, binsToTrim);
     }
 
     return {
@@ -56,22 +54,6 @@ export class TimeSeriesService {
       values: values,
     };
   }
-
-  /*
-  // internal class that implements ops for each interval type
-  countIntervals(from: number, to: number): number {
-    // TODO implement weekly, monthly
-  }
-
-  addIntervals(from: number, intervalCount: number): number {
-  }
-
-  subtractIntervals(from: number, intervalCount: number): number {
-  }
-
-  label(begins: number): string[] {
-  }
-  */
 
   emptyTimeSeries(timeSeriesMetric: TimeSeriesMetric) {
     return {
@@ -83,35 +65,63 @@ export class TimeSeriesService {
   }
 
   labelsFromTimeSeries(timeSeriesMetric: TimeSeriesMetric): string[] {
-    // TODO make this work for monthly time series, also.
-    const day = 24 * 60 * 60 * 1000; // Milliseconds in one day (hours * minutes * seconds * milliseconds).
-    const week = 7 * day;
-    const halfWeek = week / 2;
-    let begins = Number(timeSeriesMetric.begins);
-    const labels: string[] = [];
-    for (let i = 0; i < timeSeriesMetric.values.length; i++) {
-      const middle = begins + i * week + halfWeek;
-      const firstDay: Date = new Date(middle - 3 * day);
-      const lastDay: Date = new Date(middle + 3 * day);
-      const label = this.isoYearMonthDay(firstDay) + ' to ' + this.isoYearMonthDay(lastDay);
-      labels.push(label);
-    }
-    return labels;
+    const ops = this.createIntervalOps(timeSeriesMetric);
+    return ops.labels(Number(timeSeriesMetric.begins), timeSeriesMetric.values.length);
   }
-
-  // TODO labelsFromWeeklyTimeSeries, labelsFromMonthlyTimeSeries
-
-  private isoYearMonthDay(date: Date): string {
-    return date.toISOString().slice(0, 10);
-  }
-
-  /*
-  private isoYearMonth(date: Date): string {
-    return date.toISOString().slice(0, 7);
-  }
-  */
 
   private zeros(count: number): number[] {
     return new Array(count).fill(0);
   }
+
+  private createIntervalOps(timeSeriesMetric: TimeSeriesMetric): IntervalOps {
+    return new WeeklyIntervalOps();
+  }
 }
+
+module TimeConstants {
+  export const DAY_MILLIS = 24 * 60 * 60 * 1000;
+  export const WEEK_MILLIS = 7 * DAY_MILLIS;
+}
+
+interface IntervalOps {
+  countIntervals(from: number, to: number): number;
+  addIntervals(from: number, intervalCount: number): number;
+  subtractIntervals(from: number, intervalCount: number): number;
+  labels(begins: number, length: number): string[];
+}
+
+class WeeklyIntervalOps implements IntervalOps {
+  countIntervals(from: number, to: number): number {
+    return Math.ceil((to - from) / TimeConstants.WEEK_MILLIS);
+  }
+
+  addIntervals(from: number, intervalCount: number): number {
+    return from + intervalCount * TimeConstants.WEEK_MILLIS;
+  }
+
+  subtractIntervals(from: number, intervalCount: number): number {
+    return from - intervalCount * TimeConstants.WEEK_MILLIS;
+  }
+
+  labels(from: number, length: number): string[] {
+    const labels: string[] = [];
+    for (let i = 0; i < length; i++) {
+      const middle = from + (i + 0.5) * TimeConstants.WEEK_MILLIS;
+      const firstDay: Date = new Date(middle - 3 * TimeConstants.DAY_MILLIS);
+      const lastDay: Date = new Date(middle + 3 * TimeConstants.DAY_MILLIS);
+      const label = isoYearMonthDay(firstDay) + ' to ' + isoYearMonthDay(lastDay);
+      labels.push(label);
+    }
+    return labels;
+  }
+}
+
+function isoYearMonthDay(date: Date): string {
+  return date.toISOString().slice(0, 10);
+}
+
+/*
+function isoYearMonth(date: Date): string {
+  return date.toISOString().slice(0, 7);
+}
+*/
