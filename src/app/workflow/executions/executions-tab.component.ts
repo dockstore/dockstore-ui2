19fdac9f07e2a82cd06bef5cb9297cde8c1f12b9
Steve Von Worley <svonworl@ucsc.edu>
2026-01-08 09:40:21 -0800

    change run time histogram ticks to be consistent with new bin edges
    https://ucsc-cgl.atlassian.net/browse/SEAB-7408

--- src/app/workflow/executions/executions-tab.component.ts
+++ src/app/workflow/executions/executions-tab.component.ts
@@ -370,7 +370,7 @@ export class ExecutionsTabComponent extends EntryTab implements OnInit, OnChange
       const hiHoursMinutesSeconds = this.formatDuration(hi, 2);
       if (lo <= 0) {
         labels.push(`t < ${hiHoursMinutesSeconds}`);
-      } else if (hi === Number.POSITIVE_INFINITY) {
+      } else if (hi == Number.POSITIVE_INFINITY) {
         labels.push(`${loHoursMinutesSeconds} \u2264 t`);
       } else {
         labels.push(`${loHoursMinutesSeconds} \u2264 t < ${hiHoursMinutesSeconds}`);
@@ -422,7 +422,8 @@ export class ExecutionsTabComponent extends EntryTab implements OnInit, OnChange
               ...this.generateLogTicks(60, 9, histogramMetric), // 1m ticks
               ...this.generateLogTicks(600, 5, histogramMetric), // 10m ticks
               ...this.generateLogTicks(3600, 9, histogramMetric), // 1h ticks
-              ...this.generateLogTicks(36000, 2, histogramMetric), // 10h ticks
+              ...this.generateLogTicks(36000, 9, histogramMetric), // 10h ticks
+              ...this.generateLogTicks(360000, 1, histogramMetric), // 100h tick
             ];
           },
         },
