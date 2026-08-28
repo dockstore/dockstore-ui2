5296c6d343cc633f2570aaac72f0a8010ac41f84
Stephen Von Worley <svonworl@ucsc.edu>
2026-08-27 23:46:03 -0700

    adjust babel version for tests

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,6 +1,15 @@
+const path = require('path');
+
 module.exports = (on, config) => {
   const webpackPreprocessor = require('@cypress/webpack-batteries-included-preprocessor');
   const webpackOptions = webpackPreprocessor.defaultOptions.webpackOptions;
+
+  // The Angular linker plugin requires Babel ^8, but the babel-loader that hoists to the
+  // root node_modules only supports Babel 7. @angular-devkit/build-angular already bundles
+  // a compatible babel-loader + @babel/core@8 pair, so resolve through it instead.
+  const babelLoaderPath = require.resolve('babel-loader', {
+    paths: [path.dirname(require.resolve('@angular-devkit/build-angular/package.json'))],
+  });
 
   webpackOptions.module.rules.unshift({
     test: /[/\\]@angular[/\\].+\.m?js$/,
@@ -8,7 +17,7 @@ module.exports = (on, config) => {
       fullySpecified: false,
     },
     use: {
-      loader: 'babel-loader',
+      loader: babelLoaderPath,
       options: {
         plugins: ['@angular/compiler-cli/linker/babel'],
         compact: false,
